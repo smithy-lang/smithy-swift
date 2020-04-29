cf9caf89297a5f20c5ebe809ec40cddc4760ec66
Nicki Stone <nickik@amazon.com>
2020-04-29 14:32:07 -0700

    small change

--- smithy-swift/smithy-swift-codegen-test/expected/Weather/WeatherClientBehavior.swift
+++ smithy-swift/smithy-swift-codegen-test/expected/Weather/WeatherClientBehavior.swift
@@ -14,10 +14,10 @@
 //
 
 public protocol WeatherClientBehavior {
-    func getCity(input: GetCityInput, completion: (GetCityOutput) -> Void)
-    func getCityImage(input: GetCityImageInput, completion: (GetCityImageOutput) -> Void)
-    func getCurrentTime(input: GetCurrentTimeInput, completion: (GetCurrentTimeOutput) -> Void)
-    func getForecast(input: GetForecastInput, completion: (GetForecastOutput) -> Void)
-    func listCities(input: ListCitiesInput, completion: (ListCitiesOutput) -> Void)
+     func getCity(input: GetCityInput, completion: (GetCityOutput) -> Void)
+     func getCityImage(input: GetCityImageInput, completion: (GetCityImageOutput) -> Void)
+     func getCurrentTime(input: GetCurrentTimeInput, completion: (GetCurrentTimeOutput) -> Void)
+     func getForecast(input: GetForecastInput, completion: (GetForecastOutput) -> Void)
+     func listCities(input: ListCitiesInput, completion: (ListCitiesOutput) -> Void)
 
-}
+ }
