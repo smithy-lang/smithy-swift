b10dbc61f1c12b747dd4b45d86f614d54ebfb608
Nicki Stone <nickik@amazon.com>
2020-05-12 16:36:24 -0700

    fix(codegen): added copyright to each file

--- smithy-swift/smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/PodSpecGenerator.kt
+++ smithy-swift/smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/PodSpecGenerator.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package software.amazon.smithy.swift.codegen
 
 import software.amazon.smithy.build.FileManifest
@@ -19,7 +34,7 @@ fun writePodspec(settings: SwiftSettings, fileManifest: FileManifest, dependenci
         writer.write("spec.authors      = { '${settings.author}' => '${settings.author.toLowerCase().replace(" ", "")}' }")
         writer.write("spec.summary      = '${settings.moduleDescription}'")
         writer.write("spec.platform     = :ios, '8.0'")
-        writer.write("spec.swift_version = '4.0'")
+        writer.write("spec.swift_version = '5.0'")
         writer.write("spec.source       = { :git => '${settings.gitRepo}',\n" +
                 "                     :tag => ${settings.moduleVersion}}")
         writer.write("spec.requires_arc = true")
