39cca2dc1a01298a8365152f120c3c24e615a0d5
Aaron J Todd <todaaron@amazon.com>
2020-05-19 15:36:57 -0400

    feature(rt): add core HTTP protocol primitives

---
+++ smithy-kotlin/client-runtime/protocol/http/common/src/software/aws/clientrt/http/Url.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package software.aws.clientrt.http
+
+/**
+ * Represents a URL e.g. `https://mydomain.com/api?foo=baz&bar=11
+ *
+ * General form: `[scheme:][//[userinfo@]host][/]path[?query][#fragment]`
+ *
+ * @property scheme The wire protocol (e.g. http, https, ws, wss, etc)
+ * @property host hostname
+ * @property port port to connect to the host on, defaults to [Protocol.defaultPort]
+ * @property path (raw) path without the query
+ * @property parameters (raw) query parameters
+ * @property fragment URL fragment
+ * @property userInfo username and pasword (optional)
+ * @property forceQuery keep trailing question mark regardless of whether there are any query parameters
+ */
+data class Url(
+    val scheme: Protocol,
+    val host: String,
+    val port: Int = scheme.defaultPort,
+    val path: String = "",
+    val parameters: QueryParameters? = null,
+    val fragment: String? = null,
+    val userInfo: UserInfo? = null,
+    val forceQuery: Boolean = false
+) {
+    init {
+        require(port in 1..65536) { "port must be in between 1 and 65536" }
+    }
+
+    override fun toString(): String = buildString {
+        // FIXME - the userinfo, path, and fragment are raw at this point and need escaped as well probably
+        append(scheme.protocolName)
+        append("://")
+        userInfo?.let { userinfo ->
+            if (userinfo.username.isNotBlank()) {
+                append(userinfo.username)
+                if (userinfo.password.isNotBlank()) {
+                    append(":${userinfo.password}")
+                }
+                append("@")
+            }
+        }
+
+        append(host)
+        if (port != scheme.defaultPort) {
+            append(":$port")
+        }
+
+        if (path.isNotBlank()) {
+            append("/")
+            append(path.removePrefix("/"))
+        }
+
+        if ((parameters != null && !parameters.isEmpty()) || forceQuery) {
+            append("?")
+        }
+        parameters?.urlEncodeTo(this)
+
+        if (fragment != null && fragment.isNotBlank()) {
+            append("#")
+            append(fragment)
+        }
+    }
+}
+
+/**
+ * URL username and password
+ */
+data class UserInfo(val username: String, val password: String)
