16c9b11a63e8b064ec0072e3b90d92dc15a56b32
Aaron J Todd <todaaron@amazon.com>
2020-06-30 13:05:49 -0400

    refactor: update generated copy() method to go through a builder

--- smithy-kotlin/smithy-kotlin-codegen/src/test/kotlin/software/amazon/smithy/kotlin/codegen/StructureGeneratorTest.kt
+++ smithy-kotlin/smithy-kotlin-codegen/src/test/kotlin/software/amazon/smithy/kotlin/codegen/StructureGeneratorTest.kt
@@ -136,17 +136,7 @@ class MyStruct private constructor(builder: BuilderImpl) {
     @Test
     fun `it renders a copy implementation`() {
         val expected = """
-    fun copy(
-        bar: Integer = this.bar,
-        baz: Integer? = this.baz,
-        foo: String? = this.foo,
-        quux: Qux? = this.quux
-    ): MyStruct = BuilderImpl(this).apply {
-        this.bar = bar
-        this.baz = baz
-        this.foo = foo
-        this.quux = quux
-    }.build()
+    fun copy(block: DslBuilder.() -> Unit = {}): MyStruct = BuilderImpl(this).apply(block).build()
 """
         commonTestContents.shouldContain(expected)
     }
