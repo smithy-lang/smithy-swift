a75418dac9da24785c72ec650a04b7d6ad53b683
Ken Gilmer <kggilmer@amazon.com>
2020-08-11 12:00:42 -0700

    Remove invalid comments from XPP copied interface comments.

--- smithy-kotlin/client-runtime/serde/serde-xml/common/src/software.aws.clientrt.serde.xml/XmlStreamWriter.kt
+++ smithy-kotlin/client-runtime/serde/serde-xml/common/src/software.aws.clientrt.serde.xml/XmlStreamWriter.kt
@@ -7,39 +7,12 @@ package software.aws.clientrt.serde.xml
 
 
 /**
- * Define an interface to serialization of XML Infoset.
- * This interface abstracts away if serialized XML is XML 1.0 compatible text or
- * other formats of XML 1.0 serializations (such as binary XML for example with WBXML).
- *
- *
- * **PLEASE NOTE:** This interface will be part of XmlPull 1.2 API.
- * It is included as basis for discussion. It may change in any way.
- *
- *
- * Exceptions that may be thrown are: IOException or runtime exception
- * (more runtime exceptions can be thrown but are not declared and as such
- * have no semantics defined for this interface):
- *
- *  * *IllegalArgumentException* - for almost all methods to signal that
- * argument is illegal
- *  * *IllegalStateException* - to signal that call has good arguments but
- * is not expected here (violation of contract) and for features/properties
- * when requesting setting unimplemented feature/property
- * (UnsupportedOperationException would be better but it is not in MIDP)
- *
- *
- *
- * **NOTE:** writing  CDSECT, ENTITY_REF, IGNORABLE_WHITESPACE,
- * PROCESSING_INSTRUCTION, COMMENT, and DOCDECL in some implementations
- * may not be supported (for example when serializing to WBXML).
- * In such case IllegalStateException will be thrown and it is recommended
- * to use an optional feature to signal that implementation is not
- * supporting this kind of output.
+ * Defines an interface to serialization of an XML Infoset.
  */
 interface XmlStreamWriter {
 
     /**
-     * Write &lt;&#63;xml declaration with encoding (if encoding not null)
+     * Write xml declaration with encoding (if encoding not null)
      * and standalone flag (if standalone not null)
      * This method can only be called just after setOutput.
      */
@@ -77,12 +50,6 @@ interface XmlStreamWriter {
 
     /**
      * Write end tag. Repetition of namespace and name is just for avoiding errors.
-     *
-     * **Background:** in kXML endTag had no arguments, and non matching tags were
-     * very difficult to find...
-     * If namespace is null no namespace prefix is printed but just name.
-     * If namespace is empty string then serializer will make sure that
-     * default empty namespace is declared (in XML 1.0 xmlns='').
      */
     fun endTag(name: String, namespace: String? = null): XmlStreamWriter
 
