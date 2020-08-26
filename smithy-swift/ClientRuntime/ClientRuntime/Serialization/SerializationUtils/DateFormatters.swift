d77aa8fb4b86d555762fbe9858ca1fd3a9ebbb63
phani-srikar <phanisrikar93ume@gmail.com>
2020-08-26 13:51:16 -0700

    fix: remove AWSDate custom type: to be handled using custom Encodable/Decodable implementations

--- smithy-swift/ClientRuntime/ClientRuntime/Serialization/SerializationUtils/DateFormatters.swift
+++ smithy-swift/ClientRuntime/ClientRuntime/Serialization/SerializationUtils/DateFormatters.swift
@@ -20,32 +20,31 @@ extension DateFormatter {
     Configures RFC 5322(822) Date Formatter
     https://tools.ietf.org/html/rfc7231.html#section-7.1.1.1
     */
-    public static let rfc5322DateFormatter: DateFormatter = getDateFormatter(dateFormat: "EE, dd MMM yyyy HH:mm:ss zzz",
-                                                                             timeZone: TimeZone(abbreviation: "GMT"))
+    public static let rfc5322DateFormatter: DateFormatter =
+        getDateFormatter(
+            dateFormat: "EE, dd MMM yyyy HH:mm:ss zzz",
+            timeZone: TimeZone(secondsFromGMT:0)
+    )
     
     /*
     Configures ISO 8601 Date Formatter With Fractional Seconds
     https://xml2rfc.tools.ietf.org/public/rfc/html/rfc3339.html#anchor14
     */
-    public static let iso8601DateFormatterWithFractionalSeconds: DateFormatter = getDateFormatter(dateFormat: "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
-                                                                                                  timeZone: TimeZone(abbreviation: "GMT"))
-    
-    
+    public static let iso8601DateFormatterWithFractionalSeconds =
+        getDateFormatter(
+            dateFormat: "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
+            timeZone: TimeZone(secondsFromGMT:0)
+    )
     
     /*
     Configures default ISO 8601 Date Formatter
     https://xml2rfc.tools.ietf.org/public/rfc/html/rfc3339.html#anchor14
     */
-    public static let iso8601DateFormatterWithoutFractionalSeconds: DateFormatter = getDateFormatter(dateFormat: "yyyy-MM-dd'T'HH:mm:ssZ",
-                                                                                                     timeZone: TimeZone(abbreviation: "GMT"))
-    
-    /*
-    Configures an Epoch Seconds based formatter.
-    Based on the number of seconds that have elapsed since 00:00:00 Coordinated Universal Time (UTC), Thursday, 1 January 1970
-    */
-    public static let epochSecondsDateFormatter: EpochSecondsDateFormatter = {
-        return EpochSecondsDateFormatter()
-    }()
+    public static let iso8601DateFormatterWithoutFractionalSeconds: DateFormatter =
+        getDateFormatter(
+            dateFormat: "yyyy-MM-dd'T'HH:mm:ssZ",
+            timeZone: TimeZone(secondsFromGMT:0)
+    )
     
     private static func getDateFormatter(dateFormat: String, timeZone: TimeZone? = nil) -> DateFormatter {
         let formatter = DateFormatter()
@@ -54,45 +53,20 @@ extension DateFormatter {
         formatter.timeZone = timeZone
         return formatter
     }
-    
-    public func string(from date: ISO8601Date) -> String {
-        return string(from: date.value)
-    }
-
-    public func string(from date: RFC5322Date) -> String {
-        return string(from: date.value)
-    }
-
-    public func string(from date: EpochSecondsDate) -> String {
-        return string(from: date.value)
-    }
 }
 
-/*
- Configures an Epoch Seconds based formatter.
- Based on the number of seconds that have elapsed since 00:00:00 Coordinated Universal Time (UTC), Thursday, 1 January 1970
- */
-public class EpochSecondsDateFormatter: DateFormatter {
-    public override func date(from string: String) -> Date? {
-        guard let double = Double(string) else {
-            return nil
-        }
-        return date(from: double)
-    }
-    
-    public override func string(from date: Date) -> String {
-        return String(date.timeIntervalSince1970)
-    }
-    
-    public func date(from double: Double) -> Date? {
-        return Date(timeIntervalSince1970: double)
-    }
-    
-    public func double(from date: Date) -> Double {
-        return date.timeIntervalSince1970
-    }
-}
 
-enum DateDecodingError: Error {
-    case parseError
+extension Date {
+    func iso8601FractionalSecondsString() -> String {
+        let formatter = DateFormatter.iso8601DateFormatterWithFractionalSeconds
+        return formatter.string(from: self)
+    }
+    func iso8601WithoutFractionalSecondsString() -> String {
+        let formatter = DateFormatter.iso8601DateFormatterWithoutFractionalSeconds
+        return formatter.string(from: self)
+    }
+    func rfc5322String() -> String {
+        let formatter = DateFormatter.rfc5322DateFormatter
+        return formatter.string(from: self)
+    }
 }
