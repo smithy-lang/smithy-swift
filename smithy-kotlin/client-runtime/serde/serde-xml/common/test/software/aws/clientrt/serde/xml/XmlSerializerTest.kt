0a623c408e235edd4f9e52af12c7e19c0628dc63
Ken Gilmer <kggilmer@amazon.com>
2020-08-31 15:43:34 -0700

    Misc test cleanup

--- smithy-kotlin/client-runtime/serde/serde-xml/common/test/software/aws/clientrt/serde/xml/XmlSerializerTest.kt
+++ smithy-kotlin/client-runtime/serde/serde-xml/common/test/software/aws/clientrt/serde/xml/XmlSerializerTest.kt
@@ -153,6 +153,10 @@ class XmlSerializerTest {
     @Test
     fun `can serialize all primitives`() {
         val xml = XmlSerializer()
+        val data = Primitives(
+            true, 10, 20, 30, 40, 50f, 60.0, 'A', "Str0",
+            listOf(1, 2, 3)
+        )
         data.serialize(xml)
 
         assertEquals("""<struct><boolean>true</boolean><byte>10</byte><short>20</short><int>30</int><long>40</long><float>50.0</float><double>60.0</double><char>A</char><string>Str0</string><listInt><number>1</number><number>2</number><number>3</number></listInt></struct>""", xml.toByteArray().decodeToString())
@@ -209,8 +213,3 @@ data class Primitives(
         }
     }
 }
-
-val data = Primitives(
-    true, 10, 20, 30, 40, 50f, 60.0, 'A', "Str0",
-    listOf(1, 2, 3)
-)
