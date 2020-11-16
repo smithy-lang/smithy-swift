632f361bd0c112dd303408e3824440c9f77be8ce
Adireddy <sadiredd@3c22fb12605c.ant.amazon.com>
2020-11-16 10:45:10 -0800

    removed smithy-kotlin repo

--- smithy-kotlin/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "smithy-kotlin"
-enableFeaturePreview("GRADLE_METADATA")
-
-include(":smithy-kotlin-codegen")
-include(":smithy-kotlin-codegen-test")
-
-include(":client-runtime")
-include(":client-runtime:aws-smithy-client-rt-core")
