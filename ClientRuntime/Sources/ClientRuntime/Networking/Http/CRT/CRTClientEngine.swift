b2fa1ab9c06fdcc296d20c8ba8d59551fe1a8389
Nicki Stone <nickik@amazon.com>
2020-11-18 19:21:23 -0800

    fix: fix stream to have input and output cuz im dumb

--- ClientRuntime/Sources/ClientRuntime/Networking/Http/CRT/CRTClientEngine.swift
+++ ClientRuntime/Sources/ClientRuntime/Networking/Http/CRT/CRTClientEngine.swift
@@ -207,7 +207,7 @@ class CRTClientEngine: HttpClientEngine {
             if case let CRTError.crtError(unwrappedError) = error {
                 if unwrappedError.errorCode != 0 {
                     logger.error("Response encountered an error: \(error)")
-                    if let streamClosure = stream.streamResponse {
+                    if let streamClosure = stream?.streamResponse {
                         streamClosure(.errorOccurred, incomingByteBuffer, StreamErrors.unknown(error))
                     }
                     future.fail(error)
