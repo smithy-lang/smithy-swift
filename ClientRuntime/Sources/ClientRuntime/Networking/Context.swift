0ca250d49a6edd014429e1fcfc94ac82fc358bf9
Nicki Stone <nickik@amazon.com>
2020-11-19 13:49:41 -0800

    save new context file

---
+++ ClientRuntime/Sources/ClientRuntime/Networking/Context.swift
@@ -0,0 +1,23 @@
+//
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+// http://aws.amazon.com/apache2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+
+public struct Context<Output> {
+    let encoder: RequestEncoder
+    let decoder: ResponseDecoder
+    let outputType: Output
+    let operation: String
+    let serviceName: String
+    let request: SdkHttpRequest
+}
