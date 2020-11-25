ba21e987ee8309abf9e4c0c3c7695d24806b651f
Nicki Stone <nickik@amazon.com>
2020-11-25 12:03:18 -0800

    now generating config for white label sdk

---
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/ConfigField.kt
@@ -0,0 +1,9 @@
+package software.amazon.smithy.swift.codegen.integration
+
+import software.amazon.smithy.codegen.core.Symbol
+import software.amazon.smithy.utils.ToSmithyBuilder
+
+/**
+ * Represents a config field on a client config struct.
+ */
+data class ConfigField(val name: String?, val type: Symbol, private val documentation: String?)
