1e27f6f2d1d258f24b0712d2deb617d68167c9b4
Nicki Stone <nickik@amazon.com>
2020-12-01 12:47:22 -0800

    refactored to remove all section use

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/ConfigField.kt
+++
@@ -1,9 +0,0 @@
-package software.amazon.smithy.swift.codegen.integration
-
-import software.amazon.smithy.codegen.core.Symbol
-import software.amazon.smithy.utils.ToSmithyBuilder
-
-/**
- * Represents a config field on a client config struct.
- */
-data class ConfigField(val name: String?, val type: String, private val documentation: String?)
