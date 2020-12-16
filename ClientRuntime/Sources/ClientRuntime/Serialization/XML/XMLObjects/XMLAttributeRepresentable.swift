1d76046bb153f434d5a8a0c9ff6d54c5dc467911
Adireddy <sadiredd@3c22fb12605c.ant.amazon.com>
2020-12-16 10:46:36 -0800

    XML objects and parser reverted

--- ClientRuntime/Sources/ClientRuntime/Serialization/XML/XMLObjects/XMLAttributeRepresentable.swift
+++ ClientRuntime/Sources/ClientRuntime/Serialization/XML/XMLObjects/XMLAttributeRepresentable.swift
@@ -1,10 +1,9 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
- */
-
+//
 //  XMLAttributeRepresentable.swift
 //  AWSXMLParser
+//
+// TODO:: Add copyrights
+//
 
 import Foundation
 
