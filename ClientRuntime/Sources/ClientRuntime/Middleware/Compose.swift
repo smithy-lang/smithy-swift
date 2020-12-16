3629034c7dd8ad2bc3ca5011728f3adbcfe8ca77
Nicki Stone <nickik@amazon.com>
2020-12-16 14:52:31 -0800

    fix copyright headers

--- ClientRuntime/Sources/ClientRuntime/Middleware/Compose.swift
+++ ClientRuntime/Sources/ClientRuntime/Middleware/Compose.swift
@@ -1,5 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- // SPDX-License-Identifier: Apache-2.0.
+// SPDX-License-Identifier: Apache-2.0.
 
 /// Compose (wrap) the handler with the given middleware
 func compose<TContext: Any, TSubject: Any, TError, H: Handler, M: Middleware> (
