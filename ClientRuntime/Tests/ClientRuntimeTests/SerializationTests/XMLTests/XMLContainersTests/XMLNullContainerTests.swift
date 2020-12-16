25aa190ce13acfa474fd934e3566608353a19754
Adireddy <sadiredd@3c22fb12605c.ant.amazon.com>
2020-12-16 11:03:19 -0800

    XMLContainerTests files reverted

--- ClientRuntime/Tests/ClientRuntimeTests/SerializationTests/XMLTests/XMLContainersTests/XMLNullContainerTests.swift
+++ ClientRuntime/Tests/ClientRuntimeTests/SerializationTests/XMLTests/XMLContainersTests/XMLNullContainerTests.swift
@@ -1,7 +1,4 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
- */
+// TODO:: Add copyrights
 
 import XCTest
 @testable import ClientRuntime
