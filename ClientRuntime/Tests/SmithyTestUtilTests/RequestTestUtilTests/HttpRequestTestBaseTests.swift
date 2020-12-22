1f64ffaa5dec193f1a43b2cdb367aef8e42bacab
Adireddy <sadiredd@3c22fb12605c.ant.amazon.com>
2020-12-22 13:26:52 -0800

    fix compile errors in ClientRunTime

--- ClientRuntime/Tests/SmithyTestUtilTests/RequestTestUtilTests/HttpRequestTestBaseTests.swift
+++ ClientRuntime/Tests/SmithyTestUtilTests/RequestTestUtilTests/HttpRequestTestBaseTests.swift
@@ -11,7 +11,7 @@ class HttpRequestTestBaseTests: HttpRequestTestBase {
     static let host = "myapi.host.com"
     
     struct SayHelloInput: Encodable, HttpRequestBinding {
-        func buildHttpRequest(method: HttpMethodType, path: String, encoder: RequestEncoder, idempotencyTokenGenerator: IdempotencyTokenGeneratorProtocol = DefaultIdempotencyTokenGenerator()) throws -> SdkHttpRequest {
+        func buildHttpRequest(method: HttpMethodType, path: String, encoder: RequestEncoder, idempotencyTokenGenerator: IdempotencyTokenGenerator = DefaultIdempotencyTokenGenerator()) throws -> SdkHttpRequest {
             var queryItems: [URLQueryItem] = [URLQueryItem]()
             var queryItem: URLQueryItem
             if let requiredQuery = requiredQuery {
