71a8be508d5f9583b4b341e31085c0a2122e76e3
Nicki Stone <nickik@amazon.com>
2020-12-29 11:38:23 -0800

    add comments

--- ClientRuntime/Sources/ClientRuntime/Middleware/MiddlewareStackStep.swift
+++ ClientRuntime/Sources/ClientRuntime/Middleware/MiddlewareStackStep.swift
@@ -28,12 +28,17 @@ struct MiddlewareStackStep<StepInput, StepOutput>: Middleware {
             //last link in the stack needs to be called and then next inside this link needs to be called with its result.
             switch position {
             case .before:
+                //call the stack which will run it to completion through the middleware to the handler given for the step and back up
                 let stepOutput = stack.handle(context: context, input: sinput, next: handler)
+                // take the output of the stack and convert it to then call next on the next step
                 return stepOutput.flatMap { (nextStepInput) -> Result<MInput, Error> in
                     return next.handle(context: context, input: nextStepInput)
                 }
             case .after:
+                // call the handler first since is the end of the chain
                 let result = next.handle(context: context, input: input)
+                //cast the success result (i.e. http response) to an http respoonse and add it to the context
+                // then call the deserialize stack essentially or the last step in the middleware.
                 return result.flatMap { (response) -> Result<MInput, Error> in
                     var copiedContext = context
                     copiedContext.response = response as? HttpResponse
