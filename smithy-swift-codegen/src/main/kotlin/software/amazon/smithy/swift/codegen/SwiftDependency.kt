3f159bba0a79a6a1bb0b1dca3835fcd244094bad
John Woo <wooj@amazon.com>
2021-01-20 22:17:48 -0800

    fix: path generation for local dev

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/SwiftDependency.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/SwiftDependency.kt
@@ -7,13 +7,25 @@ package software.amazon.smithy.swift.codegen
 
 import software.amazon.smithy.codegen.core.SymbolDependency
 import software.amazon.smithy.codegen.core.SymbolDependencyContainer
+import java.io.File
 
 enum class SwiftDependency(val type: String, val namespace: String, val version: String, val url: String, var packageName: String) : SymbolDependencyContainer {
     // Note: "namespace" is sub module in the full library "packageName". We use the namespace to minimize the module import. But, the entire package is "packageName"
     BIG("", "ComplexModule", "0.0.5", "https://github.com/apple/swift-numerics", packageName = "swift-numerics"),
-    CLIENT_RUNTIME("", "ClientRuntime", "0.1.0", "../../../../../../ClientRuntime", "ClientRuntime"),
+    CLIENT_RUNTIME(
+        "",
+        "ClientRuntime",
+        "0.1.0",
+        computeAbsolutePath("smithy-swift/ClientRuntime"),
+        "ClientRuntime"
+    ),
     XCTest("", "XCTest", "", "", ""),
-    SMITHY_TEST_UTIL("", "SmithyTestUtil", "0.1.0", "../../../../../../ClientRuntime", "ClientRuntime");
+    SMITHY_TEST_UTIL(
+        "",
+        "SmithyTestUtil",
+        "0.1.0",
+        computeAbsolutePath("smithy-swift/ClientRuntime"),
+        "ClientRuntime");
 
     override fun getDependencies(): List<SymbolDependency> {
         val dependency = SymbolDependency.builder()
@@ -25,4 +37,17 @@ enum class SwiftDependency(val type: String, val namespace: String, val version:
             .build()
         return listOf(dependency)
     }
+}
+
+private fun computeAbsolutePath(relativePath: String): String {
+    var userDirPath = System.getProperty("user.dir")
+    while (userDirPath.isNotEmpty()) {
+        val fileName = userDirPath.removeSuffix("/") + "/" + relativePath
+        val file = File(fileName)
+        if (file.isDirectory) {
+            return fileName
+        }
+        userDirPath = userDirPath.substring(0, userDirPath.length-1)
+    }
+    return ""
 }
