002ad95daf3720157b3f7a5cc2460e1bbf0b1f0a
John Woo <wooj@amazon.com>
2021-02-04 21:17:20 -0800

    fix: stop generation of response binding for awsjson1.x

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/ErrorFromHttpResponseGenerator.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/ErrorFromHttpResponseGenerator.kt
@@ -5,3 +5,8 @@ import software.amazon.smithy.model.shapes.OperationShape
 interface ErrorFromHttpResponseGenerator {
     fun generateInitOperationFromHttpResponse(ctx: ProtocolGenerator.GenerationContext, op: OperationShape)
 }
+
+class EmptyErrorFromHttpResponseGenerator : ErrorFromHttpResponseGenerator {
+    override fun generateInitOperationFromHttpResponse(ctx: ProtocolGenerator.GenerationContext, op: OperationShape) {
+    }
+}
