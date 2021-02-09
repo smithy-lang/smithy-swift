36742a8eb712a4823b918b3a371c6dece90f8fff
Nicki Stone <nickik@amazon.com>
2021-02-08 18:46:26 -0800

    moved content type middleware to client runtime

---
+++ ClientRuntime/Sources/ClientRuntime/Networking/Http/DefaultMiddlewares/ContentTypeMiddleware.swift
@@ -0,0 +1,29 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0.
+
+public struct ContentTypeMiddleware<StackInput>: Middleware {
+    public let id: String = "ContentType"
+
+    let contentType: String
+
+    public init(contentType: String) {
+        self.contentType = contentType
+    }
+
+    public func handle<H>(context: Context,
+                          input: SerializeInput<StackInput>,
+                          next: H) -> Result<SdkHttpRequestBuilder, Error>
+    where H: Handler,
+          Self.MInput == H.Input,
+          Self.MOutput == H.Output,
+          Self.Context == H.Context {
+
+        input.builder.withHeader(name: "Content-Type", value: contentType)
+
+        return next.handle(context: context, input: input)
+    }
+
+    public typealias MInput = SerializeInput<StackInput>
+    public typealias MOutput = SdkHttpRequestBuilder
+    public typealias Context = HttpContext
+}
