e7eb685d4ea19c4fee74613c62dfb03520b3ae2f
Nicki Stone <nickik@amazon.com>
2021-02-17 09:45:00 -0800

    move package.swift to root of the repo for branch dependency managmeent

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ClientRuntime",
+    platforms: [
+    .macOS(.v10_15),
+    .iOS(.v13)
+    ],
+    products: [
+        .library(name: "ClientRuntime", targets: ["ClientRuntime"]),
+        .library(name: "SmithyTestUtil", targets: ["SmithyTestUtil"])
+    ],
+    dependencies: [
+        .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift", .branch("master")),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(
+            name: "ClientRuntime",
+            dependencies: [
+                .product(name: "AwsCommonRuntimeKit", package: "AwsCrt"),
+                .product(name: "Logging", package: "swift-log")
+            ],
+            path: "./ClientRuntime"
+        ),
+        .testTarget(
+            name: "ClientRuntimeTests",
+            dependencies: [
+	        "ClientRuntime",
+	        "SmithyTestUtil"
+	    ],
+	    path: "./ClientRuntime"
+        ),
+        .target(
+            name: "SmithyTestUtil",
+            dependencies: ["ClientRuntime"],
+            path: "./ClientRuntime"
+        ),
+        .testTarget(
+            name: "SmithyTestUtilTests",
+            dependencies: ["SmithyTestUtil"],
+            path: "./ClientRuntime"
+        )
+    ]
+)
