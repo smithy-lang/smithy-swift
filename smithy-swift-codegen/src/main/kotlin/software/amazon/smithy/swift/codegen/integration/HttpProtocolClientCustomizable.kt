ca7f8d2efd6eab8c0e7d412e69ed048e3dd25f68
John Woo <wooj@amazon.com>
2021-02-17 20:16:53 -0800

    setting default implementations

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/HttpProtocolClientCustomizable.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/HttpProtocolClientCustomizable.kt
@@ -6,8 +6,10 @@ import software.amazon.smithy.protocoltests.traits.HttpRequestTestCase
 import software.amazon.smithy.swift.codegen.SwiftWriter
 
 abstract class HttpProtocolClientCustomizable {
-    abstract fun renderMiddlewares(ctx: ProtocolGenerator.GenerationContext, writer: SwiftWriter, op: OperationShape, operationStackName: String)
-    abstract fun renderSerializeMiddleware(
+    open fun renderMiddlewares(ctx: ProtocolGenerator.GenerationContext, writer: SwiftWriter, op: OperationShape, operationStackName: String) {
+        // Default implementation is no-op
+    }
+    open fun renderSerializeMiddleware(
         writer: SwiftWriter,
         test: HttpRequestTestCase,
         operationStack: String,
@@ -15,7 +17,8 @@ abstract class HttpProtocolClientCustomizable {
         outputSymbol: Symbol,
         outputErrorName: String,
         hasHttpBody: Boolean
-    )
+    ) {
+        // Default implementation is no-op
+    }
     abstract fun renderContextAttributes(ctx: ProtocolGenerator.GenerationContext, writer: SwiftWriter, op: OperationShape)
-    abstract fun addImport(writer: SwiftWriter)
 }
