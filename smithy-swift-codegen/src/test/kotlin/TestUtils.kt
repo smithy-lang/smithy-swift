9631e26ffc7d95d670ea6a9442a2990ee123114b
John Woo <wooj@amazon.com>
2021-03-08 16:03:59 -0800

    chore: enable unit tests on ci

--- smithy-swift-codegen/src/test/kotlin/TestUtils.kt
+++ smithy-swift-codegen/src/test/kotlin/TestUtils.kt
@@ -343,7 +343,7 @@ fun getModelFileContents(namespace: String, filename: String, manifest: MockMani
 }
 
 fun getTestFileContents(namespace: String, filename: String, manifest: MockManifest): String {
-    return getFileContents(manifest, "${namespace}Tests/$filename")
+    return getFileContents(manifest, "${namespace}FAILFAILTests/$filename")
 }
 
 fun getFileContents(manifest: MockManifest, fileName: String): String {
