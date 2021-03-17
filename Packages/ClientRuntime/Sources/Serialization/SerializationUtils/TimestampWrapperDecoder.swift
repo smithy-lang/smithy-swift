0817b4bc1bf1ede22286b98266948fa841942c63
John Woo <wooj@amazon.com>
2021-03-17 14:39:34 -0700

    defaulting logic

--- Packages/ClientRuntime/Sources/Serialization/SerializationUtils/TimestampWrapperDecoder.swift
+++ Packages/ClientRuntime/Sources/Serialization/SerializationUtils/TimestampWrapperDecoder.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 struct TimestampWrapperDecoder {
-    static func parseDateStringValue(_ dateStringValue: String, format: TimestampFormat, codingKey: CodingKey) throws -> Date {
+    static func parseDateStringValue(_ dateStringValue: String,
+                                     format: TimestampFormat,
+                                     codingPath: [CodingKey]? = nil) throws -> Date {
         let formatter: DateFormatter?
         switch format {
         case .epochSeconds:
@@ -20,7 +22,7 @@ struct TimestampWrapperDecoder {
         }
 
         guard let formattedDate = formatter!.date(from: dateStringValue) else {
-            let context = DecodingError.Context(codingPath: [codingKey],
+            let context = DecodingError.Context(codingPath: codingPath ?? [],
                                                 debugDescription: "Unable to parse: \(dateStringValue) to format \(format)")
             throw DecodingError.dataCorrupted(context)
         }
