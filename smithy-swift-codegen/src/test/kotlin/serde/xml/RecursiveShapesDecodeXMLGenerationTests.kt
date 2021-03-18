10c6074a2fdd37e0a2a554a5a8c6727a4c272644
John Woo <wooj@amazon.com>
2021-03-18 16:17:51 -0700

    feat: serde for recursive shapes, non-nested

--- smithy-swift-codegen/src/test/kotlin/serde/xml/RecursiveShapesDecodeXMLGenerationTests.kt
+++ smithy-swift-codegen/src/test/kotlin/serde/xml/RecursiveShapesDecodeXMLGenerationTests.kt
@@ -5,25 +5,34 @@ import TestContext
 import defaultSettings
 import getFileContents
 import io.kotest.matchers.string.shouldContainOnlyOnce
-import listFilesFromManifest
 import org.junit.jupiter.api.Test
 
 class RecursiveShapesDecodeXMLGenerationTests {
     @Test
     fun `decode recursive shape`() {
-        val context = setupTests("Isolated/Restxml/xml-enums.smithy", "aws.protocoltests.restxml#RestXml")
-        print(listFilesFromManifest(context.manifest))
-        val contents = getFileContents(context.manifest, "/example/models/XmlEnumsInput+Encodable.swift")
+        val context = setupTests("Isolated/Restxml/xml-recursive.smithy", "aws.protocoltests.restxml#RestXml")
+        val contents = getFileContents(context.manifest, "/example/models/XmlRecursiveShapesOutputBody+Decodable.swift")
         val expectedContents =
             """
-
+            extension XmlRecursiveShapesOutputBody: Decodable {
+                private enum CodingKeys: String, CodingKey {
+                    case nested
+                }
+            
+                public init (from decoder: Decoder) throws {
+                    let containerValues = try decoder.container(keyedBy: CodingKeys.self)
+                    let nestedDecoded = try containerValues.decodeIfPresent(RecursiveShapesInputOutputNested1.self, forKey: .nested)
+                    nested = nestedDecoded
+                }
+            }
             """.trimIndent()
         contents.shouldContainOnlyOnce(expectedContents)
     }
 
     @Test
     fun `decode recursive nested shape`() {
-        val context = setupTests("Isolated/Restxml/xml-enums.smithy", "aws.protocoltests.restxml#RestXml")
+        /*
+        val context = setupTests("Isolated/Restxml/xml-recursive.smithy", "aws.protocoltests.restxml#RestXml")
         print(listFilesFromManifest(context.manifest))
         val contents = getFileContents(context.manifest, "/example/models/XmlEnumsInput+Encodable.swift")
         val expectedContents =
@@ -31,6 +40,7 @@ class RecursiveShapesDecodeXMLGenerationTests {
 
             """.trimIndent()
         contents.shouldContainOnlyOnce(expectedContents)
+         */
     }
 
     private fun setupTests(smithyFile: String, serviceShapeId: String): TestContext {
--- smithy-swift-codegen/src/test/kotlin/serde/xml/RecursiveShapesEncodeXMLGenerationTests.kt
+++ smithy-swift-codegen/src/test/kotlin/serde/xml/RecursiveShapesEncodeXMLGenerationTests.kt
@@ -5,22 +5,76 @@ import TestContext
 import defaultSettings
 import getFileContents
 import io.kotest.matchers.string.shouldContainOnlyOnce
-import listFilesFromManifest
 import org.junit.jupiter.api.Test
 
 class RecursiveShapesEncodeXMLGenerationTests {
     @Test
-    fun `encode recursive shape`() {
-        val context = setupTests("Isolated/Restxml/xml-enums.smithy", "aws.protocoltests.restxml#RestXml")
-        print(listFilesFromManifest(context.manifest))
-        val contents = getFileContents(context.manifest, "/example/models/XmlEnumsInput+Encodable.swift")
+    fun `encode recursive shape Nested1`() {
+        val context = setupTests("Isolated/Restxml/xml-recursive.smithy", "aws.protocoltests.restxml#RestXml")
+        val contents = getFileContents(context.manifest, "/example/models/RecursiveShapesInputOutputNested1+Codable.swift")
         val expectedContents =
             """
-
+            extension RecursiveShapesInputOutputNested1: Codable, Reflection {
+                private enum CodingKeys: String, CodingKey {
+                    case foo
+                    case nested
+                }
+            
+                public func encode(to encoder: Encoder) throws {
+                    var container = encoder.container(keyedBy: CodingKeys.self)
+                    if let foo = foo {
+                        try container.encode(foo, forKey: .foo)
+                    }
+                    if let nested = nested {
+                        try container.encode(nested, forKey: .nested)
+                    }
+                }
+            
+                public init (from decoder: Decoder) throws {
+                    let containerValues = try decoder.container(keyedBy: CodingKeys.self)
+                    let fooDecoded = try containerValues.decodeIfPresent(String.self, forKey: .foo)
+                    foo = fooDecoded
+                    let nestedDecoded = try containerValues.decodeIfPresent(Box<RecursiveShapesInputOutputNested2>.self, forKey: .nested)
+                    nested = nestedDecoded
+                }
+            }
             """.trimIndent()
         contents.shouldContainOnlyOnce(expectedContents)
     }
 
+    @Test
+    fun `encode recursive shape Nested2`() {
+        val context = setupTests("Isolated/Restxml/xml-recursive.smithy", "aws.protocoltests.restxml#RestXml")
+        val contents = getFileContents(context.manifest, "/example/models/RecursiveShapesInputOutputNested2+Codable.swift")
+        val expectedContents =
+            """
+            extension RecursiveShapesInputOutputNested2: Codable, Reflection {
+                private enum CodingKeys: String, CodingKey {
+                    case bar
+                    case recursiveMember
+                }
+            
+                public func encode(to encoder: Encoder) throws {
+                    var container = encoder.container(keyedBy: CodingKeys.self)
+                    if let bar = bar {
+                        try container.encode(bar, forKey: .bar)
+                    }
+                    if let recursiveMember = recursiveMember {
+                        try container.encode(recursiveMember, forKey: .recursiveMember)
+                    }
+                }
+            
+                public init (from decoder: Decoder) throws {
+                    let containerValues = try decoder.container(keyedBy: CodingKeys.self)
+                    let barDecoded = try containerValues.decodeIfPresent(String.self, forKey: .bar)
+                    bar = barDecoded
+                    let recursiveMemberDecoded = try containerValues.decodeIfPresent(RecursiveShapesInputOutputNested1.self, forKey: .recursiveMember)
+                    recursiveMember = recursiveMemberDecoded
+                }
+            }""".trimIndent()
+
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
     @Test
     fun `encode recursive nested shape`() {
         /*
