aebb91c1158ffa064f5818d088ede539b90bb1bc
Jean Ro <frimfram@amazon.com>
2021-04-08 18:51:19 -0700

    feat: support for enum cases with Swift reserved words (#183)

---
+++ smithy-swift-codegen/src/test/kotlin/ReservedWordsGeneratorTests.kt
@@ -0,0 +1,60 @@
+
+import io.kotest.matchers.string.shouldContainOnlyOnce
+import org.junit.jupiter.api.Test
+
+class ReservedWordsGeneratorTests {
+    @Test
+    fun `test enum`() {
+        val context = setupTests("reserved-name-enum-test.smithy", "com.test#Example")
+        val contents = getFileContents(context.manifest, "/example/models/ReservedWordsEnum.swift")
+        val expectedContents =
+            """
+                public enum ReservedWordsEnum {
+                    case any
+                    case `open`
+                    case `self`
+                    case `protocol`
+                    case sdkUnknown(String)
+                }
+
+                extension ReservedWordsEnum : Equatable, RawRepresentable, Codable, CaseIterable, Hashable {
+                    public static var allCases: [ReservedWordsEnum] {
+                        return [
+                            .any,
+                            .open,
+                            .self,
+                            .protocol,
+                            .sdkUnknown("")
+                        ]
+                    }
+                    public init?(rawValue: String) {
+                        let value = Self.allCases.first(where: { ${'$'}0.rawValue == rawValue })
+                        self = value ?? Self.sdkUnknown(rawValue)
+                    }
+                    public var rawValue: String {
+                        switch self {
+                        case .any: return "Any"
+                        case .open: return "OPEN"
+                        case .self: return "Self"
+                        case .protocol: return "PROTOCOL"
+                        case let .sdkUnknown(s): return s
+                        }
+                    }
+                    public init(from decoder: Decoder) throws {
+                        let container = try decoder.singleValueContainer()
+                        let rawValue = try container.decode(RawValue.self)
+                        self = ReservedWordsEnum(rawValue: rawValue) ?? ReservedWordsEnum.sdkUnknown(rawValue)
+                    }
+                }
+            """.trimIndent()
+
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
+
+    private fun setupTests(smithyFile: String, serviceShapeId: String): TestContext {
+        val context = TestContext.initContextFrom(smithyFile, serviceShapeId)
+        context.generator.generateSerializers(context.generationCtx)
+        context.generationCtx.delegator.flushWriters()
+        return context
+    }
+}
---
+++ smithy-swift-codegen/src/test/resources/reserved-name-enum-test.smithy
@@ -0,0 +1,50 @@
+$version: "1.0"
+namespace com.test
+
+use aws.protocols#restJson1
+use smithy.test#httpRequestTests
+use smithy.test#httpResponseTests
+
+@restJson1
+service Example {
+    version: "1.0.0",
+    operations: [
+            EnumInput
+    ]
+}
+
+@http(method: "POST", uri: "/input/enum")
+operation EnumInput {
+    input: EnumInputRequest
+}
+
+structure EnumInputRequest {
+    nestedWithEnum: NestedEnum,
+
+    @httpHeader("X-EnumHeader")
+    enumHeader: ReservedWordsEnum
+}
+
+structure NestedEnum {
+    myEnum: ReservedWordsEnum
+}
+
+@enum([
+    {
+        value: "PROTOCOL",
+        name: "protocol"
+    },
+    {
+        value: "OPEN",
+        name: "OPEN"
+    },
+    {
+        value: "Self",
+        name: "Self"
+    },
+    {
+        value: "Any",
+        name: "Any"
+    }
+])
+string ReservedWordsEnum
