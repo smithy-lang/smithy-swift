acc26cf2a2697ca0cc616cf4bd8605a61a3860c6
John Woo <wooj@amazon.com>
2021-05-05 16:02:01 -0700

    filter client tests only

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/HttpProtocolTestGenerator.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/HttpProtocolTestGenerator.kt
@@ -4,14 +4,18 @@
  */
 package software.amazon.smithy.swift.codegen.integration
 
+import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.model.knowledge.OperationIndex
 import software.amazon.smithy.model.knowledge.TopDownIndex
+import software.amazon.smithy.model.shapes.OperationShape
 import software.amazon.smithy.model.shapes.Shape
+import software.amazon.smithy.protocoltests.traits.AppliesTo
 import software.amazon.smithy.protocoltests.traits.HttpMessageTestCase
 import software.amazon.smithy.protocoltests.traits.HttpRequestTestsTrait
 import software.amazon.smithy.protocoltests.traits.HttpResponseTestsTrait
 import software.amazon.smithy.swift.codegen.SwiftDependency
 import software.amazon.smithy.swift.codegen.capitalizedName
+import software.amazon.smithy.swift.codegen.getOrNull
 import java.util.TreeSet
 import java.util.logging.Logger
 
@@ -39,58 +43,99 @@ class HttpProtocolTestGenerator(
         val serviceSymbol = ctx.symbolProvider.toSymbol(ctx.service)
 
         for (operation in TreeSet(topDownIndex.getContainedOperations(ctx.service).filterNot(::serverOnly))) {
+            renderRequestTests(operation, serviceSymbol)
+            renderResponseTests(operation, serviceSymbol)
+            renderErrorTestCases(operation, serviceSymbol, operationIndex)
+        }
+    }
 
-            // 1. Generate test cases for each request.
-            operation.getTrait(HttpRequestTestsTrait::class.java)
-                .ifPresent { trait: HttpRequestTestsTrait ->
-                    val testCases = filterProtocolTestCases(trait.testCases)
-                    if (testCases.isEmpty()) {
-                        return@ifPresent
-                    }
+    private fun renderRequestTests(operation: OperationShape, serviceSymbol: Symbol) {
+        val tempTestCases = operation.getTrait(HttpRequestTestsTrait::class.java)
+            .getOrNull()
+            ?.getTestCasesFor(AppliesTo.CLIENT)
+            .orEmpty()
+        val requestTestCases = filterProtocolTestCases(tempTestCases)
+        if (requestTestCases.isNotEmpty()) {
+            val testClassName = "${operation.capitalizedName()}RequestTest"
+            val testFilename = "./${ctx.settings.moduleName}Tests/$testClassName.swift"
+            ctx.delegator.useTestFileWriter(testFilename, ctx.settings.moduleName) { writer ->
+                LOGGER.fine("Generating request protocol test cases for ${operation.id}")
 
-                    val testClassName = "${operation.capitalizedName()}RequestTest"
-                    val testFilename = "./${ctx.settings.moduleName}Tests/$testClassName.swift"
-                    ctx.delegator.useTestFileWriter(testFilename, ctx.settings.moduleName) { writer ->
-                        LOGGER.fine("Generating request protocol test cases for ${operation.id}")
-                        // import dependencies
-                        writer.addImport(SwiftDependency.CLIENT_RUNTIME.target)
-                        writer.addImport(ctx.settings.moduleName, true)
-                        writer.addImport(SwiftDependency.SMITHY_TEST_UTIL.target)
-                        writer.addImport(SwiftDependency.XCTest.target)
+                writer.addImport(SwiftDependency.CLIENT_RUNTIME.target)
+                writer.addImport(ctx.settings.moduleName, true)
+                writer.addImport(SwiftDependency.SMITHY_TEST_UTIL.target)
+                writer.addImport(SwiftDependency.XCTest.target)
 
-                        requestTestBuilder
-                            .writer(writer)
-                            .model(ctx.model)
-                            .symbolProvider(ctx.symbolProvider)
-                            .operation(operation)
-                            .serviceName(serviceSymbol.name)
-                            .testCases(testCases)
-                            .httpProtocolCustomizable(httpProtocolCustomizable)
-                            .serdeContext(serdeContext)
-                            .build()
-                            .renderTestClass(testClassName)
-                    }
-                }
+                requestTestBuilder
+                    .writer(writer)
+                    .model(ctx.model)
+                    .symbolProvider(ctx.symbolProvider)
+                    .operation(operation)
+                    .serviceName(serviceSymbol.name)
+                    .testCases(requestTestCases)
+                    .httpProtocolCustomizable(httpProtocolCustomizable)
+                    .serdeContext(serdeContext)
+                    .build()
+                    .renderTestClass(testClassName)
+            }
+        }
+    }
 
-            // 2. Generate test cases for each response.
-            operation.getTrait(HttpResponseTestsTrait::class.java)
+    private fun renderResponseTests(operation: OperationShape, serviceSymbol: Symbol) {
+        val tempResponseTests = operation.getTrait(HttpResponseTestsTrait::class.java)
+            .getOrNull()
+            ?.getTestCasesFor(AppliesTo.CLIENT)
+            .orEmpty()
+        val responseTestCases = filterProtocolTestCases(tempResponseTests)
+        if (responseTestCases.isNotEmpty()) {
+            val testClassName = "${operation.id.name.capitalize()}ResponseTest"
+            val testFilename = "./${ctx.settings.moduleName}Tests/$testClassName.swift"
+            ctx.delegator.useTestFileWriter(testFilename, ctx.settings.moduleName) { writer ->
+                LOGGER.fine("Generating response protocol test cases for ${operation.id}")
+
+                writer.addImport(SwiftDependency.CLIENT_RUNTIME.target)
+                writer.addImport(ctx.settings.moduleName, true)
+                writer.addImport(SwiftDependency.SMITHY_TEST_UTIL.target)
+                writer.addImport(SwiftDependency.XCTest.target)
+
+                responseTestBuilder
+                    .writer(writer)
+                    .model(ctx.model)
+                    .symbolProvider(ctx.symbolProvider)
+                    .operation(operation)
+                    .serviceName(serviceSymbol.name)
+                    .testCases(responseTestCases)
+                    .httpProtocolCustomizable(httpProtocolCustomizable)
+                    .serdeContext(serdeContext)
+                    .build()
+                    .renderTestClass(testClassName)
+            }
+        }
+    }
+
+    private fun renderErrorTestCases(operation: OperationShape, serviceSymbol: Symbol, operationIndex: OperationIndex) {
+        for (error in operationIndex.getErrors(operation).filterNot(::serverOnly)) {
+            error.getTrait(HttpResponseTestsTrait::class.java)
                 .ifPresent { trait: HttpResponseTestsTrait ->
                     val testCases = filterProtocolTestCases(trait.testCases)
                     if (testCases.isEmpty()) {
                         return@ifPresent
                     }
-
-                    val testClassName = "${operation.id.name.capitalize()}ResponseTest"
-                    val testFilename = "./${ctx.settings.moduleName}Tests/$testClassName.swift"
+                    // multiple error (tests) may be associated with a single operation,
+                    // use the operation name + error name as the class name
+                    val opName = operation.id.name.capitalize()
+                    val testClassName = "${opName}${error.capitalizedName()}Test"
+                    val testFilename = "./${ctx.settings.moduleName}Tests/${opName}ErrorTest.swift"
                     ctx.delegator.useTestFileWriter(testFilename, ctx.settings.moduleName) { writer ->
-                        LOGGER.fine("Generating response protocol test cases for ${operation.id}")
-                        // import dependencies
+                        LOGGER.fine("Generating error protocol test cases for ${operation.id}")
+
                         writer.addImport(SwiftDependency.CLIENT_RUNTIME.target)
                         writer.addImport(ctx.settings.moduleName, true)
                         writer.addImport(SwiftDependency.SMITHY_TEST_UTIL.target)
                         writer.addImport(SwiftDependency.XCTest.target)
 
-                        responseTestBuilder
+                        errorTestBuilder
+                            .error(error)
                             .writer(writer)
                             .model(ctx.model)
                             .symbolProvider(ctx.symbolProvider)
@@ -103,43 +148,6 @@ class HttpProtocolTestGenerator(
                             .renderTestClass(testClassName)
                     }
                 }
-
-            // 3. Generate test cases for each error on each operation.
-            for (error in operationIndex.getErrors(operation).filterNot(::serverOnly)) {
-                error.getTrait(HttpResponseTestsTrait::class.java)
-                    .ifPresent { trait: HttpResponseTestsTrait ->
-                        val testCases = filterProtocolTestCases(trait.testCases)
-                        if (testCases.isEmpty()) {
-                            return@ifPresent
-                        }
-                        // multiple error (tests) may be associated with a single operation,
-                        // use the operation name + error name as the class name
-                        val opName = operation.id.name.capitalize()
-                        val testClassName = "${opName}${error.capitalizedName()}Test"
-                        val testFilename = "./${ctx.settings.moduleName}Tests/${opName}ErrorTest.swift"
-                        ctx.delegator.useTestFileWriter(testFilename, ctx.settings.moduleName) { writer ->
-                            LOGGER.fine("Generating error protocol test cases for ${operation.id}")
-                            // import dependencies
-                            writer.addImport(SwiftDependency.CLIENT_RUNTIME.target)
-                            writer.addImport(ctx.settings.moduleName, true)
-                            writer.addImport(SwiftDependency.SMITHY_TEST_UTIL.target)
-                            writer.addImport(SwiftDependency.XCTest.target)
-
-                            errorTestBuilder
-                                .error(error)
-                                .writer(writer)
-                                .model(ctx.model)
-                                .symbolProvider(ctx.symbolProvider)
-                                .operation(operation)
-                                .serviceName(serviceSymbol.name)
-                                .testCases(testCases)
-                                .httpProtocolCustomizable(httpProtocolCustomizable)
-                                .serdeContext(serdeContext)
-                                .build()
-                                .renderTestClass(testClassName)
-                        }
-                    }
-            }
         }
     }
 
