2fd42631239bfde570550f76350bd0c944acd8b8
Nicki Stone <nickik@amazon.com>
2021-05-25 17:32:14 -0700

    make linter happy

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/MiddlewarePosition.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/MiddlewarePosition.kt
@@ -2,7 +2,7 @@ package software.amazon.smithy.swift.codegen.integration
 
 enum class MiddlewarePosition {
     BEFORE {
-        override fun stringValue():String = ".before"
+        override fun stringValue(): String = ".before"
     },
     AFTER {
         override fun stringValue(): String = ".after"
