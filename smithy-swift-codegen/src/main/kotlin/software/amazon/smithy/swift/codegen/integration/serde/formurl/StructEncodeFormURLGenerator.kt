46b7bce07d25d5f199f1265659ddaa85de958791
John Woo <wooj@amazon.com>
2021-06-01 11:21:56 -0700

    feat: initial commit for formurl encoder

---
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/serde/formurl/StructEncodeFormURLGenerator.kt
@@ -0,0 +1,70 @@
+package software.amazon.smithy.swift.codegen.integration.serde.formurl
+
+import software.amazon.smithy.model.shapes.CollectionShape
+import software.amazon.smithy.model.shapes.MapShape
+import software.amazon.smithy.model.shapes.MemberShape
+import software.amazon.smithy.model.shapes.OperationShape
+import software.amazon.smithy.model.shapes.Shape
+import software.amazon.smithy.model.shapes.TimestampShape
+import software.amazon.smithy.model.traits.TimestampFormatTrait
+import software.amazon.smithy.swift.codegen.SwiftWriter
+import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
+import software.amazon.smithy.swift.codegen.model.ShapeMetadata
+
+class StructEncodeFormURLGenerator(
+    private val ctx: ProtocolGenerator.GenerationContext,
+    private val shapeContainingMembers: Shape,
+    private val shapeMetadata: Map<ShapeMetadata, Any>,
+    private val members: List<MemberShape>,
+    private val writer: SwiftWriter,
+    private val defaultTimestampFormat: TimestampFormatTrait.Format
+) : MemberShapeEncodeFormURLGenerator(ctx, writer, defaultTimestampFormat) {
+    override fun render() {
+        writer.openBlock("public func encode(to encoder: Encoder) throws {", "}") {
+            val containerName = "container"
+            renderEncodeBody(containerName)
+            addConstantMembers(containerName)
+        }
+    }
+
+    private fun renderEncodeBody(containerName: String) {
+        writer.write("var $containerName = encoder.container(keyedBy: Key.self)")
+
+        val membersSortedByName: List<MemberShape> = members.sortedBy { it.memberName }
+        membersSortedByName.forEach { member ->
+            renderSingleMember(member, containerName)
+        }
+    }
+
+    private fun renderSingleMember(member: MemberShape, containerName: String) {
+        val memberTarget = ctx.model.expectShape(member.target)
+        when (memberTarget) {
+
+            is CollectionShape -> {
+                // renderListMember(member, memberTarget, containerName)
+                // TODO
+            }
+            is MapShape -> {
+                // renderMapMember(member, memberTarget, containerName)
+                // TODO
+            }
+            is TimestampShape -> {
+                // renderTimestampMember(member, memberTarget, containerName)
+                // TODO
+            }
+            else -> {
+                renderScalarMember(member, memberTarget, containerName)
+            }
+        }
+    }
+
+    // TODO: Make this pluggable so that this code can exist in aws-sdk-swift
+    private fun addConstantMembers(containerName: String) {
+        if (shapeMetadata.containsKey(ShapeMetadata.OPERATION_SHAPE) && shapeMetadata.containsKey(ShapeMetadata.SERVICE_VERSION)) {
+            val operationShape = shapeMetadata[ShapeMetadata.OPERATION_SHAPE] as OperationShape
+            val version = shapeMetadata[ShapeMetadata.SERVICE_VERSION] as String
+            writer.write("try $containerName.encode(\"${operationShape.id.name}\", forKey:Key(\"Action\"))")
+            writer.write("try $containerName.encode(\"${version}\", forKey:Key(\"Version\"))")
+        }
+    }
+}
---
+++ smithy-swift-codegen/src/test/kotlin/serde/formurl/QueryIdempotencyTokenAutoFillGeneratorTests.kt
@@ -0,0 +1,46 @@
+package software.amazon.smithy.aws.swift.codegen.awsquery
+
+import TestContext
+import defaultSettings
+import getFileContents
+import io.kotest.matchers.string.shouldContainOnlyOnce
+import mocks.MockHttpAWSQueryProtocolGenerator
+import org.junit.jupiter.api.Test
+import shouldSyntacticSanityCheck
+
+class QueryIdempotencyTokenAutoFillGeneratorTests {
+    @Test
+    fun `001 hardcodes action and version into input type`() {
+        val context = setupTests("Isolated/FormUrl/query-idempotency-token.smithy", "aws.protocoltests.query#AwsQuery")
+        val contents = getFileContents(context.manifest, "/Example/models/QueryIdempotencyTokenAutoFillInput+Encodable.swift")
+        contents.shouldSyntacticSanityCheck()
+        val expectedContents =
+            """
+            extension QueryIdempotencyTokenAutoFillInput: Encodable, Reflection {
+                enum CodingKeys: String, CodingKey {
+                    case token
+                }
+            
+                public func encode(to encoder: Encoder) throws {
+                    var container = encoder.container(keyedBy: Key.self)
+                    if let token = token {
+                        try container.encode(token, forKey: Key("token"))
+                    }
+                    try container.encode("QueryIdempotencyTokenAutoFill", forKey:Key("Action"))
+                    try container.encode("2020-01-08", forKey:Key("Version"))
+                }
+            }
+            """.trimIndent()
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
+
+    private fun setupTests(smithyFile: String, serviceShapeId: String): TestContext {
+        val context = TestContext.initContextFrom(smithyFile, serviceShapeId, MockHttpAWSQueryProtocolGenerator()) { model ->
+            model.defaultSettings(serviceShapeId, "Example", "2020-01-08", "aws query protocol")
+        }
+        context.generator.generateCodableConformanceForNestedTypes(context.generationCtx)
+        context.generator.generateSerializers(context.generationCtx)
+        context.generationCtx.delegator.flushWriters()
+        return context
+    }
+}
