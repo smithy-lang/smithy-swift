7942707226da3a976e966818ddb1df49cf0d5d5f
John Woo <wooj@amazon.com>
2021-06-03 16:14:25 -0700

    fix: support awsQueryError trait

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/httpResponse/HttpResponseBindingErrorNarrowGenerator.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/httpResponse/HttpResponseBindingErrorNarrowGenerator.kt
@@ -1,11 +1,13 @@
 package software.amazon.smithy.swift.codegen.integration.httpResponse
 
+import software.amazon.smithy.aws.traits.protocols.AwsQueryErrorTrait
 import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.model.shapes.OperationShape
 import software.amazon.smithy.model.shapes.StructureShape
 import software.amazon.smithy.swift.codegen.ServiceGenerator
 import software.amazon.smithy.swift.codegen.SwiftDependency
 import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
+import software.amazon.smithy.swift.codegen.model.getTrait
 
 class HttpResponseBindingErrorNarrowGenerator(
     val ctx: ProtocolGenerator.GenerationContext,
@@ -31,13 +33,23 @@ class HttpResponseBindingErrorNarrowGenerator(
                 writer.openBlock("public init(errorType: String?, httpResponse: HttpResponse, decoder: ResponseDecoder? = nil, message: String? = nil, requestID: String? = nil) throws {", "}") {
                     writer.write("switch errorType {")
                     for (errorShape in errorShapes) {
-                        val errorShapeName = ctx.symbolProvider.toSymbol(errorShape).name
-                        writer.write("case \$S : self = .\$L(try \$L(httpResponse: httpResponse, decoder: decoder, message: message, requestID: requestID))", errorShapeName, errorShapeName.decapitalize(), errorShapeName)
+                        var errorShapeName = resolveErrorShapeName(errorShape)
+                        var errorShapeType = ctx.symbolProvider.toSymbol(errorShape).name
+                        var errorShapeEnumCase = errorShapeType.decapitalize()
+                        writer.write("case \$S : self = .\$L(try \$L(httpResponse: httpResponse, decoder: decoder, message: message, requestID: requestID))", errorShapeName, errorShapeEnumCase, errorShapeType)
                     }
                     writer.write("default : self = .unknown($unknownServiceErrorType(httpResponse: httpResponse, message: message, requestID: requestID))")
                     writer.write("}")
                 }
             }
+        }
+    }
+
+    private fun resolveErrorShapeName(errorShape: StructureShape): String {
+        errorShape.getTrait<AwsQueryErrorTrait>()?.let {
+            return it.code
+        } ?: run {
+            return ctx.symbolProvider.toSymbol(errorShape).name
         }
     }
 }
