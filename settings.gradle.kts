be9314e3af20ccf85f08256b56acd4ce96a7319f
John Woo <wooj@amazon.com>
2021-06-10 21:28:17 -0700

    chore: Speed up CI by eliminating jobs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ rootProject.name = "smithy-swift"
 enableFeaturePreview("GRADLE_METADATA")
 
 include("smithy-swift-codegen")
-include("smithy-swift-codegen-test")
+//include("smithy-swift-codegen-test")
