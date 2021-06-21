e77c407c5e0a22a7b5f8c9d85d8db69b5858f6d3
Nicki Stone <nickik@amazon.com>
2021-06-21 16:46:42 -0700

    undo changes to config

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/ServiceConfig.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/ServiceConfig.kt
@@ -6,7 +6,7 @@ import software.amazon.smithy.swift.codegen.SwiftWriter
 /**
  * Represents a config field on a client config struct.
  */
-data class ConfigField(val name: String?, val type: String, private val documentation: String?, val isFromOpenClass: Boolean = false)
+data class ConfigField(val name: String?, val type: String, private val documentation: String?)
 
 /**
  * ServiceConfig abstract class that allows configuration customizations to be configured for the protocol client generator
@@ -17,11 +17,11 @@ abstract class ServiceConfig(val writer: SwiftWriter, val serviceName: String) {
 
     open val typesToConformConfigTo: List<String> = mutableListOf("ClientRuntime.Configuration")
 
-    open fun getConfigFields(): List<ConfigField> = listOf(ConfigField("clientLogMode", "ClientLogMode", null))
+    open fun getConfigFields(): List<ConfigField> = listOf()
 
     open fun renderStaticDefaultImplementation(serviceSymbol: Symbol) {
         writer.openBlock("public static func `default`() throws -> ${serviceSymbol.name}Configuration {", "}") {
-            writer.write("return try ${serviceSymbol.name}Configuration(clientLogMode: .requestAndResponse)")
+            writer.write("return try ${serviceSymbol.name}Configuration()")
         }
     }
 
