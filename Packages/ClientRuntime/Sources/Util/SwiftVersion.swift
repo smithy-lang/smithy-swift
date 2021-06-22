9bedd021fd2ae5b246bb4c78187b4cb2e13b5a03
Nicki Stone <nickik@amazon.com>
2021-06-22 16:22:28 -0700

    add user agent utils

---
+++ Packages/ClientRuntime/Sources/Util/SwiftVersion.swift
@@ -0,0 +1,31 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+        
+public var currentSwiftVersion: String {
+    #if swift(>=5.5)
+    return "5.5"
+    
+    #elseif swift(>=5.4)
+    return "5.4"
+    
+    #elseif swift(>=5.3)
+    return "5.3"
+
+    #elseif swift(>=5.2)
+    return "5.2"
+
+    #elseif swift(>=5.1)
+    return "5.1"
+
+    #elseif swift(>=5.0)
+    return "5.0"
+    
+    #else
+    return "unknown"
+    #endif
+}
+
