20450ac934cf52cf6d274ef31c271effa4e83b48
Nicki Stone <nickik@amazon.com>
2021-06-23 12:56:46 -0700

    feat: add user agent utils (#265)

---
+++ Packages/ClientRuntime/Sources/Util/PlatformOperatingSystem.swift
@@ -0,0 +1,36 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+        
+public enum PlatformOperatingSystem: String {
+    case windows
+    case linux
+    case iOS
+    case macOS
+    case watchOS
+    case tvOS
+    case unknown
+}
+
+
+public var currentOS: PlatformOperatingSystem {
+    #if os(Linux)
+    return .linux
+    #elseif os(macOS)
+    return .macOS
+    #elseif os(iOS)
+    return .iOS
+    #elseif os(watchOS)
+    return .watchOS
+    #elseif os(Windows)
+    return .windows
+    #elseif os(tvOS)
+    return .tvOS
+    #else
+     #error("Cannot use a an operating system we do not support")
+    #endif
+}
+
---
+++ Packages/ClientRuntime/Sources/Util/SwiftVersion.swift
@@ -0,0 +1,27 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+public var swiftVersion: String {
+    #if swift(>=5.6)
+      #error("Cannot use a version of Swift greater than available. Please create a Github issue for us to add support for the version of Swift you want to use.")
+    #elseif swift(>=5.5)
+    return "5.5"
+    #elseif swift(>=5.4)
+    return "5.4"
+    #elseif swift(>=5.3)
+    return "5.3"
+    #elseif swift(>=5.2)
+    return "5.2"
+    #elseif swift(>=5.1)
+    return "5.1"
+    #elseif swift(>=5.0)
+    return "5.0"
+    #else
+      #error("Cannot use a version of Swift less than 5.0")
+    #endif
+}
+
