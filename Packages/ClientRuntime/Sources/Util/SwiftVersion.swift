1082b4433d5fe8ba83495c631a897ad4b356707d
Nicki Stone <nickik@amazon.com>
2021-06-22 17:03:00 -0700

    update swift version to fail if above swift 5.5

--- Packages/ClientRuntime/Sources/Util/SwiftVersion.swift
+++ Packages/ClientRuntime/Sources/Util/SwiftVersion.swift
@@ -4,26 +4,23 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 //
-        
+
 public var currentSwiftVersion: String {
-    #if swift(>=5.5)
+    #if swift(>=5.6)
+    public let failure: String?
+    return failure!
+    #elseif swift(>=5.5)
     return "5.5"
-    
     #elseif swift(>=5.4)
     return "5.4"
-    
     #elseif swift(>=5.3)
     return "5.3"
-
     #elseif swift(>=5.2)
     return "5.2"
-
     #elseif swift(>=5.1)
     return "5.1"
-
     #elseif swift(>=5.0)
     return "5.0"
-    
     #else
     return "unknown"
     #endif
