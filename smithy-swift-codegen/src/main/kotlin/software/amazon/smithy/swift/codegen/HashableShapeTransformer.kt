0a1930be893eb69420638e7b33810073c67ca00d
Nicki Stone <nickik@amazon.com>
2021-06-24 11:51:59 -0700

    got it owrking and add more tests

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/HashableShapeTransformer.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/HashableShapeTransformer.kt
@@ -1,9 +1,11 @@
 package software.amazon.smithy.swift.codegen
 
 import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.loader.Prelude
 import software.amazon.smithy.model.neighbor.RelationshipType
 import software.amazon.smithy.model.neighbor.Walker
 import software.amazon.smithy.model.shapes.Shape
+import software.amazon.smithy.model.shapes.StructureShape
 import software.amazon.smithy.model.transform.ModelTransformer
 import software.amazon.smithy.swift.codegen.model.hasTrait
 
@@ -22,14 +24,14 @@ object HashableShapeTransformer {
         //find all the shapes in this models shapes that have a struct shape contained within a list or set and don't already have the trait
         //find all the other shapes referencing that shape and mark with hashable.
         val allShapesNeedingHashable = mutableSetOf<Shape>()
-        val shapesNeedingHashable = model.operationShapes.filter { isHashable(model, it) && it.hasTrait<HashableTrait>() }
+        val shapesNeedingHashable = model.shapes().filter { isHashable(model, it) && !it.hasTrait<HashableTrait>() && !Prelude.isPreludeShape(it) }
         shapesNeedingHashable.forEach {allShapesNeedingHashable.add(it)}
         val walker = Walker(model)
 
         // walk all the shapes in the set and find all other
         // structs in the graph from that shape that are nested
 
-        shapesNeedingHashable.forEach { shape ->
+        allShapesNeedingHashable.forEach { shape ->
             walker.iterateShapes(shape) { relationship ->
                 when (relationship.relationshipType) {
                     RelationshipType.STRUCTURE_MEMBER,
@@ -37,7 +39,7 @@ object HashableShapeTransformer {
                     else -> false
                 }
             }.forEach {
-                if (!it.hasTrait<HashableTrait>()) {
+                if (it is StructureShape && !it.hasTrait<HashableTrait>()) {
                     allShapesNeedingHashable.add(it)
                 }
             }
@@ -57,7 +59,12 @@ object HashableShapeTransformer {
     }
 
     private fun isHashable(model: Model, shape: Shape): Boolean {
-        return model.listShapes.any { it.member.target == shape.toShapeId() } || model.setShapes.any { it.member.target == shape.toShapeId() }
+        return if (shape is StructureShape && !Prelude.isPreludeShape(shape)) {
+            val allCollectionShapes = model.setShapes.filter { !Prelude.isPreludeShape(it) }
+            allCollectionShapes.any { it.member.target == shape.toShapeId() }
+        } else {
+            false
+        }
     }
 
 }
--- smithy-swift-codegen/src/test/kotlin/HashableShapeTransformerTests.kt
+++ smithy-swift-codegen/src/test/kotlin/HashableShapeTransformerTests.kt
@@ -1,7 +1,9 @@
+import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import software.amazon.smithy.swift.codegen.HashableShapeTransformer
-import software.amazon.smithy.swift.codegen.HashableTrait
+import software.amazon.smithy.build.MockManifest
+import software.amazon.smithy.model.shapes.ShapeId
+import software.amazon.smithy.swift.codegen.*
 import software.amazon.smithy.swift.codegen.model.hasTrait
 import kotlin.streams.toList
 
@@ -9,7 +11,7 @@ class HashableShapeTransformerTests {
 
     @Test
     fun `leave non-hashable models unchanged`() {
-        val model = javaClass.getResource("hashable-trait-test.smithy").asSmithy()
+        val model = javaClass.getResource("simple-service-with-operation-and-dependency.smithy").asSmithy()
         val transformed = HashableShapeTransformer.transform(model)
         transformed.shapes().toList().forEach {
             Assertions.assertFalse(transformed.getShape(it.id).get().hasTrait<HashableTrait>())
@@ -17,5 +19,105 @@ class HashableShapeTransformerTests {
     }
 
     @Test
-    fun 
+    fun `add the hashable trait to hashable shapes`() {
+        val model = javaClass.getResource("hashable-trait-test.smithy").asSmithy()
+        val transformed = HashableShapeTransformer.transform(model)
+
+        val traitedMember = "smithy.example#HashableStructure"
+        val traitedMemberShape = transformed.getShape(ShapeId.from(traitedMember)).get()
+        Assertions.assertTrue(traitedMemberShape.hasTrait<HashableTrait>())
+
+        val traitedMember2 = "smithy.example#NestedHashableStructure"
+        val traitedMemberShape2 = transformed.getShape(ShapeId.from(traitedMember2)).get()
+        Assertions.assertTrue(traitedMemberShape2.hasTrait<HashableTrait>())
+
+        val untraitedMember = "smithy.example#HashableInput"
+        val untraitedMemberShape = transformed.getShape(ShapeId.from(untraitedMember)).get()
+        Assertions.assertFalse(untraitedMemberShape.hasTrait<HashableTrait>())
+    }
+
+    @Test
+    fun `add the hashable trait to hashable shapes during integration with SwiftCodegenPlugin`() {
+        val model = javaClass.getResource("hashable-trait-test.smithy").asSmithy()
+        val manifest = MockManifest()
+        val context = buildMockPluginContext(model, manifest, "smithy.example#Example")
+        SwiftCodegenPlugin().execute(context)
+
+        val hashableShapeInput = manifest
+            .getFileString("example/models/HashableShapesInput.swift").get()
+        Assertions.assertNotNull(hashableShapeInput)
+        val expected = """
+            public struct HashableShapesInput: Equatable {
+                public let `set`: Set<HashableStructure>?
+                public let bar: String?
+            
+                public init (
+                    `set`: Set<HashableStructure>? = nil,
+                    bar: String? = nil
+                )
+                {
+                    self.`set` = `set`
+                    self.bar = bar
+                }
+            }
+        """.trimIndent()
+        hashableShapeInput.shouldContain(expected)
+
+        val hashableShapeOutput = manifest
+            .getFileString("example/models/HashableShapesOutputResponse.swift").get()
+        Assertions.assertNotNull(hashableShapeOutput)
+        val expectedOutput = """
+            public struct HashableShapesOutputResponse: Equatable {
+                public let quz: String?
+            
+                public init (
+                    quz: String? = nil
+                )
+                {
+                    self.quz = quz
+                }
+            }
+        """.trimIndent()
+        hashableShapeOutput.shouldContain(expectedOutput)
+
+        val hashableSetShape = manifest
+            .getFileString("example/models/HashableStructure.swift").get()
+        Assertions.assertNotNull(hashableSetShape)
+        val expectedStructureShape = """
+            public struct HashableStructure: Equatable, Hashable {
+                public let baz: NestedHashableStructure?
+                public let foo: String?
+            
+                public init (
+                    baz: NestedHashableStructure? = nil,
+                    foo: String? = nil
+                )
+                {
+                    self.baz = baz
+                    self.foo = foo
+                }
+            }
+        """.trimIndent()
+        hashableSetShape.shouldContain(expectedStructureShape )
+
+        val hashableNestedStructure = manifest
+            .getFileString("example/models/NestedHashableStructure.swift").get()
+        Assertions.assertNotNull(hashableNestedStructure)
+        val expectedNestedStructureShape = """
+            public struct NestedHashableStructure: Equatable, Hashable {
+                public let bar: String?
+                public let quz: Int?
+            
+                public init (
+                    bar: String? = nil,
+                    quz: Int? = nil
+                )
+                {
+                    self.bar = bar
+                    self.quz = quz
+                }
+            }
+        """.trimIndent()
+        hashableNestedStructure.shouldContain(expectedNestedStructureShape)
+    }
 }
