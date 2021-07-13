0c5998e5ff078318e654dd3cabae3f8320610ffe
Nicki Stone <nickik@amazon.com>
2021-07-12 22:14:34 -0700

    bug fix

--- Packages/ClientRuntime/Sources/Networking/Streaming/StreamSink.swift
+++ Packages/ClientRuntime/Sources/Networking/Streaming/StreamSink.swift
@@ -125,11 +125,4 @@ public class DataStreamSink: StreamSink {
     }
 }
 
-extension Data {
-    func copyBytes<T>(as _: T.Type) -> [T] {
-        return withUnsafeBytes { (bytes: UnsafePointer<T>) in
-            Array(UnsafeBufferPointer(start: bytes, count: count / MemoryLayout<T>.stride))
-        }
-    }
-}
 
