17f076ed62f04673d7018496e3c98819653fabc7
Nicki Stone <nickik@amazon.com>
2021-07-28 19:52:39 -0700

    fix swift lint

--- Packages/ClientRuntime/Sources/Networking/Http/CRT/SDKDefaultIO.swift
+++ Packages/ClientRuntime/Sources/Networking/Http/CRT/SDKDefaultIO.swift
@@ -18,21 +18,32 @@ public final class SDKDefaultIO {
 
     private init() {
         self.eventLoopGroup = EventLoopGroup(threadCount: 0)
-        self.hostResolver = DefaultHostResolver(eventLoopGroup: eventLoopGroup, maxHosts: 8, maxTTL: 30)
+        self.hostResolver = DefaultHostResolver(eventLoopGroup: eventLoopGroup,
+                                                maxHosts: 8,
+                                                maxTTL: 30)
         
         do {
-            self.clientBootstrap = try! ClientBootstrap(eventLoopGroup: eventLoopGroup, hostResolver: hostResolver)
+            self.clientBootstrap = try ClientBootstrap(eventLoopGroup: eventLoopGroup,
+                                                       hostResolver: hostResolver)
         } catch {
-            fatalError("Client Bootstrap failed to create. This could be due to lack of memory but should never happen. Please open a Github issue with us at https://github.com/awslabs/aws-sdk-swift.")
+            fatalError("""
+                       Client Bootstrap failed to create. This could be due to lack
+                       of memory but should never happen. Please open a Github issue with
+                       us at https://github.com/awslabs/aws-sdk-swift.
+                       """)
         }
         
         let tlsContextOptions = TlsContextOptions()
         tlsContextOptions.setVerifyPeer(true)
         
         do {
-            self.tlsContext = try! TlsContext(options: tlsContextOptions, mode: .client)
+            self.tlsContext = try TlsContext(options: tlsContextOptions,
+                                             mode: .client)
         } catch {
-            fatalError("Tls Context failed to create. This should never happen. Please open a Github issue with us at https://github.com/awslabs/aws-sdk-swift.")
+            fatalError("""
+                        Tls Context failed to create. This should never happen.Please open a
+                        Github issue with us at https://github.com/awslabs/aws-sdk-swift.
+                        """)
         }
     }
 }
