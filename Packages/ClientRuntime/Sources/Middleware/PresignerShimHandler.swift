408f52f829bfd964c436272dec938f4a595ee8c9
John Woo <1911939+wooj2@users.noreply.github.com>
2021-09-14 10:46:43 -0700

    feat: foundational presigner components (#336)

---
+++ Packages/ClientRuntime/Sources/Middleware/PresignerShimHandler.swift
@@ -0,0 +1,42 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+	
+typealias PresignerShimHandler = (SdkHttpRequestBuilder) -> Void
+
+struct PresignerShim<OperationStackOutput: HttpResponseBinding,
+                              OperationStackError: HttpResponseBinding>: Middleware {
+    public let id: String = "PresignerShim"
+
+    private let handler: PresignerShimHandler
+
+    init(handler: @escaping PresignerShimHandler) {
+        self.handler = handler
+    }
+    
+    public typealias MInput = SdkHttpRequestBuilder
+    public typealias MOutput = OperationOutput<OperationStackOutput>
+    public typealias Context = HttpContext
+    public typealias MError = SdkError<OperationStackError>
+    
+    public func handle<H>(context: HttpContext,
+                          input: SdkHttpRequestBuilder,
+                          next: H) -> Result<OperationOutput<OperationStackOutput>, MError>
+    where H: Handler,
+    Self.Context == H.Context,
+    Self.MInput == H.Input,
+    Self.MOutput == H.Output,
+    Self.MError == H.MiddlewareError {
+        handler(input)
+        let httpResponse = HttpResponse(body: .none, statusCode: .ok)
+        do {
+            let output: OperationStackOutput? = try OperationStackOutput(httpResponse: httpResponse, decoder: nil)
+            return .success(.init(httpResponse: httpResponse, output: output))
+        } catch {
+            return .failure(.unknown(ClientError.unknownError("PresignerShimHandler: This code should not execute")))
+        }
+    }
+}
