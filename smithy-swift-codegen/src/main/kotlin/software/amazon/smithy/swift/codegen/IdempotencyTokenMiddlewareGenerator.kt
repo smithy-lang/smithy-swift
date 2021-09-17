859762d5630c16b61e531a7d1e8ce2d8b3a7b6bd
John Woo <wooj@amazon.com>
2021-09-17 10:54:14 -0700

    chore: add unit test for idempoency token trait

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/IdempotencyTokenMiddlewareGenerator.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/IdempotencyTokenMiddlewareGenerator.kt
@@ -35,7 +35,6 @@ class IdempotencyTokenMiddlewareGenerator(
      * ```
      * */
     fun renderIdempotencyMiddleware() {
-        // TODO: Need to write a unit test for this
         writer.openBlock("$operationMiddlewareStackName.initializeStep.intercept(position: .before, id: \"IdempotencyTokenMiddleware\") { (context, input, next) -> \$N<\$N<$outputShapeName>, \$N<$outputErrorShapeName>> in", "}", SwiftTypes.Result, ClientRuntimeTypes.Middleware.OperationOutput, ClientRuntimeTypes.Core.SdkError) {
             writer.write("let idempotencyTokenGenerator = context.getIdempotencyTokenGenerator()")
             writer.write("var copiedInput = input")
---
+++ smithy-swift-codegen/src/test/resources/Isolated/idempotencyToken.smithy
@@ -0,0 +1,26 @@
+
+$version: "1.0"
+
+namespace aws.protocoltests.restxml
+
+use aws.api#service
+use aws.protocols#restXml
+
+@service(sdkId: "Rest Xml Protocol")
+@restXml
+service RestXml {
+    version: "2019-12-16",
+    operations: [
+        IdempotencyTokenWithStructure
+    ]
+}
+
+@http(uri: "/IdempotencyTokenWithStructure", method: "PUT")
+operation IdempotencyTokenWithStructure {
+    input: IdempotencyToken,
+}
+
+structure IdempotencyToken {
+    @idempotencyToken
+    token: String,
+}
