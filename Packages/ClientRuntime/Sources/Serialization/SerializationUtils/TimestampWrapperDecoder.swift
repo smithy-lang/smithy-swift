50a5b35bbf7ace6056c4abd38dcee2d86aacccca
John Woo <wooj@amazon.com>
2021-09-17 18:11:36 -0700

    embellish codez

--- Packages/ClientRuntime/Sources/Serialization/SerializationUtils/TimestampWrapperDecoder.swift
+++ Packages/ClientRuntime/Sources/Serialization/SerializationUtils/TimestampWrapperDecoder.swift
@@ -11,29 +11,26 @@ public struct TimestampWrapperDecoder {
     static public func parseDateStringValue(_ dateStringValue: String,
                                             format: TimestampFormat,
                                             codingPath: [CodingKey]? = nil) throws -> Date {
-        var formatter: DateFormatter?
+        var formattedDate: Date?
+
         switch format {
         case .epochSeconds:
-            return Date(timeIntervalSince1970: TimeInterval(dateStringValue)!)
+            formattedDate = Date(timeIntervalSince1970: TimeInterval(dateStringValue)!)
         case .dateTime:
-            formatter = DateFormatter.iso8601DateFormatterWithoutFractionalSeconds
-            if let formattedDate = formatter!.date(from: dateStringValue) {
-                return formattedDate
-            } else {
-                formatter = DateFormatter.iso8601DateFormatterWithFractionalSeconds
+            if let date = DateFormatter.iso8601DateFormatterWithoutFractionalSeconds.date(from: dateStringValue) {
+                formattedDate = date
+            } else if let date = DateFormatter.iso8601DateFormatterWithFractionalSeconds.date(from: dateStringValue) {
+                formattedDate = date
             }
-
         case .httpDate:
-            formatter = DateFormatter.rfc5322DateFormatter
-
+            formattedDate = DateFormatter.rfc5322DateFormatter.date(from: dateStringValue)
         }
-        
-        guard let formattedDate = formatter!.date(from: dateStringValue) else {
+
+        guard let formattedDate = formattedDate else {
             let context = DecodingError.Context(codingPath: codingPath ?? [],
                                                 debugDescription: "Unable to parse: \(dateStringValue) to format \(format)")
             throw DecodingError.dataCorrupted(context)
         }
         return formattedDate
-
-    }
+    }    
 }
