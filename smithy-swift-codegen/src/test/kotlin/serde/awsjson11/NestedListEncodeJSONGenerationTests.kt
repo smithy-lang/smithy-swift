4f4e3c1a66aeabafb46487305cb3e594d8a7654b
Nicki Stone <nickik@amazon.com>
2021-09-20 14:17:30 -0700

    add tests

---
+++ smithy-swift-codegen/src/test/kotlin/serde/awsjson11/NestedListEncodeJSONGenerationTests.kt
@@ -0,0 +1,77 @@
+package serde.awsjson11
+
+import TestContext
+import defaultSettings
+import getFileContents
+import io.kotest.matchers.string.shouldContainOnlyOnce
+import mocks.MockHttpAWSJson11ProtocolGenerator
+import org.junit.jupiter.api.Test
+import shouldSyntacticSanityCheck
+
+class NestedListEncodeJSONGenerationTests {
+
+    @Test
+    fun `list of maps of lists`() {
+        val context = setupTests("Isolated/json11/list-of-maps-of-lists.smithy", "aws.protocoltests.json#JsonProtocol")
+        val contents = getFileContents(context.manifest, "/Example/models/ListOfMapsOperationInput.swift")
+        contents.shouldSyntacticSanityCheck()
+        val expectedContents =
+            """
+            public struct ListOfMapsOperationInput: Equatable {
+                public let targetMaps: [[String:[String]?]?]?
+                public init (
+                    targetMaps: [[String:[String]?]?]? = nil
+                )
+                {
+                    self.targetMaps = targetMaps
+                }
+            }
+            """.trimIndent()
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
+
+    @Test
+    fun `encode list of maps of lists`() {
+        val context = setupTests("Isolated/json11/list-of-maps-of-lists.smithy", "aws.protocoltests.json#JsonProtocol")
+        val contents = getFileContents(context.manifest, "/Example/models/ListOfMapsOperationInput+Encodable.swift")
+        contents.shouldSyntacticSanityCheck()
+        val expectedContents =
+            """
+            extension ListOfMapsOperationInput: Encodable, Reflection {
+                enum CodingKeys: String, CodingKey {
+                    case targetMaps
+                }
+            
+                public func encode(to encoder: Encoder) throws {
+                    var encodeContainer = encoder.container(keyedBy: CodingKeys.self)
+                    if let targetMaps = targetMaps {
+                        var targetMapsContainer = encodeContainer.nestedUnkeyedContainer(forKey: .targetMaps)
+                        for targetmaps0 in targetMaps {
+                            var targetmaps0Container = targetMapsContainer.nestedContainer(keyedBy: Key.self)
+                            if let targetmaps0 = targetmaps0 {
+                                for (dictKey1, targetmap1) in targetmaps0 {
+                                    var targetmap1Container = targetmaps0Container.nestedUnkeyedContainer(forKey: Key(dictKey1))
+                                    if let targetmap1 = targetmap1 {
+                                        for item in targetmap1 {
+                                            try targetmap1Container.encode(item)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            """.trimIndent()
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
+    private fun setupTests(smithyFile: String, serviceShapeId: String): TestContext {
+        val context = TestContext.initContextFrom(smithyFile, serviceShapeId, MockHttpAWSJson11ProtocolGenerator()) { model ->
+            model.defaultSettings(serviceShapeId, "Example", "2014-11-06", "aws json 11")
+        }
+        context.generator.generateCodableConformanceForNestedTypes(context.generationCtx)
+        context.generator.generateSerializers(context.generationCtx)
+        context.generationCtx.delegator.flushWriters()
+        return context
+    }
+}
