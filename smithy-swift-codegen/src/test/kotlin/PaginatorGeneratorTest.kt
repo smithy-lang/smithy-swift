9f42467decb078fca58f3d901ac150065be976de
Nicki Stone <nickik@amazon.com>
2022-02-28 18:39:11 -0800

    fix tests

--- smithy-swift-codegen/src/test/kotlin/PaginatorGeneratorTest.kt
+++ smithy-swift-codegen/src/test/kotlin/PaginatorGeneratorTest.kt
@@ -24,7 +24,7 @@ class PaginatorGeneratorTest {
        ///     - input: A `[ListFunctions2Input]` to start pagination
        /// - Returns: An `AsyncSequence` that can iterate over `ListFunctions2OutputResponse`
        extension TestClient {
-           func listFunctions2Paginated(input: ListFunctions2Input) -> ClientRuntime.PaginatorSequence<ListFunctions2Input, ListFunctions2OutputResponse> {
+           public func listFunctions2Paginated(input: ListFunctions2Input) -> ClientRuntime.PaginatorSequence<ListFunctions2Input, ListFunctions2OutputResponse> {
                return ClientRuntime.PaginatorSequence<ListFunctions2Input, ListFunctions2OutputResponse>(input: input, inputKey: \ListFunctions2Input.marker, outputKey: \ListFunctions2OutputResponse.nextMarker, paginationFunction: self.listFunctions2(input:))
            }
        }
@@ -57,7 +57,7 @@ class PaginatorGeneratorTest {
         ///     - input: A `[ListFunctionsInput]` to start pagination
         /// - Returns: An `AsyncSequence` that can iterate over `ListFunctionsOutputResponse`
         extension TestClient {
-            func listFunctionsPaginated(input: ListFunctionsInput) -> ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutputResponse> {
+            public func listFunctionsPaginated(input: ListFunctionsInput) -> ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutputResponse> {
                 return ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutputResponse>(input: input, inputKey: \ListFunctionsInput.marker, outputKey: \ListFunctionsOutputResponse.nextMarker, paginationFunction: self.listFunctions(input:))
             }
         }
