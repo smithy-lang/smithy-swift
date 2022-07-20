902cbcd8051349f9fd0cf9ad4fc02f24e430b819
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2022-07-20 09:52:38 -0400

    removes the redundant call for releaseToken which has causes double frees (#422)

--- Packages/ClientRuntime/Sources/Retries/Retryer.swift
+++ Packages/ClientRuntime/Sources/Retries/Retryer.swift
@@ -9,6 +9,7 @@ protocol Retryer {
     func acquireToken(partitionId: String) async throws -> RetryToken
     func scheduleRetry(token: RetryToken, error: RetryError) async throws -> RetryToken
     func recordSuccess(token: RetryToken)
+    @available(*, deprecated, message: "This function will be removed soon.")
     func releaseToken(token: RetryToken)
     func isErrorRetryable<E>(error: SdkError<E>) -> Bool
     func getErrorType<E>(error: SdkError<E>) -> RetryError
