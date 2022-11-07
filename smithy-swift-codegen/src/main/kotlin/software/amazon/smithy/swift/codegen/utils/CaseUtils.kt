cb478655c8bdfe386d25f8b6729b3c9546c52742
Ganesh Jangir <jangirg@amazon.com>
2022-11-07 12:37:46 -0800

    fix!: consistent naming for types, protocols and everything else (#461)

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/utils/CaseUtils.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/utils/CaseUtils.kt
@@ -32,9 +32,25 @@ fun String.splitOnWordBoundaries(): List<String> {
     return result.split(" ")
 }
 
-fun String.toPascalCase(): String = splitOnWordBoundaries().joinToString(separator = "") { it.lowercase().replaceFirstChar { c -> c.uppercaseChar() } }
-
-fun String.toCamelCase(): String = toPascalCase().replaceFirstChar { c -> c.lowercaseChar() }
-
 // See https://awslabs.github.io/smithy/1.0/spec/aws/aws-core.html#using-sdk-service-id-for-client-naming
-fun String.clientName(): String = toPascalCase()
+fun String.clientName(): String = toUpperCamelCase()
+
+fun String.toLowerCamelCase(): String {
+    val words = this.splitOnWordBoundaries()
+
+    // make first part lowercase
+    val firstWord = words.first().lowercase()
+
+    // join
+    return firstWord + words.drop(1).joinToString(separator = "")
+}
+
+fun String.toUpperCamelCase(): String {
+    val words = this.splitOnWordBoundaries()
+
+    // make first part uppercase
+    val firstWord = words.first().replaceFirstChar { c -> c.uppercaseChar() }
+
+    // join
+    return firstWord + words.drop(1).joinToString(separator = "")
+}
