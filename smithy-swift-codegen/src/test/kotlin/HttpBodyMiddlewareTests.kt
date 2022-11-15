29c28eafd6af314064162532af68840db7c7002d
Ed Paulosky <eeppaauu@amazon.com>
2022-11-15 11:47:33 -0500

    Fixes tests after adjusting HttpBodyMiddleware

--- smithy-swift-codegen/src/test/kotlin/HttpBodyMiddlewareTests.kt
+++ smithy-swift-codegen/src/test/kotlin/HttpBodyMiddlewareTests.kt
@@ -149,13 +149,9 @@ class HttpBodyMiddlewareTests {
                 Self.Context == H.Context
                 {
                     do {
-                        let encoder = context.getEncoder()
                         if let payload1 = input.operationInput.payload1 {
+                            let encoder = context.getEncoder()
                             let payload1data = try encoder.encode(payload1)
-                            let payload1body = ClientRuntime.HttpBody.data(payload1data)
-                            input.builder.withBody(payload1body)
-                        } else {
-                            let payload1data = try encoder.encode(input.operationInput)
                             let payload1body = ClientRuntime.HttpBody.data(payload1data)
                             input.builder.withBody(payload1body)
                         }
