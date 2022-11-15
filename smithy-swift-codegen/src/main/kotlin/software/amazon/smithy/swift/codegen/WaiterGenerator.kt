5d710ab03b5f0a6b8639479d818a9f03dc8b8910
Josh Elkins <jbelkins@amazon.com>
2022-11-15 09:03:01 -0600

    Generate methods for waiters

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/WaiterGenerator.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/WaiterGenerator.kt
@@ -1,246 +1,88 @@
 package software.amazon.smithy.swift.codegen
 
-import software.amazon.smithy.codegen.core.CodegenException
-import software.amazon.smithy.codegen.core.Symbol
-import software.amazon.smithy.codegen.core.SymbolProvider
 import software.amazon.smithy.model.Model
-import software.amazon.smithy.model.knowledge.PaginatedIndex
-import software.amazon.smithy.model.knowledge.PaginationInfo
-import software.amazon.smithy.model.shapes.CollectionShape
-import software.amazon.smithy.model.shapes.MapShape
 import software.amazon.smithy.model.shapes.OperationShape
 import software.amazon.smithy.model.shapes.ServiceShape
-import software.amazon.smithy.model.shapes.Shape
 import software.amazon.smithy.swift.codegen.core.CodegenContext
 import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
 import software.amazon.smithy.swift.codegen.integration.SwiftIntegration
-import software.amazon.smithy.swift.codegen.model.SymbolProperty
-import software.amazon.smithy.swift.codegen.model.camelCaseName
 import software.amazon.smithy.swift.codegen.model.expectShape
 import software.amazon.smithy.swift.codegen.model.hasTrait
-import software.amazon.smithy.swift.codegen.model.isBoxed
-import software.amazon.smithy.swift.codegen.utils.toCamelCase
+import software.amazon.smithy.swift.codegen.model.toLowerCamelCase
+import software.amazon.smithy.swift.codegen.utils.toUpperCamelCase
 import software.amazon.smithy.waiters.WaitableTrait
+import software.amazon.smithy.waiters.Waiter
 
 /**
- * Generate paginators for supporting operations.  See
- * https://awslabs.github.io/smithy/1.0/spec/core/behavior-traits.html#paginated-trait for details.
+ * Generate waiters for supporting operations.  See
+ * https://smithy.io/2.0/additional-specs/waiters.html for details.
  */
 class WaiterGenerator : SwiftIntegration {
     override fun enabledForService(model: Model, settings: SwiftSettings): Boolean =
         model.operationShapes.any { it.hasTrait<WaitableTrait>() }
 
-    override fun writeAdditionalFiles(ctx: CodegenContext, protoCtx: ProtocolGenerator.GenerationContext, delegator: SwiftDelegator) {
+    override fun writeAdditionalFiles(
+        ctx: CodegenContext,
+        protoCtx: ProtocolGenerator.GenerationContext,
+        delegator: SwiftDelegator
+    ) {
         val service = ctx.model.expectShape<ServiceShape>(ctx.settings.service)
-        val paginatedIndex = PaginatedIndex.of(ctx.model)
-
         delegator.useFileWriter("${ctx.settings.moduleName}/Waiters.swift") { writer ->
-            val waitedOperations = service.allOperations
-                .map { ctx.model.expectShape<OperationShape>(it) }
-                .filter { operationShape -> operationShape.hasTrait(WaitableTrait.ID) }
+            writer.addImport(SwiftDependency.CLIENT_RUNTIME.target)
+            val serviceSymbol = ctx.symbolProvider.toSymbol(service)
+            writer.openBlock("extension \$L {", "}", serviceSymbol.name) {
+                val waitedOperations = service.allOperations
+                    .map { ctx.model.expectShape<OperationShape>(it) }
+                    .filter { operationShape -> operationShape.hasTrait(WaitableTrait.ID) }
 
-            waitedOperations.forEach { waitedOperation ->
-                val waiterInfo = paginatedIndex.getPaginationInfo(service, paginatedOperation).getOrNull()
-                    ?: throw CodegenException("Unexpectedly unable to get PaginationInfo from $service $paginatedOperation")
-                val paginationItemInfo = getItemDescriptorOrNull(paginationInfo, ctx)
-                renderWaiterForOperation(writer, ctx, service, paginatedOperation, paginationInfo, paginationItemInfo)
+                waitedOperations.forEach { waitedOperation ->
+                    val waitableTraits = waitedOperation.allTraits.mapNotNull { (_, trait) -> trait as? WaitableTrait }
+                    waitableTraits.forEach { waitableTrait ->
+                        waitableTrait.waiters.forEach { (waiterName, waiter) ->
+                            renderWaiterForOperation(writer, ctx, service, waitedOperation, waiterName, waiter)
+                        }
+                    }
+                }
             }
         }
     }
 
-    // Render paginator(s) for operation
     private fun renderWaiterForOperation(
         writer: SwiftWriter,
         ctx: CodegenContext,
         service: ServiceShape,
         waitedOperation: OperationShape,
-        trait: WaitableTrait,
-        itemDesc: ItemDescriptor?
+        waiterName: String,
+        waiter: Waiter
     ) {
         val serviceSymbol = ctx.symbolProvider.toSymbol(service)
-        val outputSymbol = ctx.symbolProvider.toSymbol(paginationInfo.output)
-        val inputSymbol = ctx.symbolProvider.toSymbol(paginationInfo.input)
-        val cursorMember = ctx.model.getShape(paginationInfo.inputTokenMember.target).get()
-        val cursorSymbol = ctx.symbolProvider.toSymbol(cursorMember)
-
-        renderResponsePaginator(
-            writer,
-            ctx.model,
-            ctx.symbolProvider,
-            serviceSymbol,
-            paginatedOperation,
-            inputSymbol,
-            outputSymbol,
-            paginationInfo,
-            cursorSymbol
-        )
-
-        // Optionally generate paginator when nested item is specified on the trait.
-        if (itemDesc != null) {
-            renderItemPaginator(
-                writer,
-                service,
-                paginatedOperation,
-                itemDesc,
-                inputSymbol,
-                outputSymbol
-            )
-        }
-    }
-
-    // Generate the paginator that iterates over responses
-    private fun renderResponsePaginator(
-        writer: SwiftWriter,
-        model: Model,
-        symbolProvider: SymbolProvider,
-        serviceSymbol: Symbol,
-        operationShape: OperationShape,
-        inputSymbol: Symbol,
-        outputSymbol: Symbol,
-        paginationInfo: PaginationInfo,
-        cursorSymbol: Symbol
-    ) {
-        writer.addImport(SwiftDependency.CLIENT_RUNTIME.target)
-        val nextMarkerLiteral = paginationInfo.outputTokenMemberPath.joinToString(separator = "?.") {
-            it.camelCaseName()
-        }
-        val markerLiteral = paginationInfo.inputTokenMember.camelCaseName()
-        val markerLiteralShape = model.expectShape(paginationInfo.inputTokenMember.target)
-        val markerLiteralSymbol = symbolProvider.toSymbol(markerLiteralShape)
-        val docBody = """
-            Paginate over `[${outputSymbol.name}]` results.
-            
-            When this operation is called, an `AsyncSequence` is created. AsyncSequences are lazy so no service
-            calls are made until the sequence is iterated over. This also means there is no guarantee that the request is valid
-            until then. If there are errors in your request, you will see the failures only after you start iterating.
-            - Parameters: 
-                - input: A `[${inputSymbol.name}]` to start pagination
-            - Returns: An `AsyncSequence` that can iterate over `${outputSymbol.name}`
-        """.trimIndent()
+        val inputType = waitedOperation.inputShape.name
+        val outputType = waitedOperation.outputShape.name
         writer.write("")
+        val docBody = """
+            Initiates waiting for the $waiterName event on the ${waitedOperation.toLowerCamelCase()} operation.
+            The operation will be tried and (if necessary) retried until the wait succeeds, fails, or times out.
+            Returns a `WaiterOutcome` asynchronously on waiter success, throws an error asynchronously on
+            waiter failure or timeout.
+            - Parameters:
+              - options: `WaiterOptions` to be used to configure this wait.
+              - input: The `$inputType` object to be used as a parameter when performing the operation.
+            - Returns: A `WaiterOutcome` with the result of the final, successful performance of the operation.
+            - Throws: `WaiterFailureError` if the waiter fails due to matching an `Acceptor` with state `failure`
+            or there is an error not handled by any `Acceptor.`
+            `WaiterTimeoutError` if the waiter times out.
+        """.trimIndent()
         writer.writeSingleLineDocs {
             this.write(docBody)
         }
-
-        writer.openBlock("extension \$L {", "}", serviceSymbol.name) {
-            writer.openBlock(
-                "public func \$LPaginated(input: \$N) -> \$N<\$N, \$N> {", "}",
-                operationShape.camelCaseName(),
-                inputSymbol,
-                ClientRuntimeTypes.Core.PaginatorSequence,
-                inputSymbol,
-                outputSymbol
-            ) {
-                writer.write(
-                    "return \$N<\$N, \$N>(input: input, inputKey: \\\$N.$markerLiteral, outputKey: \\\$N.$nextMarkerLiteral, paginationFunction: self.\$L(input:))",
-                    ClientRuntimeTypes.Core.PaginatorSequence,
-                    inputSymbol,
-                    outputSymbol,
-                    inputSymbol,
-                    outputSymbol,
-                    operationShape.camelCaseName()
-                )
-            }
-        }
-
-        writer.write("")
-
-        writer.openBlock("extension \$N: \$N {", "}", inputSymbol, ClientRuntimeTypes.Core.PaginateToken) {
-            writer.openBlock("public func usingPaginationToken(_ token: \$N) -> \$N {", "}", markerLiteralSymbol, inputSymbol) {
-                writer.writeInline("return ")
-                    .call {
-                        val inputShape = model.expectShape(operationShape.input.get())
-                        writer.writeInline("\$N(", inputSymbol)
-                            .indent()
-                            .call {
-                                val sortedMembers = inputShape.members().sortedBy { it.camelCaseName() }
-                                for ((index, member) in sortedMembers.withIndex()) {
-                                    if (member.memberName.toCamelCase() != markerLiteral) {
-                                        writer.writeInline("\n\$L: \$L", member.camelCaseName(), "self.${member.camelCaseName()}")
-                                    } else {
-                                        writer.writeInline("\n\$L: \$L", member.camelCaseName(), "token")
-                                    }
-                                    if (index < sortedMembers.size - 1) {
-                                        writer.writeInline(",")
-                                    }
-                                }
-                            }
-                            .dedent()
-                            .writeInline("\n)")
-                    }
-            }
+        writer.openBlock(
+            "public func waitUntil${waiterName.toUpperCamelCase()}(options: WaiterOptions, input: $inputType) async throws -> WaiterOutcome<$outputType> {",
+            "}"
+        ) {
+            writer.write("let acceptors: [WaiterConfiguration<$inputType, $outputType>.Acceptor] = []  // acceptors will be filled in a future PR")
+            writer.write("let config = try WaiterConfiguration(acceptors: acceptors, minDelay: ${waiter.minDelay}.0, maxDelay: ${waiter.maxDelay}.0)")
+            writer.write("let waiter = Waiter(config: config, operation: self.${waitedOperation.toLowerCamelCase()}(input:))")
+            writer.write("return try await waiter.waitUntil(options: options, input: input)")
         }
     }
-
-    // Generate a paginator that iterates over the model-specified item
-    private fun renderItemPaginator(
-        writer: SwiftWriter,
-        serviceShape: ServiceShape,
-        operationShape: OperationShape,
-        itemDesc: ItemDescriptor,
-        inputSymbol: Symbol,
-        outputSymbol: Symbol,
-    ) {
-        writer.write("")
-        val itemSymbolShape = itemDesc.itemSymbol.getProperty("shape").getOrNull() as? Shape
-        val docBody = """
-        This paginator transforms the `AsyncSequence` returned by `${operationShape.camelCaseName()}Paginated`
-        to access the nested member `${itemDesc.collectionLiteral}`
-        - Returns: `${itemDesc.collectionLiteral}`
-        """.trimIndent()
-
-        writer.writeSingleLineDocs {
-            this.write(docBody)
-        }
-
-        writer.openBlock("extension PaginatorSequence where Input == \$N, Output == \$N {", "}", inputSymbol, outputSymbol) {
-            writer.openBlock("public func \$L() async throws -> \$L {", "}", itemDesc.itemLiteral, itemDesc.collectionLiteral) {
-                if (itemSymbolShape?.isListShape == true) {
-                    writer.write("return try await self.asyncCompactMap { item in item.\$L }", itemDesc.itemPathLiteral)
-                } else if (itemSymbolShape?.isMapShape == true) {
-                    val suffix = if (itemDesc.itemSymbol.isBoxed()) "?" else ""
-                    writer.write("return try await self.asyncCompactMap { item in item.\$L$suffix.map { (\$\$0, \$\$1) } }", itemDesc.itemPathLiteral)
-                } else {
-                    error("Unexpected shape type $itemSymbolShape")
-                }
-            }
-        }
-    }
-}
-
-/**
- * Model info necessary to codegen paginator item
- */
-private data class ItemDescriptor(
-    val collectionLiteral: String,
-    val itemLiteral: String,
-    val itemPathLiteral: String,
-    val itemSymbol: Symbol
-)
-
-/**
- * Return an [ItemDescriptor] if model supplies, otherwise null
- */
-private fun getItemDescriptorOrNull(paginationInfo: PaginationInfo, ctx: CodegenContext): ItemDescriptor? {
-    val itemMemberId = paginationInfo.itemsMemberPath?.lastOrNull()?.target ?: return null
-    val itemLiteral = paginationInfo.itemsMemberPath!!.last()!!.camelCaseName()
-    val itemPathLiteral = paginationInfo.itemsMemberPath.joinToString(separator = "?.") { it.camelCaseName() }
-    val itemMember = ctx.model.expectShape(itemMemberId)
-    val collectionLiteral = when (itemMember) {
-        is MapShape -> {
-            val entryType = ctx.symbolProvider.toSymbol(itemMember).expectProperty(SymbolProperty.ENTRY_EXPRESSION) as String
-            "[$entryType]"
-        }
-        is CollectionShape -> {
-            ctx.symbolProvider.toSymbol(itemMember).fullName
-        }
-        else -> error("Unexpected shape type ${itemMember.type}")
-    }
-
-    return ItemDescriptor(
-        collectionLiteral,
-        itemLiteral,
-        itemPathLiteral,
-        ctx.symbolProvider.toSymbol(itemMember)
-    )
 }
---
+++ smithy-swift-codegen/src/test/resources/waiters.smithy
@@ -0,0 +1,44 @@
+namespace com.test
+
+use smithy.waiters#waitable
+use aws.protocols#restJson1
+
+service TestS3 {
+    operations: [HeadBucket]
+}
+
+@readonly
+@waitable(
+    BucketExists: {
+        documentation: "Wait until a bucket exists"
+        acceptors: [
+            {
+                state: "success"
+                matcher: {
+                    success: true
+                }
+            }
+            {
+                state: "retry"
+                matcher: {
+                    errorType: "NotFound"
+                }
+            }
+        ]
+        minDelay: 7
+        maxDelay: 22
+    }
+)
+@http(method: "HEAD", uri: "/bucket", code: 200)
+operation HeadBucket {
+    input: HeadBucketRequest,
+    output: HeadBucketResponse
+}
+
+structure HeadBucketRequest {
+    @httpQuery("BucketName")
+    bucketName: String,
+}
+
+structure HeadBucketResponse {
+}
