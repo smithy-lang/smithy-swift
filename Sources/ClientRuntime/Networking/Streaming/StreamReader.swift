8e181e2df6cbe8eb0c140f8fc07cb1f88a90e471
Ed Paulosky <eeppaauu@amazon.com>
2022-12-21 11:06:24 -0500

    Deletes empty files

--- Sources/ClientRuntime/Networking/Streaming/StreamReader.swift
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
- */
-
-import AwsCommonRuntimeKit
-import class Foundation.FileHandle
-import class Foundation.FileManager
