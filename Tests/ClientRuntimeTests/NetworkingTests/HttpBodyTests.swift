d7911da095266160765949a969eef32fc1285efd
Ed Paulosky <eeppaauu@amazon.com>
2023-01-04 13:55:17 -0500

    fix: Adds `HttpBody.isEmpty` to help parse S3 404 errors properly (#499)
    * Adds `isEmpty` computed var to HttpBody
    
    * Adds tests for HttpBody.isEmpty

---
+++ Tests/ClientRuntimeTests/NetworkingTests/HttpBodyTests.swift
@@ -0,0 +1,46 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import XCTest
+import AwsCommonRuntimeKit
+@testable import ClientRuntime
+
+class HttpBodyTests: XCTestCase {
+    func testWhenDataIsEmptyThenIsEmptyIsTrue() {
+        let data = Data()
+        let body = HttpBody.data(data)
+        XCTAssertTrue(body.isEmpty)
+    }
+    
+    func testWhenDataIsNilThenIsEmptyIsTrue() {
+        let body = HttpBody.data(nil)
+        XCTAssertTrue(body.isEmpty)
+    }
+    
+    func testWhenDataIsNotEmptyThenIsEmptyIsFalse() {
+        let data = "foo".data(using: .utf8)!
+        let body = HttpBody.data(data)
+        XCTAssertFalse(body.isEmpty)
+    }
+    
+    func testWhenStreamIsEmptyThenIsEmptyIsTrue() {
+        let stream = ByteStream.from(data: Data())
+        let body = HttpBody.stream(stream)
+        XCTAssertTrue(body.isEmpty)
+    }
+    
+    func testWhenStreamIsNotEmptyThenIsEmptyIsFalse() {
+        let stream = ByteStream.from(data: "foo".data(using: .utf8)!)
+        let body = HttpBody.stream(stream)
+        XCTAssertFalse(body.isEmpty)
+    }
+    
+    func testWhenBodyIsNoneThenIsEmptyIsTrue() {
+        let body = HttpBody.none
+        XCTAssertTrue(body.isEmpty)
+    }
+}
