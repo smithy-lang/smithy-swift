98884df611cc6af8918eb379c5ce2be4f16fe449
Ganesh Jangir <jangirg@amazon.com>
2023-03-06 15:53:25 -0800

    add todos

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "SmithyTestUtil", targets: ["SmithyTestUtil"])
     ],
     dependencies: [
-        .package(url: "https://github.com/awslabs/aws-crt-swift.git", branch: "jangirg/refactor/streams"),
+        .package(url: "https://github.com/awslabs/aws-crt-swift.git", branch: "jangirg/refactor/streams"), // TODO: change to exact version before merging to main
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.13.0")
     ],
