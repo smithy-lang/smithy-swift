014171fb6d4ffb5148f4aff051703594a42d12dc
Ganesh Jangir <jangirg@amazon.com>
2023-03-10 14:27:57 -0800

    docs

--- Sources/ClientRuntime/EventStream/MessageMarshaller.swift
+++ Sources/ClientRuntime/EventStream/MessageMarshaller.swift
@@ -5,6 +5,14 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+/// Marshals a event stream event into a `Message`.
+/// Codgegen generates a conformance to this protocol for each event stream event.
 public protocol MessageMarshaller {
+    /// Marshals a event stream event into a `Message`.
+    /// - Parameters:
+    ///   - encoder: RequestEncoder to use to encode the event stream event.
+    ///              Note: event type may contain nested types that need to be encoded
+    ///              using the same encoder.
+    /// - Returns: The marshalled `Message`.
     func marshall(encoder: RequestEncoder) throws -> EventStream.Message
 }
--- Sources/ClientRuntime/EventStream/MessageUnmarshaller.swift
+++ Sources/ClientRuntime/EventStream/MessageUnmarshaller.swift
@@ -5,6 +5,13 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+/// Unmarshals a `Message` into a event stream event.
 public protocol MessageUnmarshaller {
+    /// Unmarshals a `Message` into a event stream event.
+    /// - Parameters:
+    ///   - message: The message to unmarshal.
+    ///   - decoder: ResponseDecoder to use to decode the event stream event.
+    ///              Note: event type may contain nested types that need to be decoded
+    ///              using the same decoder.
     init(message: EventStream.Message, decoder: ResponseDecoder) throws
 }
