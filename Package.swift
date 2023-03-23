7938a48845d81a20ffb6a2c043d83e36cc0094b3
Ganesh Jangir <jangirg@amazon.com>
2023-03-23 16:20:07 -0700

    update branch

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // TODO: change to exact version before merging to main
-        .package(url: "https://github.com/awslabs/aws-crt-swift.git", branch: "jangirg/feat/runtime-support"),
+        .package(url: "https://github.com/awslabs/aws-crt-swift.git", branch: "jangirg/feat/es-codegen"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.13.0")
     ],
