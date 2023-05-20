f48cf07d38be8f6af6407eb25fe7cf9755c42aa1
Josh Elkins <jbelkins@amazon.com>
2023-05-20 15:58:45 -0500

    Fix SmithyTestUtilTests

--- Tests/SmithyTestUtilTests/RequestTestUtilTests/HttpRequestTestBaseTests.swift
+++ Tests/SmithyTestUtilTests/RequestTestUtilTests/HttpRequestTestBaseTests.swift
@@ -237,8 +237,8 @@ class HttpRequestTestBaseTests: HttpRequestTestBase {
         _ = try await operationStack.handleMiddleware(context: context, input: input, next: MockHandler { (_, _) in
             XCTFail("Deserialize was mocked out, this should fail")
             let httpResponse = HttpResponse(body: .none, statusCode: .badRequest)
-            let mockServiceError = try! MockMiddlewareError(httpResponse: httpResponse, decoder: context.getDecoder())
-            throw SdkError.service(mockServiceError, httpResponse)
+            let mockServiceError = try MockMiddlewareError.makeError(httpResponse: httpResponse, decoder: context.getDecoder())
+            throw mockServiceError
         })
     }
 }
