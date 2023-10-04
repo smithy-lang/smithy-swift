86c7d2cb7dc6539d46255220861a511b515c2fc4
Sichan Yoo <chanyoo@amazon.com>
2023-10-04 10:45:39 -0700

    Add signer protocol.

---
+++ Sources/ClientRuntime/Auth/HTTPAuthAPI/Signer.swift
@@ -0,0 +1,14 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+public protocol Signer {
+    associatedtype IdentityT: Identity
+    
+    func sign(requestBuilder: SdkHttpRequestBuilder, identity: IdentityT, signingProperties: Attributes) async throws -> SdkHttpRequestBuilder
+}
