1f7d2acbabc4cd10a575b20446ed31289b84ce20
Sichan Yoo <chanyoo@amazon.com>
2023-10-06 14:31:08 -0700

    Add auth scheme resolver to httpcontext, and change return values of added methods to optionals

--- Sources/ClientRuntime/Networking/Http/HttpContext.swift
+++ Sources/ClientRuntime/Networking/Http/HttpContext.swift
@@ -18,9 +18,13 @@ public class HttpContext: MiddlewareContext {
         }
         return builder
     }
+    
+    public func getAuthSchemeResolver() -> AuthSchemeResolver? {
+        return attributes.get(key: AttributeKeys.authSchemeResolver)
+    }
 
-    public func getAuthSchemes() -> Attributes {
-        return attributes.get(key: AttributeKeys.authSchemes)!
+    public func getAuthSchemes() -> Attributes? {
+        return attributes.get(key: AttributeKeys.authSchemes)
     }
 
     public func getDecoder() -> ResponseDecoder {
@@ -43,8 +47,8 @@ public class HttpContext: MiddlewareContext {
         return attributes.get(key: AttributeKeys.idempotencyTokenGenerator)!
     }
 
-    public func getIdentityResolvers() -> Attributes {
-        return attributes.get(key: AttributeKeys.identityResolvers)!
+    public func getIdentityResolvers() -> Attributes? {
+        return attributes.get(key: AttributeKeys.identityResolvers)
     }
 
     public func getLogger() -> LogAgent? {
@@ -108,6 +112,12 @@ public class HttpContextBuilder {
     public func with<T>(key: AttributeKey<T>,
                         value: T) -> HttpContextBuilder {
         self.attributes.set(key: key, value: value)
+        return self
+    }
+    
+    @discardableResult
+    public func withAuthSchemeResolver(value: AuthSchemeResolver) -> HttpContextBuilder {
+        self.attributes.set(key: AttributeKeys.authSchemeResolver, value: value)
         return self
     }
 
@@ -218,6 +228,7 @@ public class HttpContextBuilder {
 
 
 public enum AttributeKeys {
+    public static let authSchemeResolver = AttributeKey<AuthSchemeResolver>(name: "AuthSchemeResolver")
     public static let authSchemes = AttributeKey<Attributes>(name: "AuthSchemes")
     public static let bidirectionalStreaming = AttributeKey<Bool>(name: "BidirectionalStreaming")
     public static let decoder = AttributeKey<ResponseDecoder>(name: "Decoder")
