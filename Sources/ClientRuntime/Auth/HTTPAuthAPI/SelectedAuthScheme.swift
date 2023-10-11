dd4324b49f8e8c31a964cdfc562035be8764e8e6
Sichan Yoo <chanyoo@amazon.com>
2023-10-11 15:34:47 -0700

    Change schemeId to schemeID in auth scheme too.

--- Sources/ClientRuntime/Auth/HTTPAuthAPI/SelectedAuthScheme.swift
+++ Sources/ClientRuntime/Auth/HTTPAuthAPI/SelectedAuthScheme.swift
@@ -6,7 +6,7 @@
 //
 
 public struct SelectedAuthScheme {
-    let schemeId: String
+    let schemeID: String
     let identity: Identity?
     let signingProperties: Attributes?
     let signer: Signer?
