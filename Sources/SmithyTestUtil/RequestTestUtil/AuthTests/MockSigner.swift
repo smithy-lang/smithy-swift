de25f6ea875ce5f99c0166917b83f5b9e692ebf1
Sichan Yoo <chanyoo@amazon.com>
2023-11-14 14:29:33 -0800

    Update MockSigner to conform to modified Signer API with signEvent.

--- Sources/SmithyTestUtil/RequestTestUtil/AuthTests/MockSigner.swift
+++ Sources/SmithyTestUtil/RequestTestUtil/AuthTests/MockSigner.swift
@@ -10,12 +10,21 @@ import Foundation
 
 public struct MockSigner: ClientRuntime.Signer {
     public init() {}
-    public func sign<IdentityT: Identity>(
+
+    public func signRequest<IdentityT: Identity>(
         requestBuilder: ClientRuntime.SdkHttpRequestBuilder,
         identity: IdentityT,
         signingProperties: ClientRuntime.Attributes
     ) async throws -> ClientRuntime.SdkHttpRequestBuilder {
         requestBuilder.withHeader(name: "Mock-Authorization", value: "Mock-Signed")
         return requestBuilder
+    }
+
+    public func signEvent(
+        payload: Data,
+        previousSignature: String,
+        signingProperties: Attributes
+    ) async throws -> SigningResult<EventStream.Message> {
+        return SigningResult(output: EventStream.Message(), signature: "")
     }
 }
