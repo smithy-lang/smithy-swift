586b05b0830f58d677c8ff068d9a7c4950951067
David Yaffe <dayaffe@amazon.com>
2023-12-01 14:32:37 -0500

    update tests

--- smithy-swift-codegen/src/test/kotlin/PaginatorGeneratorTest.kt
+++ smithy-swift-codegen/src/test/kotlin/PaginatorGeneratorTest.kt
@@ -25,7 +25,7 @@ class PaginatorGeneratorTest {
             ///     - input: A `[ListFunctionsInput]` to start pagination
             /// - Returns: An `AsyncSequence` that can iterate over `ListFunctionsOutput`
             public func listFunctionsPaginated(input: ListFunctionsInput) -> ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutput> {
-                return ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutput>(input: input, inputKey: \ListFunctionsInput.marker, outputKey: \ListFunctionsOutput.nextMarker, paginationFunction: self.listFunctions(input:))
+                return ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutput>(input: input, inputKey: \.marker, outputKey: \.nextMarker, paginationFunction: self.listFunctions(input:))
             }
         }
 
@@ -58,7 +58,7 @@ class PaginatorGeneratorTest {
             ///     - input: A `[ListFunctionsInput]` to start pagination
             /// - Returns: An `AsyncSequence` that can iterate over `ListFunctionsOutput`
             public func listFunctionsPaginated(input: ListFunctionsInput) -> ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutput> {
-                return ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutput>(input: input, inputKey: \ListFunctionsInput.marker, outputKey: \ListFunctionsOutput.nextMarker, paginationFunction: self.listFunctions(input:))
+                return ClientRuntime.PaginatorSequence<ListFunctionsInput, ListFunctionsOutput>(input: input, inputKey: \.marker, outputKey: \.nextMarker, paginationFunction: self.listFunctions(input:))
             }
         }
         
@@ -100,7 +100,7 @@ class PaginatorGeneratorTest {
             ///     - input: A `[PaginatedMapInput]` to start pagination
             /// - Returns: An `AsyncSequence` that can iterate over `PaginatedMapOutput`
             public func paginatedMapPaginated(input: PaginatedMapInput) -> ClientRuntime.PaginatorSequence<PaginatedMapInput, PaginatedMapOutput> {
-                return ClientRuntime.PaginatorSequence<PaginatedMapInput, PaginatedMapOutput>(input: input, inputKey: \PaginatedMapInput.nextToken, outputKey: \PaginatedMapOutput.inner?.token, paginationFunction: self.paginatedMap(input:))
+                return ClientRuntime.PaginatorSequence<PaginatedMapInput, PaginatedMapOutput>(input: input, inputKey: \.nextToken, outputKey: \.inner?.token, paginationFunction: self.paginatedMap(input:))
             }
         }
         
