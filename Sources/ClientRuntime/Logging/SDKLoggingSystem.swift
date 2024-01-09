f9046ac56b88aa65531b2d01dc66cb2faf9c01d3
Chan Yoo <55515281+sichanyoo@users.noreply.github.com>
2024-01-09 09:17:38 -0500

    fix!: Add no-op behavior for initialize methods of logging system. (#637)
    * Add no-op behavior for initialize methods if it isn't the first time being called.
    
    * Make LockingSystem threadsafe.
    
    * Make initialize methods async.
    
    ---------
    
    Co-authored-by: Sichan Yoo <chanyoo@amazon.com>

--- Sources/ClientRuntime/Logging/SDKLoggingSystem.swift
+++ Sources/ClientRuntime/Logging/SDKLoggingSystem.swift
@@ -7,15 +7,17 @@
 
 import Logging
 
-public class SDKLoggingSystem {
+public actor SDKLoggingSystem {
+    private static var isInitialized = false
     private static var factories: [String: SDKLogHandlerFactory] = [:]
 
-    public class func add(logHandlerFactory: SDKLogHandlerFactory) {
+    public static func add(logHandlerFactory: SDKLogHandlerFactory) {
         let label = logHandlerFactory.label
         factories[label] = logHandlerFactory
     }
 
-    public class func initialize(defaultLogLevel: SDKLogLevel = .info) {
+    public static func initialize(defaultLogLevel: SDKLogLevel = .info) async {
+        if isInitialized { return } else { isInitialized = true }
         LoggingSystem.bootstrap { label in
             if let factory = factories[label] {
                 return factory.construct(label: label)
@@ -26,7 +28,8 @@ public class SDKLoggingSystem {
         }
     }
 
-    public class func initialize(logLevel: SDKLogLevel) {
+    public static func initialize(logLevel: SDKLogLevel) async {
+        if isInitialized { return } else { isInitialized = true }
         LoggingSystem.bootstrap { label in
             var handler = StreamLogHandler.standardOutput(label: label)
             handler.logLevel = logLevel.toLoggerType()
