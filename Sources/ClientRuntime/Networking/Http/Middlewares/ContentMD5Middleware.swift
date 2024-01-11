29f734a5b8840028b6614222bcaf180f09586066
David Yaffe <dayaffe@amazon.com>
2024-01-11 13:47:19 -0500

    bump up CRT version to 0.22.0 (#639)

--- Sources/ClientRuntime/Networking/Http/Middlewares/ContentMD5Middleware.swift
+++ Sources/ClientRuntime/Networking/Http/Middlewares/ContentMD5Middleware.swift
@@ -20,13 +20,11 @@ public struct ContentMD5Middleware<OperationStackOutput>: Middleware {
 
         switch input.body {
         case .data(let data):
-            guard
-                let data = data,
-                let bodyString = String(data: data, encoding: .utf8)
-            else {
+            guard let data = data else {
                 return try await next.handle(context: context, input: input)
             }
-            let base64Encoded = try bodyString.base64EncodedMD5()
+            let md5Hash = try data.computeMD5()
+            let base64Encoded = md5Hash.base64EncodedString()
             input.headers.update(name: "Content-MD5", value: base64Encoded)
         case .stream:
             guard let logger = context.getLogger() else {
