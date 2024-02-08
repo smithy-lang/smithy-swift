40d2a141a46b5d659fae95c6f910f67172cfdba0
Josh Elkins <jbelkins@amazon.com>
2024-02-08 12:48:58 -0600

    chore: Update aws-crt-swift to 0.26.0

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .library(name: "SmithyTestUtil", targets: ["SmithyTestUtil"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/awslabs/aws-crt-swift.git", exact: "0.22.0"),
+        .package(url: "https://github.com/awslabs/aws-crt-swift.git", exact: "0.26.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", exact: "0.17.0")
     ],
