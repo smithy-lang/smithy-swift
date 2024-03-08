558ac0569b3c9a2d1077907b712bb8519a4bbc7d
Chan <55515281+sichanyoo@users.noreply.github.com>
2024-03-08 12:48:43 -0800

    fix: Add overwrite behavior for endpoints 2.0 for signing properties (#678)
    * Add convenience method to SelectedAuthScheme that returns copied auth scheme, except one signing property updated to a new value.
    
    * nit: fix swiftlint issues that got merged previously.
    
    ---------
    
    Co-authored-by: Sichan Yoo <chanyoo@amazon.com>

--- Sources/ClientRuntime/Plugins/AuthSchemePlugin.swift
+++ Sources/ClientRuntime/Plugins/AuthSchemePlugin.swift
@@ -11,17 +11,20 @@ public class AuthSchemePlugin: Plugin {
 
     private var authSchemeResolver: ClientRuntime.AuthSchemeResolver?
 
-    public init(authSchemeResolver: ClientRuntime.AuthSchemeResolver? = nil, authSchemes: [ClientRuntime.AuthScheme]? = nil) {
+    public init(
+        authSchemeResolver: ClientRuntime.AuthSchemeResolver? = nil,
+        authSchemes: [ClientRuntime.AuthScheme]? = nil
+    ) {
         self.authSchemeResolver = authSchemeResolver
         self.authSchemes = authSchemes
     }
 
     public func configureClient(clientConfiguration: ClientConfiguration) {
         if var config = clientConfiguration as? DefaultHttpClientConfiguration {
-            if (self.authSchemes != nil) {
+            if self.authSchemes != nil {
                 config.authSchemes = self.authSchemes!
             }
-            if (self.authSchemeResolver != nil) {
+            if self.authSchemeResolver != nil {
                 config.authSchemeResolver = self.authSchemeResolver!
             }
         }
