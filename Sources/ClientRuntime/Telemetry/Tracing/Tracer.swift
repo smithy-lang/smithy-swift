e3def72219071dcde40fdf50cc255ab6114aed76
Steven Yuan <yuasteve@amazon.com>
2024-03-14 16:01:20 -0700

    Add Telemetry Tracing interfaces

---
+++ Sources/ClientRuntime/Telemetry/Tracing/Tracer.swift
@@ -0,0 +1,26 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/// A Tracer is the entry point for creating spans.
+///
+/// Implementations MAY provide convenient builder APIs or other extensions to create spans, but ultimately new spans
+/// should be created using the tracer interface.
+public protocol Tracer {
+    /// Create a new Trace Span.
+    ///
+    /// - Parameter name: name of the span
+    /// - Parameter initialAttributes: initial span attributes
+    /// - Parameter spanKind: kind of span
+    /// - Parameter parentContext: parent context of the span
+    /// - Returns: returns the new Trace Span
+    func createSpan(
+        name: String,
+        initialAttributes: Attributes?,
+        spanKind: SpanKind,
+        parentContext: TelemetryContext?
+    ) -> TraceSpan
+}
---
+++ Sources/ClientRuntime/Telemetry/Tracing/TracerProvider.swift
@@ -0,0 +1,16 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/// A Tracer Provider provides implementations of Tracers.
+public protocol TracerProvider {
+    /// Gets a scoped Tracer.
+    ///
+    /// - Parameter scope: the unique scope of the Tracer
+    /// - Parameter attributes: instrumentation scope attributes
+    /// - Returns: a Tracer
+    func getTracer(scope: String, attributes: Attributes?) -> Tracer
+}
