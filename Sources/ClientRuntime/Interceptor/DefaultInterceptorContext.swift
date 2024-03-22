c8246ca99c9c2920481f265c3d22d5d730660c46
Miles Ziemer <mziemer@amazon.com>
2024-03-22 12:10:15 -0400

    feat: add interceptors interfaces
    Add interceptors interfaces to client libraries.
    
    Interceptors defines hooks that can be used to inject functionality
    into different phases of the operation execution pipeline.

---
+++ Sources/ClientRuntime/Interceptor/DefaultInterceptorContext.swift
@@ -0,0 +1,89 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/// Default implementation for all interceptor context types.
+/// 
+/// This object will be created before operation execution, and passed through each interceptor
+/// hook in the execution pipeline.
+public class DefaultInterceptorContext<RequestType, ResponseType, AttributesType: HasAttributes>: InterceptorContext {
+    private var attributes: AttributesType
+    private var input: Any
+    private var request: RequestType?
+    private var response: ResponseType?
+    private var output: Any?
+
+    public init(input: Any, attributes: AttributesType) {
+        self.input = input
+        self.attributes = attributes
+    }
+
+    public func getInput() -> Any {
+        self.input
+    }
+
+    public func getAttributes() -> AttributesType {
+        return self.attributes
+    }
+}
+
+extension DefaultInterceptorContext: BeforeSerialization {}
+
+extension DefaultInterceptorContext: MutableInput {
+    public func updateInput(updated: Any) {
+        self.input = updated
+    }
+}
+
+extension DefaultInterceptorContext: AfterSerialization {
+    public func getRequest() -> RequestType {
+        self.request!
+    }
+}
+
+extension DefaultInterceptorContext: MutableRequest {
+    public func updateRequest(updated: RequestType) {
+        self.request = updated
+    }
+}
+
+extension DefaultInterceptorContext: BeforeDeserialization {
+    public func getResponse() -> ResponseType {
+        self.response!
+    }
+}
+
+extension DefaultInterceptorContext: MutableResponse {
+    public func updateResponse(updated: ResponseType) {
+        self.response = updated
+    }
+}
+
+extension DefaultInterceptorContext: AfterDeserialization {
+    public func getOutput() -> Any {
+        self.output!
+    }
+}
+
+extension DefaultInterceptorContext: AfterAttempt {
+    public func getResponse() -> ResponseType? {
+        self.response
+    }
+}
+
+extension DefaultInterceptorContext: MutableOutputAfterAttempt {
+    public func updateOutput(updated: Any) {
+        self.output = updated
+    }
+}
+
+extension DefaultInterceptorContext: Finalization {
+    public func getRequest() -> RequestType? {
+        self.request
+    }
+}
+
+extension DefaultInterceptorContext: MutableOutputFinalization {}
---
+++ Sources/ClientRuntime/Interceptor/Interceptor.swift
@@ -0,0 +1,128 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+/// An interceptor allows injecting code into the SDK's request execution pipeline.
+public protocol Interceptor<RequestType, ResponseType, AttributesType> {
+
+    /// The type of the request transport messages sent by operations this interceptor can hook into.
+    associatedtype RequestType
+
+    /// The type of the response transport message received by operations this interceptor can hook into.
+    associatedtype ResponseType
+
+    /// The type of the attributes that will be available to the interceptor.
+    associatedtype AttributesType: HasAttributes
+
+    /// A hook called at the start of execution, before the SDK does anything else.
+    func readBeforeExecution(context: some BeforeSerialization<AttributesType>) async throws
+
+    /// A hook called before the operation input is serialized into the transport message. This
+    /// method has the ability to modify the operation input.
+    func modifyBeforeSerialization(context: inout some MutableInput<AttributesType>) async throws
+
+    /// A hook called before the operation input is serialized into the transport message.
+    func readBeforeSerialization(context: some BeforeSerialization<AttributesType>) async throws
+
+    /// A hook called after the operation input is serialized into the transport message.
+    func readAfterSerialization(context: some AfterSerialization<RequestType, AttributesType>) async throws
+
+    /// A hook called before the retry loop is entered. This method has the ability to modify the transport
+    /// request message.
+    func modifyBeforeRetryLoop(context: inout some MutableRequest<RequestType, AttributesType>) async throws
+
+    /// A hook called before each attempt at sending the tranport request message to the service.
+    func readBeforeAttempt(context: some AfterSerialization<RequestType, AttributesType>) async throws
+
+    /// A hook called before the transport request message is signed. This method has the ability to modify
+    /// the transport request message.
+    func modifyBeforeSigning(context: inout some MutableRequest<RequestType, AttributesType>) async throws
+
+    /// A hook called before the transport request message is signed.
+    func readBeforeSigning(context: some AfterSerialization<RequestType, AttributesType>) async throws
+
+    /// A hook called after the transport request message is signed.
+    func readAfterSigning(context: some AfterSerialization<RequestType, AttributesType>) async throws
+
+    /// A hook called before the transport request message is sent to the service. This method has the ability
+    /// to modify the transport request message.
+    func modifyBeforeTransmit(context: inout some MutableRequest<RequestType, AttributesType>) async throws
+
+    /// A hook called before the transport request message is sent to the service.
+    func readBeforeTransmit(context: some AfterSerialization<RequestType, AttributesType>) async throws
+
+    /// A hook called after the transport request message is sent to the service, and a transport response
+    /// message has been received.
+    func readAfterTransmit(context: some BeforeDeserialization<RequestType, ResponseType, AttributesType>) async throws
+
+    /// A hook called before the transport response message is deserialized. This method has the ability to
+    /// modify the transport response message.
+    func modifyBeforeDeserialization(context: inout some MutableResponse<RequestType, ResponseType, AttributesType>) async throws
+
+    /// A hook alled before the transport response message is deserialized.
+    func readBeforeDeserialization(context: some BeforeDeserialization<RequestType, ResponseType, AttributesType>) async throws
+
+    /// A hook called after the transport response message is deserialized.
+    func readAfterDeserialization(context: some AfterDeserialization<RequestType, ResponseType, AttributesType>) async throws
+
+    /// A hook called when an attempt is completed. This method has the ability to modify the operation output.
+    func modifyBeforeAttemptCompletion(context: inout some MutableOutputAfterAttempt<RequestType, ResponseType, AttributesType>) async throws
+
+    /// A hook called when an attempt is completed.
+    func readAfterAttempt(context: some AfterAttempt<RequestType, ResponseType, AttributesType>) async throws
+
+    /// A hook called when execution is completed. This method has the ability to modify the operation output.
+    func modifyBeforeCompletion(context: inout some MutableOutputFinalization<RequestType, ResponseType, AttributesType>) async throws
+
+    /// A hook called when execution is completed.
+    func readAfterExecution(context: some Finalization<RequestType, ResponseType, AttributesType>) async throws
+}
+
+public extension Interceptor {
+    func readBeforeExecution(context: some BeforeSerialization<AttributesType>) async throws {}
+
+    func modifyBeforeSerialization(context: inout some MutableInput<AttributesType>) async throws {}
+
+    func readBeforeSerialization(context: some BeforeSerialization<AttributesType>) async throws {}
+
+    func readAfterSerialization(context: some AfterSerialization<RequestType, AttributesType>) async throws {}
+
+    func modifyBeforeRetryLoop(context: inout some MutableRequest<RequestType, AttributesType>) async throws {}
+
+    func readBeforeAttempt(context: some AfterSerialization<RequestType, AttributesType>) async throws {}
+
+    func modifyBeforeSigning(context: inout some MutableRequest<RequestType, AttributesType>) async throws {}
+
+    func readBeforeSigning(context: some AfterSerialization<RequestType, AttributesType>) async throws {}
+
+    func readAfterSigning(context: some AfterSerialization<RequestType, AttributesType>) async throws {}
+
+    func modifyBeforeTransmit(context: inout some MutableRequest<RequestType, AttributesType>) async throws {}
+
+    func readBeforeTransmit(context: some AfterSerialization<RequestType, AttributesType>) async throws {}
+
+    func readAfterTransmit(context: some BeforeDeserialization<RequestType, ResponseType, AttributesType>) async throws {}
+
+    func modifyBeforeDeserialization(context: inout some MutableResponse<RequestType, ResponseType, AttributesType>) async throws {}
+
+    func readBeforeDeserialization(context: some BeforeDeserialization<RequestType, ResponseType, AttributesType>) async throws {}
+
+    func readAfterDeserialization(context: some AfterDeserialization<RequestType, ResponseType, AttributesType>) async throws {}
+
+    func modifyBeforeAttemptCompletion(context: inout some MutableOutputAfterAttempt<RequestType, ResponseType, AttributesType>) async throws {}
+
+    func readAfterAttempt(context: some AfterAttempt<RequestType, ResponseType, AttributesType>) async throws {}
+
+    func modifyBeforeCompletion(context: inout some MutableOutputFinalization<RequestType, ResponseType, AttributesType>) async throws {}
+
+    func readAfterExecution(context: some Finalization<RequestType, ResponseType, AttributesType>) async throws {}
+}
+
+public extension Interceptor {
+    func erase() -> AnyInterceptor<RequestType, ResponseType, AttributesType> {
+        return AnyInterceptor(interceptor: self)
+    }
+}
