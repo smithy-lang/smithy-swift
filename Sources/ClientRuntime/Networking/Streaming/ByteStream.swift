7315200569c96eeb49f773d0285c43d2b6861444
Sichan Yoo <chanyoo@amazon.com>
2024-04-25 12:09:03 -0700

    Move ByteStream's Equatable conformance back to runtime. Apparently it's used internally when doing switch-case on objects.

--- Sources/ClientRuntime/Networking/Streaming/ByteStream.swift
+++ Sources/ClientRuntime/Networking/Streaming/ByteStream.swift
@@ -28,6 +28,23 @@ public enum ByteStream {
     }
 }
 
+extension ByteStream: Equatable {
+    public static func ==(lhs: ByteStream, rhs: ByteStream) -> Bool {
+        switch (lhs, rhs) {
+        case (.data(let lhsData), .data(let rhsData)):
+            return lhsData == rhsData
+        case (.stream(let lhsStream), .stream(let rhsStream)):
+            // swiftlint:disable force_try
+            return try! lhsStream.readToEnd() == rhsStream.readToEnd()
+            // swiftlint:enable force_try
+        case (.noStream, .noStream):
+            return true
+        default:
+            return false
+        }
+    }
+}
+
 extension ByteStream: Codable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
