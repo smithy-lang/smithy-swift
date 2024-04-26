cfdb66138aecd64c5cbeb1db9e401791319c8034
Sichan Yoo <chanyoo@amazon.com>
2024-04-26 09:05:47 -0700

    Simplify FloatingPointValuesMatch test utility function by using generics instead of any?.

--- Sources/SmithyTestUtil/ResponseTestUtil/FloatingPointEquivalenceChecker.swift
+++ Sources/SmithyTestUtil/ResponseTestUtil/FloatingPointEquivalenceChecker.swift
@@ -5,20 +5,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-public func floatingPointValuesMatch(
-    lhs: (any FloatingPoint)?,
-    rhs: (any FloatingPoint)?
+public func floatingPointValuesMatch<FP: FloatingPoint>(
+    lhs: FP?,
+    rhs: FP?
 ) -> Bool {
-    // Both are nil => equal
-    if (lhs == nil && rhs == nil) { return true }
-    // Only one of them is nil => not equal
-    if (lhs == nil || rhs == nil) { return false }
-    // Both are NaN => equal
-    if (lhs!.isNaN && rhs!.isNaN) { return true }
-    // Only one of them is NaN => not equal
-    if (lhs!.isNaN || rhs!.isNaN) { return false }
-    // Both are non-nil & non-NaN => equal IFF values equal
-    let doubleMatch = (lhs as? Double ?? .nan) == (rhs as? Double ?? .nan)
-    let floatMatch = (lhs as? Float ?? .nan) == (rhs as? Float ?? .nan)
-    return doubleMatch || floatMatch
+    if let lhs, let rhs, lhs.isNaN, rhs.isNaN { return true }
+    return lhs == rhs
 }
