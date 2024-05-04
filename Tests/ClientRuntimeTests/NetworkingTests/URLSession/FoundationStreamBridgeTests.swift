c3f4feda4871d66c6c8aefb17f2924aaa2c1c84b
Josh Elkins <jbelkins@amazon.com>
2024-05-04 16:05:46 -0500

    Adjust test

--- Tests/ClientRuntimeTests/NetworkingTests/URLSession/FoundationStreamBridgeTests.swift
+++ Tests/ClientRuntimeTests/NetworkingTests/URLSession/FoundationStreamBridgeTests.swift
@@ -5,13 +5,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-// Run this unit test on macOS only.
-//
-// `FoundationStreamBridge` is not usable on Linux because it uses ObjC-interop features.
-//
-// Unit tests of types that spawn threads are unreliable on Apple-platform simulators.
-// Mac tests "run on metal", not a simulator, so they will run reliably.
-#if os(macOS)
+// `FoundationStreamBridge` is not usable on Linux because it uses ObjC-interop features,
+// so this test is disabled there.
+
+#if !os(Linux)
 
 import Foundation
 import XCTest
@@ -22,10 +19,7 @@ class FoundationStreamBridgeTests: XCTestCase {
     func test_open_streamsAllDataToOutputBuffer() async throws {
 
         // The maximum size of input streaming data in the tests
-        let maxDataSize = 1 * 1024 * 1024
-
-        // The max size of the buffer for data being streamed.
-        let maxBufferSize = 2 * maxDataSize
+        let maxDataSize = 65_536  // 64 kb
 
         // Create & fill a buffer with random bytes, for use in later test setup
         // Random buffer is reused because creating random data is slow
@@ -39,24 +33,28 @@ class FoundationStreamBridgeTests: XCTestCase {
 
         // Create a temp buffer we can use to copy the input stream bytes
         // We are responsible for deallocating it
-        let tempBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: maxDataSize)
+        let tempBufferSize = maxDataSize
+        let tempBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: tempBufferSize)
         defer { tempBuffer.deallocate() }
 
         // FoundationStreamBridge is susceptible to spurious bugs due to data races & other
         // not readily reproducible causes, so run this test repeatedly to help uncover
         // problems
 
-        let numberOfRuns = maxDataSize
+        let numberOfRuns = 100
 
         for run in 1...numberOfRuns {
             // Run a test for every possible data size up to the maximum
             let dataSize = min(run, maxDataSize)
 
-            // The bridge buffer may be as small as 1 byte, up to 2x as big as the data size capped by maxBufferSize
-            let bridgeBufferSize = Int.random(in: 1...min(2 * dataSize, maxBufferSize))
+            // Sizes of the following buffers within the `FoundationStreamBridge` under test are randomized to
+            // try and elicit errors in the streaming process.
 
-            // The bound stream buffer may be as small as 1 byte, up to 2x as big as the data size capped by maxBufferSize
-            let boundStreamBufferSize = Int.random(in: 1...min(2 * dataSize, maxBufferSize))
+            // The bridge buffer may be as small as 1 byte, up to 2x as big as the data size
+            let bridgeBufferSize = Int.random(in: 1...(2 * dataSize))
+
+            // The bound stream buffer may be as small as 1 byte, up to 2x as big as the data size
+            let boundStreamBufferSize = Int.random(in: 1...(2 * dataSize))
 
             // Fill a data buffer with dataSize random numbers
             let originalData = Data(bytes: randomBuffer, count: dataSize)
@@ -74,7 +72,7 @@ class FoundationStreamBridgeTests: XCTestCase {
             while ![.atEnd, .error].contains(subject.inputStream.streamStatus) {
 
                 // Copy the input stream to the temp buffer.  When count is positive, bytes were read
-                let count = subject.inputStream.read(tempBuffer, maxLength: maxDataSize)
+                let count = subject.inputStream.read(tempBuffer, maxLength: tempBufferSize)
                 if count > 0 {
                     // Add the read bytes onto the bridged data
                     bridgedData.append(tempBuffer, count: count)
