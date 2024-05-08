104e46216b55320aaf307f1d9be586e5e98cd924
Josh Elkins <jbelkins@amazon.com>
2024-05-08 13:44:14 -0500

    Fix codegen tests

--- smithy-swift-codegen/src/test/kotlin/HttpProtocolClientGeneratorTests.kt
+++ smithy-swift-codegen/src/test/kotlin/HttpProtocolClientGeneratorTests.kt
@@ -14,103 +14,103 @@ class HttpProtocolClientGeneratorTests {
         val context = setupTests("service-generator-test-operations.smithy", "com.test#Example")
         val contents = getFileContents(context.manifest, "/RestJson/RestJsonProtocolClient.swift")
         contents.shouldSyntacticSanityCheck()
-        contents.shouldContainOnlyOnce(
-            """
-            public class RestJsonProtocolClient: Client {
-                public static let clientName = "RestJsonProtocolClient"
-                let client: ClientRuntime.SdkHttpClient
-                let config: RestJsonProtocolClient.RestJsonProtocolClientConfiguration
-                let serviceName = "Rest Json Protocol"
-                let encoder: ClientRuntime.RequestEncoder
-                let decoder: ClientRuntime.ResponseDecoder
-            
-                public required init(config: RestJsonProtocolClient.RestJsonProtocolClientConfiguration) {
-                    client = ClientRuntime.SdkHttpClient(engine: config.httpClientEngine, config: config.httpClientConfiguration)
-                    let encoder = ClientRuntime.JSONEncoder()
-                    encoder.dateEncodingStrategy = .secondsSince1970
-                    encoder.nonConformingFloatEncodingStrategy = .convertToString(positiveInfinity: "Infinity", negativeInfinity: "-Infinity", nan: "NaN")
-                    self.encoder = encoder
-                    let decoder = ClientRuntime.JSONDecoder()
-                    decoder.dateDecodingStrategy = .secondsSince1970
-                    decoder.nonConformingFloatDecodingStrategy = .convertFromString(positiveInfinity: "Infinity", negativeInfinity: "-Infinity", nan: "NaN")
-                    self.decoder = decoder
-                    self.config = config
-                }
-            
-                public convenience required init() throws {
-                    let config = try RestJsonProtocolClient.RestJsonProtocolClientConfiguration()
-                    self.init(config: config)
-                }
-            
-            }
-            
-            extension RestJsonProtocolClient {
-                public class RestJsonProtocolClientConfiguration: DefaultClientConfiguration & DefaultHttpClientConfiguration {
-                    public var telemetryProvider: ClientRuntime.TelemetryProvider
-            
-                    public var retryStrategyOptions: ClientRuntime.RetryStrategyOptions
-            
-                    public var clientLogMode: ClientRuntime.ClientLogMode
-            
-                    public var endpoint: Swift.String?
-            
-                    public var idempotencyTokenGenerator: ClientRuntime.IdempotencyTokenGenerator
-            
-                    public var httpClientEngine: ClientRuntime.HTTPClient
-            
-                    public var httpClientConfiguration: ClientRuntime.HttpClientConfiguration
-            
-                    public var authSchemes: [ClientRuntime.AuthScheme]?
-            
-                    public var authSchemeResolver: ClientRuntime.AuthSchemeResolver
-            
-                    private init(_ telemetryProvider: ClientRuntime.TelemetryProvider, _ retryStrategyOptions: ClientRuntime.RetryStrategyOptions, _ clientLogMode: ClientRuntime.ClientLogMode, _ endpoint: Swift.String?, _ idempotencyTokenGenerator: ClientRuntime.IdempotencyTokenGenerator, _ httpClientEngine: ClientRuntime.HTTPClient, _ httpClientConfiguration: ClientRuntime.HttpClientConfiguration, _ authSchemes: [ClientRuntime.AuthScheme]?, _ authSchemeResolver: ClientRuntime.AuthSchemeResolver) {
-                        self.telemetryProvider = telemetryProvider
-                        self.retryStrategyOptions = retryStrategyOptions
-                        self.clientLogMode = clientLogMode
-                        self.endpoint = endpoint
-                        self.idempotencyTokenGenerator = idempotencyTokenGenerator
-                        self.httpClientEngine = httpClientEngine
-                        self.httpClientConfiguration = httpClientConfiguration
-                        self.authSchemes = authSchemes
-                        self.authSchemeResolver = authSchemeResolver
-                    }
-            
-                    public convenience init(telemetryProvider: ClientRuntime.TelemetryProvider? = nil, retryStrategyOptions: ClientRuntime.RetryStrategyOptions? = nil, clientLogMode: ClientRuntime.ClientLogMode? = nil, endpoint: Swift.String? = nil, idempotencyTokenGenerator: ClientRuntime.IdempotencyTokenGenerator? = nil, httpClientEngine: ClientRuntime.HTTPClient? = nil, httpClientConfiguration: ClientRuntime.HttpClientConfiguration? = nil, authSchemes: [ClientRuntime.AuthScheme]? = nil, authSchemeResolver: ClientRuntime.AuthSchemeResolver? = nil) throws {
-                        self.init(telemetryProvider ?? ClientRuntime.DefaultTelemetry.provider, retryStrategyOptions ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultRetryStrategyOptions, clientLogMode ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultClientLogMode, endpoint, idempotencyTokenGenerator ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultIdempotencyTokenGenerator, httpClientEngine ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.makeClient(), httpClientConfiguration ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultHttpClientConfiguration, authSchemes, authSchemeResolver ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultAuthSchemeResolver)
-                    }
-            
-                    public convenience required init() async throws {
-                        try await self.init(telemetryProvider: nil, retryStrategyOptions: nil, clientLogMode: nil, endpoint: nil, idempotencyTokenGenerator: nil, httpClientEngine: nil, httpClientConfiguration: nil, authSchemes: nil, authSchemeResolver: nil)
-                    }
-            
-                    public var partitionID: String? {
-                        return ""
-                    }
-                }
-            
-                public static func builder() -> ClientBuilder<RestJsonProtocolClient> {
-                    return ClientBuilder<RestJsonProtocolClient>(defaultPlugins: [
-                        ClientRuntime.DefaultClientPlugin()
-                    ])
-                }
-            }
-            
-            public struct RestJsonProtocolClientLogHandlerFactory: ClientRuntime.SDKLogHandlerFactory {
-                public var label = "RestJsonProtocolClient"
-                let logLevel: ClientRuntime.SDKLogLevel
-                public func construct(label: String) -> LogHandler {
-                    var handler = StreamLogHandler.standardOutput(label: label)
-                    handler.logLevel = logLevel.toLoggerType()
-                    return handler
-                }
-                public init(logLevel: ClientRuntime.SDKLogLevel) {
-                    self.logLevel = logLevel
-                }
-            }
-            """.trimIndent()
-        )
+        val expected = """
+public class RestJsonProtocolClient: Client {
+    public static let clientName = "RestJsonProtocolClient"
+    let client: ClientRuntime.SdkHttpClient
+    let config: RestJsonProtocolClient.RestJsonProtocolClientConfiguration
+    let serviceName = "Rest Json Protocol"
+    let encoder: ClientRuntime.RequestEncoder
+    let decoder: ClientRuntime.ResponseDecoder
+
+    public required init(config: RestJsonProtocolClient.RestJsonProtocolClientConfiguration) {
+        client = ClientRuntime.SdkHttpClient(engine: config.httpClientEngine, config: config.httpClientConfiguration)
+        let encoder = ClientRuntime.JSONEncoder()
+        encoder.dateEncodingStrategy = .secondsSince1970
+        encoder.nonConformingFloatEncodingStrategy = .convertToString(positiveInfinity: "Infinity", negativeInfinity: "-Infinity", nan: "NaN")
+        self.encoder = encoder
+        let decoder = ClientRuntime.JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        decoder.nonConformingFloatDecodingStrategy = .convertFromString(positiveInfinity: "Infinity", negativeInfinity: "-Infinity", nan: "NaN")
+        self.decoder = decoder
+        self.config = config
     }
+
+    public convenience required init() throws {
+        let config = try RestJsonProtocolClient.RestJsonProtocolClientConfiguration()
+        self.init(config: config)
+    }
+
+}
+
+extension RestJsonProtocolClient {
+    public class RestJsonProtocolClientConfiguration: DefaultClientConfiguration & DefaultHttpClientConfiguration {
+        public var telemetryProvider: ClientRuntime.TelemetryProvider
+
+        public var retryStrategyOptions: SmithyRetriesAPI.RetryStrategyOptions
+
+        public var clientLogMode: ClientRuntime.ClientLogMode
+
+        public var endpoint: Swift.String?
+
+        public var idempotencyTokenGenerator: ClientRuntime.IdempotencyTokenGenerator
+
+        public var httpClientEngine: ClientRuntime.HTTPClient
+
+        public var httpClientConfiguration: ClientRuntime.HttpClientConfiguration
+
+        public var authSchemes: [ClientRuntime.AuthScheme]?
+
+        public var authSchemeResolver: ClientRuntime.AuthSchemeResolver
+
+        private init(_ telemetryProvider: ClientRuntime.TelemetryProvider, _ retryStrategyOptions: SmithyRetriesAPI.RetryStrategyOptions, _ clientLogMode: ClientRuntime.ClientLogMode, _ endpoint: Swift.String?, _ idempotencyTokenGenerator: ClientRuntime.IdempotencyTokenGenerator, _ httpClientEngine: ClientRuntime.HTTPClient, _ httpClientConfiguration: ClientRuntime.HttpClientConfiguration, _ authSchemes: [ClientRuntime.AuthScheme]?, _ authSchemeResolver: ClientRuntime.AuthSchemeResolver) {
+            self.telemetryProvider = telemetryProvider
+            self.retryStrategyOptions = retryStrategyOptions
+            self.clientLogMode = clientLogMode
+            self.endpoint = endpoint
+            self.idempotencyTokenGenerator = idempotencyTokenGenerator
+            self.httpClientEngine = httpClientEngine
+            self.httpClientConfiguration = httpClientConfiguration
+            self.authSchemes = authSchemes
+            self.authSchemeResolver = authSchemeResolver
+        }
+
+        public convenience init(telemetryProvider: ClientRuntime.TelemetryProvider? = nil, retryStrategyOptions: SmithyRetriesAPI.RetryStrategyOptions? = nil, clientLogMode: ClientRuntime.ClientLogMode? = nil, endpoint: Swift.String? = nil, idempotencyTokenGenerator: ClientRuntime.IdempotencyTokenGenerator? = nil, httpClientEngine: ClientRuntime.HTTPClient? = nil, httpClientConfiguration: ClientRuntime.HttpClientConfiguration? = nil, authSchemes: [ClientRuntime.AuthScheme]? = nil, authSchemeResolver: ClientRuntime.AuthSchemeResolver? = nil) throws {
+            self.init(telemetryProvider ?? ClientRuntime.DefaultTelemetry.provider, retryStrategyOptions ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultRetryStrategyOptions, clientLogMode ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultClientLogMode, endpoint, idempotencyTokenGenerator ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultIdempotencyTokenGenerator, httpClientEngine ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.makeClient(), httpClientConfiguration ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultHttpClientConfiguration, authSchemes, authSchemeResolver ?? DefaultSDKRuntimeConfiguration<DefaultRetryStrategy, DefaultRetryErrorInfoProvider>.defaultAuthSchemeResolver)
+        }
+
+        public convenience required init() async throws {
+            try await self.init(telemetryProvider: nil, retryStrategyOptions: nil, clientLogMode: nil, endpoint: nil, idempotencyTokenGenerator: nil, httpClientEngine: nil, httpClientConfiguration: nil, authSchemes: nil, authSchemeResolver: nil)
+        }
+
+        public var partitionID: String? {
+            return ""
+        }
+    }
+
+    public static func builder() -> ClientBuilder<RestJsonProtocolClient> {
+        return ClientBuilder<RestJsonProtocolClient>(defaultPlugins: [
+            ClientRuntime.DefaultClientPlugin()
+        ])
+    }
+}
+
+public struct RestJsonProtocolClientLogHandlerFactory: ClientRuntime.SDKLogHandlerFactory {
+    public var label = "RestJsonProtocolClient"
+    let logLevel: ClientRuntime.SDKLogLevel
+    public func construct(label: String) -> LogHandler {
+        var handler = StreamLogHandler.standardOutput(label: label)
+        handler.logLevel = logLevel.toLoggerType()
+        return handler
+    }
+    public init(logLevel: ClientRuntime.SDKLogLevel) {
+        self.logLevel = logLevel
+    }
+}
+"""
+        contents.shouldContainOnlyOnce(expected)
+    }
+
     @Test
     fun `it renders host prefix with label in context correctly`() {
         val context = setupTests("host-prefix-operation.smithy", "com.test#Example")
@@ -164,7 +164,7 @@ class HttpProtocolClientGeneratorTests {
         operation.serializeStep.intercept(position: .after, middleware: ContentTypeMiddleware<AllocateWidgetInput, AllocateWidgetOutput>(contentType: "application/json"))
         operation.serializeStep.intercept(position: .after, middleware: ClientRuntime.BodyMiddleware<AllocateWidgetInput, AllocateWidgetOutput, ClientRuntime.JSONWriter>(documentWritingClosure: ClientRuntime.JSONReadWrite.documentWritingClosure(encoder: encoder), inputWritingClosure: JSONReadWrite.writingClosure()))
         operation.finalizeStep.intercept(position: .before, middleware: ClientRuntime.ContentLengthMiddleware<AllocateWidgetInput, AllocateWidgetOutput>())
-        operation.finalizeStep.intercept(position: .after, middleware: ClientRuntime.RetryMiddleware<ClientRuntime.DefaultRetryStrategy, ClientRuntime.DefaultRetryErrorInfoProvider, AllocateWidgetOutput>(options: config.retryStrategyOptions))
+        operation.finalizeStep.intercept(position: .after, middleware: ClientRuntime.RetryMiddleware<SmithyRetries.DefaultRetryStrategy, ClientRuntime.DefaultRetryErrorInfoProvider, AllocateWidgetOutput>(options: config.retryStrategyOptions))
         operation.finalizeStep.intercept(position: .before, middleware: ClientRuntime.SignerMiddleware<AllocateWidgetOutput>())
         operation.deserializeStep.intercept(position: .after, middleware: ClientRuntime.DeserializeMiddleware<AllocateWidgetOutput>(responseClosure(decoder: decoder), responseErrorClosure(AllocateWidgetOutputError.self, decoder: decoder)))
         operation.deserializeStep.intercept(position: .after, middleware: ClientRuntime.LoggerMiddleware<AllocateWidgetInput, AllocateWidgetOutput>(clientLogMode: config.clientLogMode))
