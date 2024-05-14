af83cff4ff3a50d0fa7e4f29a7d6f148ab504f32
Josh Elkins <jbelkins@amazon.com>
2024-05-14 16:55:52 -0500

    Split adaptive-related retry error from generic max attempts error; add doc comments

---
+++ Sources/SmithyRetries/DefaultRetryStrategy/DefaultRetryStrategy+Error.swift
@@ -0,0 +1,19 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+public extension DefaultRetryStrategy {
+
+    /// Errors that may be thrown when an operation is retried unsuccessfully.
+    public enum Error: Swift.Error {
+        
+        /// The number and frequency of retries being attempted has exceeded the
+        /// current limits.
+        ///
+        /// This error is only raised when the `adaptive` rate limiting mode of retry is used.
+        case insufficientQuota
+    }
+}
--- Sources/SmithyRetriesAPI/RetryError.swift
+++ Sources/SmithyRetriesAPI/RetryError.swift
@@ -5,7 +5,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+/// Errors that may be thrown when an operation is retried unsuccessfully.
 public enum RetryError: Error {
+
+    /// The maximum number of allowed retry attempts were made,
+    /// but the operation could not be successfully completed.
     case maxAttemptsReached
-    case insufficientQuota
 }
