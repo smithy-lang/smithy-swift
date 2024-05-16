2b46ad173b05bfb54235da3444577664dbc00e32
Josh Elkins <jbelkins@users.noreply.github.com>
2024-05-16 11:23:28 -0500

    chore: Move event marshal/unmarshal code to smithy-swift (#728)

--- smithy-swift-codegen/src/test/kotlin/mocks/MockHTTPRestXMLProtocolGenerator.kt
+++ smithy-swift-codegen/src/test/kotlin/mocks/MockHTTPRestXMLProtocolGenerator.kt
@@ -26,14 +26,6 @@ class MockHTTPRestXMLProtocolGenerator : HTTPBindingProtocolGenerator(MockRestXM
         // Intentionally empty
     }
 
-    override fun generateMessageMarshallable(ctx: ProtocolGenerator.GenerationContext) {
-        TODO("Not yet implemented")
-    }
-
-    override fun generateMessageUnmarshallable(ctx: ProtocolGenerator.GenerationContext) {
-        TODO("Not yet implemented")
-    }
-
     override fun generateProtocolUnitTests(ctx: ProtocolGenerator.GenerationContext): Int {
         val requestTestBuilder = HttpProtocolUnitTestRequestGenerator.Builder()
         val responseTestBuilder = HttpProtocolUnitTestResponseGenerator.Builder()
