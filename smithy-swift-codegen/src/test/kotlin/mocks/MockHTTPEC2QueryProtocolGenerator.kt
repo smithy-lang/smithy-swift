2811202525ff69a55599a5d7b296f3caffa6f8f8
Josh Elkins <jbelkins@amazon.com>
2024-05-16 10:51:39 -0500

    Remove empty marshal/unmarshal implementations from test protocol generators

--- smithy-swift-codegen/src/test/kotlin/mocks/MockHTTPEC2QueryProtocolGenerator.kt
+++ smithy-swift-codegen/src/test/kotlin/mocks/MockHTTPEC2QueryProtocolGenerator.kt
@@ -78,14 +78,6 @@ class MockHTTPEC2QueryProtocolGenerator : HTTPBindingProtocolGenerator(MockEC2Qu
         // Intentionally empty
     }
 
-    override fun generateMessageMarshallable(ctx: ProtocolGenerator.GenerationContext) {
-        TODO("Not yet implemented")
-    }
-
-    override fun generateMessageUnmarshallable(ctx: ProtocolGenerator.GenerationContext) {
-        TODO("Not yet implemented")
-    }
-
     override fun getProtocolHttpBindingResolver(ctx: ProtocolGenerator.GenerationContext, defaultContentType: String):
         HttpBindingResolver = MockEC2QueryHttpBindingResolver(ctx, defaultContentType)
 
