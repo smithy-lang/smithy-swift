e5e13379d5a1ed12f8edf283f15900e06d099b4a
Josh Elkins <jbelkins@users.noreply.github.com>
2024-06-05 15:37:43 -0500

    chore: Refactor symbol creation logic (#749)

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/swiftmodules/SmithyRetriesTypes.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/swiftmodules/SmithyRetriesTypes.kt
@@ -3,7 +3,6 @@ package software.amazon.smithy.swift.codegen.swiftmodules
 import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.swift.codegen.SwiftDeclaration
 import software.amazon.smithy.swift.codegen.SwiftDependency
-import software.amazon.smithy.swift.codegen.model.buildSymbol
 
 /**
  * Commonly used runtime types. Provides a single definition of a runtime symbol such that codegen isn't littered
@@ -15,9 +14,8 @@ object SmithyRetriesTypes {
     val DefaultRetryStrategy = runtimeSymbol("DefaultRetryStrategy")
 }
 
-private fun runtimeSymbol(name: String, declaration: SwiftDeclaration? = null): Symbol = buildSymbol {
-    this.name = name
-    this.namespace = SwiftDependency.SMITHY_RETRIES.target
-    declaration?.let { this.setProperty("decl", it.keyword) }
-    dependency(SwiftDependency.SMITHY_RETRIES)
-}
+private fun runtimeSymbol(name: String, declaration: SwiftDeclaration? = null): Symbol = SwiftSymbol.make(
+    name,
+    declaration,
+    SwiftDependency.SMITHY_RETRIES,
+)
