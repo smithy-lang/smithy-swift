6011f2f394a783217dbe7bc69c0222545fe2e2b4
Josh Elkins <jbelkins@amazon.com>
2024-07-11 14:46:29 -0500

    Add declaration type to TestInitializer, disable Package.swift in service clients

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/swiftmodules/SmithyTestUtilTypes.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/swiftmodules/SmithyTestUtilTypes.kt
@@ -1,17 +1,19 @@
 package software.amazon.smithy.swift.codegen.swiftmodules
 
 import software.amazon.smithy.codegen.core.Symbol
+import software.amazon.smithy.swift.codegen.SwiftDeclaration
 import software.amazon.smithy.swift.codegen.SwiftDependency
 import software.amazon.smithy.swift.codegen.model.buildSymbol
 
 object SmithyTestUtilTypes {
-    val TestInitializer = runtimeSymbol("TestInitializer")
+    val TestInitializer = runtimeSymbol("TestInitializer", SwiftDeclaration.ENUM)
     val TestBaseError = runtimeSymbol("TestBaseError")
     val SelectNoAuthScheme = runtimeSymbol("SelectNoAuthScheme")
 }
 
-private fun runtimeSymbol(name: String): Symbol = buildSymbol {
-    this.name = name
-    this.namespace = SwiftDependency.SMITHY_TEST_UTIL.target
-    dependency(SwiftDependency.SMITHY_TEST_UTIL)
-}
+private fun runtimeSymbol(name: String, declaration: SwiftDeclaration? = null): Symbol = SwiftSymbol.make(
+    name,
+    declaration,
+    SwiftDependency.SMITHY_TEST_UTIL,
+    null,
+)
