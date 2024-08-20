9ee8ade3be4fb3bc05c305fc1557201b99770c79
David Yaffe <dayaffe@amazon.com>
2024-08-20 15:32:43 -0400

    fix codegen for orchestrator telemetry (#802)

--- smithy-swift-codegen/src/test/kotlin/HttpProtocolClientGeneratorTests.kt
+++ smithy-swift-codegen/src/test/kotlin/HttpProtocolClientGeneratorTests.kt
@@ -180,7 +180,9 @@ extension RestJsonProtocolClient {
         let op = builder.attributes(context)
             .telemetry(ClientRuntime.OrchestratorTelemetry(
                 telemetryProvider: config.telemetryProvider,
-                metricsAttributes: metricsAttributes
+                metricsAttributes: metricsAttributes,
+                meterScope: serviceName,
+                tracerScope: serviceName
             ))
             .executeRequest(client)
             .build()
