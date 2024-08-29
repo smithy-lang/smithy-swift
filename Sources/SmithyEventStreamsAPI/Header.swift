2943c731d361e8e24b0af295b2859ed0efe8ce48
Josh Elkins <jbelkins@users.noreply.github.com>
2024-08-29 15:00:59 -0500

    chore: Fix indentation of code after modularization (#804)

--- Sources/SmithyEventStreamsAPI/Header.swift
+++ Sources/SmithyEventStreamsAPI/Header.swift
@@ -7,38 +7,32 @@
 
 import Foundation
 
-// Code left indented to prevent Git diff from being blown up by whitespace changes.
-// Will fix after event stream modularizaion has been reviewed.
+/// A header in an event stream message.
+/// Headers are used to convey metadata about the message.
+public struct Header: Equatable {
 
-    /// A header in an event stream message.
-    /// Headers are used to convey metadata about the message.
-    public struct Header: Equatable {
+    /// The name of the header.
+    public let name: String
 
-        /// The name of the header.
-        public let name: String
+    /// The value of the header.
+    public let value: HeaderValue
 
-        /// The value of the header.
-        public let value: HeaderValue
-
-        public init(name: String, value: HeaderValue) {
-            self.name = name
-            self.value = value
-        }
+    public init(name: String, value: HeaderValue) {
+        self.name = name
+        self.value = value
     }
+}
 
-// Code left indented to prevent Git diff from being blown up by whitespace changes.
-// Will fix after event stream modularizaion has been reviewed.
-
-    /// The value of a header in an event stream message.
-    /// Encoders and decoders may use this to determine how to encode or decode the header.
-    public enum HeaderValue: Equatable {
-        case bool(Bool)
-        case byte(Int8)
-        case int16(Int16)
-        case int32(Int32)
-        case int64(Int64)
-        case byteArray(Data)
-        case string(String)
-        case timestamp(Date)
-        case uuid(UUID)
-    }
+/// The value of a header in an event stream message.
+/// Encoders and decoders may use this to determine how to encode or decode the header.
+public enum HeaderValue: Equatable {
+    case bool(Bool)
+    case byte(Int8)
+    case int16(Int16)
+    case int32(Int32)
+    case int64(Int64)
+    case byteArray(Data)
+    case string(String)
+    case timestamp(Date)
+    case uuid(UUID)
+}
