73094d4259a9650119bece849aee7a5bd09f6f13
Sichan Yoo <chanyoo@amazon.com>
2024-10-08 11:38:00 -0700

    Basic scaffold.

---
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/SmokeTestGenerator.kt
@@ -0,0 +1,13 @@
+package software.amazon.smithy.swift.codegen.integration
+
+open class SmokeTestGenerator(
+    ctx: ProtocolGenerator.GenerationContext
+) {
+    fun generateSmokeTests() {
+
+    }
+
+    fun handleVendorParams(/* ARGUMENTS TBD */) {
+
+    }
+}
