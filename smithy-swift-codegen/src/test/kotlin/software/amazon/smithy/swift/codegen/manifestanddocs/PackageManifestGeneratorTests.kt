211bf19e42e838d4546c02658ec071fb890535f0
Josh Elkins <jbelkins@users.noreply.github.com>
2024-11-06 14:37:51 -0600

    fix: Add service client version to user-agent (#851)

--- smithy-swift-codegen/src/test/kotlin/software/amazon/smithy/swift/codegen/manifestanddocs/PackageManifestGeneratorTests.kt
+++ smithy-swift-codegen/src/test/kotlin/software/amazon/smithy/swift/codegen/manifestanddocs/PackageManifestGeneratorTests.kt
@@ -6,6 +6,7 @@ package software.amazon.smithy.swift.codegen.manifestanddocs
  */
 
 import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldStartWith
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import software.amazon.smithy.swift.codegen.PackageManifestGenerator
@@ -17,30 +18,38 @@ class PackageManifestGeneratorTests {
     private val testContext = setupTests("simple-service-with-operation-and-dependency.smithy", "smithy.example#Example")
 
     @Test
+    fun `it starts with a swift-tools-version statement`() {
+        val packageManifest = testContext.manifest.getFileString("Package.swift.txt").get()
+        assertNotNull(packageManifest)
+        packageManifest.shouldStartWith("// swift-tools-version: 5.5.0")
+    }
+
+    @Test
     fun `it renders package manifest file with macOS and iOS platforms block`() {
         val packageManifest = testContext.manifest.getFileString("Package.swift.txt").get()
         assertNotNull(packageManifest)
-        packageManifest.shouldContain(
-            "platforms: [\n" +
-                "        .macOS(.v10_15), .iOS(.v13)\n" +
-                "    ]"
-        )
+        val expected = """
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13)
+    ],
+"""
+        packageManifest.shouldContain(expected)
     }
 
     @Test
     fun `it renders package manifest file with single library in product block`() {
         val packageManifest = testContext.manifest.getFileString("Package.swift.txt").get()
         assertNotNull(packageManifest)
-        packageManifest.shouldContain(
-            "products: [\n" +
-                "        .library(name: \"MockSDK\", targets: [\"MockSDK\"])\n" +
-                "    ]"
-        )
+        val expected = """
+    products: [
+        .library(name: "MockSDK", targets: ["MockSDK"])
+    ],
+"""
+        packageManifest.shouldContain(expected)
     }
 
     @Test
     fun `it renders package manifest file with target and test target`() {
-        println(testContext.manifest.files)
         val packageManifest = testContext.manifest.getFileString("Package.swift.txt").get()
         assertNotNull(packageManifest)
         val expected = """
@@ -48,9 +57,6 @@ class PackageManifestGeneratorTests {
         .target(
             name: "MockSDK",
             dependencies: [
-            ],
-            resources: [
-                .process("Resources")
             ]
         ),
         .testTarget(
@@ -59,7 +65,7 @@ class PackageManifestGeneratorTests {
                 "MockSDK",
                 .product(
                     name: "SmithyTestUtil",
-                    package: "aws-sdk-swift.smithy-swift"
+                    package: "smithy-swift"
                 ),
             ]
         )
