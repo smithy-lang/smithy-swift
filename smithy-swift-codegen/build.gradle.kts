402da3b634a958079f23bf9da1fb7c40f2fc2869
Josh Elkins <jbelkins@amazon.com>
2024-11-21 16:50:06 -0600

    Fix jacoco test report task

--- smithy-swift-codegen/build.gradle.kts
+++ smithy-swift-codegen/build.gradle.kts
@@ -88,16 +88,17 @@ tasks.jar {
 }
 
 // Configure jacoco (code coverage) to generate an HTML report
-// tasks.jacocoTestReport {
-//     reports {
-//         xml.isEnabled = false
-//         csv.isEnabled = false
-//         html.destination = file("$buildDir/reports/jacoco")
-//     }
-// }
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(file("$buildDir/reports/jacoco"))
+    }
+}
 
 // Always run the jacoco test report after testing.
-tasks["test"].finalizedBy(tasks["jacocoTestReport"])
+tasks["test"].finalizedBy(tasks.jacocoTestReport)
 
 publishing {
     publications {
