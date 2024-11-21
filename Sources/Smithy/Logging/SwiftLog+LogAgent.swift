650267e30eefb5b1e63eb6aadd484fa3c096cdb9
Josh Elkins <jbelkins@users.noreply.github.com>
2024-11-21 15:41:09 -0600

    fix: Change logger params to @autoclosures (#865)

--- Sources/Smithy/Logging/SwiftLog+LogAgent.swift
+++ Sources/Smithy/Logging/SwiftLog+LogAgent.swift
@@ -1,68 +1,64 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
- */
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
 
 import Logging
 
 public struct SwiftLogger: LogAgent {
+    public var level: LogAgentLevel
+
     private let logger: Logger
     private let label: String
-    private var logLevel: LogAgentLevel
 
     public init(label: String) {
         self.label = label
         self.logger = Logger(label: label)
-        self.logLevel = LogAgentLevel.info
+        self.level = LogAgentLevel.info
     }
 
     public init(label: String, logLevel: LogAgentLevel) {
         self.label = label
         self.logger = Logger(label: label)
-        self.logLevel = logLevel
+        self.level = logLevel
     }
 
     // This initializer is currently only used in tests, to inject a mock LogHandler.
     init(label: String, logLevel: LogAgentLevel, factory: (String) -> any LogHandler) {
         self.label = label
-        self.logLevel = logLevel
+        self.level = logLevel
         self.logger = Logger(label: label, factory: factory)
-    }
-
-    public var level: LogAgentLevel {
-        get {
-            return logLevel
-        }
-        set(value) {
-            logLevel = value
-        }
     }
 
     public var name: String {
         return label
     }
 
-    public func log(level: LogAgentLevel,
-                    message: String,
-                    metadata: [String: String]?,
-                    source: String,
-                    file: String,
-                    function: String,
-                    line: UInt) {
-        let mappedDict = metadata?.mapValues { (value) -> Logger.MetadataValue in
-            return Logger.MetadataValue.string(value)
-        }
-        self.logger.log(level: level.toLoggerLevel(),
-                        Logger.Message(stringLiteral: message),
-                        metadata: mappedDict,
-                        source: source,
-                        file: file,
-                        function: function,
-                        line: line)
+    public func log(
+        level: LogAgentLevel,
+        message: @autoclosure () -> String,
+        metadata: @autoclosure () -> [String: String]?,
+        source: @autoclosure () -> String,
+        file: String,
+        function: String,
+        line: UInt
+    ) {
+        self.logger.log(
+            level: level.toLoggerLevel(),
+            Logger.Message(stringLiteral: message()),
+            metadata: metadata()?.mapValues { Logger.MetadataValue.string($0) },
+            source: source(),
+            file: file as String,
+            function: function as String,
+            line: line
+        )
     }
 }
 
 extension LogAgentLevel {
+
     func toLoggerLevel() -> Logger.Level {
         switch self {
         case .trace:
