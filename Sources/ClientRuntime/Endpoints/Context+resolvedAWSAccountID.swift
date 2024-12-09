59a44d97eba4215ab5d9b4c56ba5b00d719fe360
Josh Elkins <jbelkins@amazon.com>
2024-12-09 16:22:54 -0600

    Fix Swiftlint

--- Sources/ClientRuntime/Endpoints/Context+resolvedAWSAccountID.swift
+++ Sources/ClientRuntime/Endpoints/Context+resolvedAWSAccountID.swift
@@ -10,11 +10,11 @@ import struct Smithy.AttributeKey
 import struct SmithyIdentity.AWSCredentialIdentity
 
 public extension Context {
-    
+
     /// The AWS account ID associated with the selected auth scheme.
     ///
     /// Will be `nil` if an auth scheme has not yet been selected, an AWS credential identity was not resolved, or the identity did not resolve with an AWS account ID.
     var resolvedAWSAccountID: String? {
-        get { (selectedAuthScheme?.identity as? AWSCredentialIdentity)?.accountID }
+        (selectedAuthScheme?.identity as? AWSCredentialIdentity)?.accountID
     }
 }
