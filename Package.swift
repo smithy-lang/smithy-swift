27982f85b7752436d183f9ca2b73e96174677789
David Yaffe <dayaffe@amazon.com>
2025-06-20 11:41:26 -0400

    fix client runtime dependencies

--- Package.swift
+++ Package.swift
@@ -102,21 +102,12 @@ let package = Package(
                 .product(name: "AwsCommonRuntimeKit", package: "aws-crt-swift"),
                 .product(name: "InMemoryExporter", package: "opentelemetry-swift"),
                 // Only include these on macOS, iOS, tvOS, and watchOS (visionOS and Linux are excluded)
-                .product(
-                    name: "OpenTelemetryApi",
-                    package: "opentelemetry-swift",
-                    condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS])
-                ),
-                .product(
-                    name: "OpenTelemetrySdk",
-                    package: "opentelemetry-swift",
-                    condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS])
-                ),
-                .product(
-                    name: "OpenTelemetryProtocolExporterHTTP",
-                    package: "opentelemetry-swift",
-                    condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS])
-                ),
+                #if !os(visionOS) && !os(Linux)
+                .product(name: "InMemoryExporter", package: "opentelemetry-swift"),
+                .product(name: "OpenTelemetryApi", package: "opentelemetry-swift"),
+                .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift"),
+                .product(name: "OpenTelemetryProtocolExporterHTTP", package: "opentelemetry-swift"),
+                #endif
             ],
             resources: [
                 .copy("PrivacyInfo.xcprivacy")
