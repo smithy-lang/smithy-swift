32f19605e0debffda680909da6955eb8d7ee9077
Josh Elkins <jbelkins@amazon.com>
2025-08-05 14:42:17 -0500

    Rename JSON generator in test target

--- smithy-swift-codegen/src/test/kotlin/software/amazon/smithy/swift/codegen/manifestanddocs/ServiceClientJSONManifestGeneratorTests.kt
+++ smithy-swift-codegen/src/test/kotlin/software/amazon/smithy/swift/codegen/manifestanddocs/ServiceClientJSONManifestGeneratorTests.kt
@@ -8,7 +8,7 @@ package software.amazon.smithy.swift.codegen.manifestanddocs
 import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
-import software.amazon.smithy.swift.codegen.ServiceClientJSONGenerator
+import software.amazon.smithy.swift.codegen.DependencyJSONGenerator
 import software.amazon.smithy.swift.codegen.TestContext
 import software.amazon.smithy.swift.codegen.defaultSettings
 import software.amazon.smithy.swift.codegen.protocolgeneratormocks.MockHTTPAWSJson11ProtocolGenerator
@@ -35,7 +35,7 @@ class ServiceClientJSONManifestGeneratorTests {
             TestContext.initContextFrom(smithyFile, serviceShapeId, MockHTTPAWSJson11ProtocolGenerator()) { model ->
                 model.defaultSettings(serviceShapeId, "MockSDK", "2019-12-16", "MockSDKID")
             }
-        ServiceClientJSONGenerator(context.generationCtx).writePackageJSON(context.generationCtx.delegator.dependencies)
+        DependencyJSONGenerator(context.generationCtx).writePackageJSON(context.generationCtx.delegator.dependencies)
         context.generationCtx.delegator.flushWriters()
         return context
     }
