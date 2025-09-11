01696713076c7e92baa41692ae46e7076cb7cfda
David Yaffe <dayaffe@amazon.com>
2025-09-11 12:28:17 -0400

    Add AsyncHTTPClient dependency and basic class (#961)

---
+++ Sources/ClientRuntime/Networking/Http/NIO/NIOHTTPClient.swift
@@ -0,0 +1,37 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import AsyncHTTPClient
+import NIOCore
+import NIOPosix
+import SmithyHTTPAPI
+
+/// AsyncHTTPClient-based HTTP client implementation that conforms to SmithyHTTPAPI.HTTPClient
+/// This implementation is thread-safe and supports concurrent request execution.
+public final class NIOHTTPClient: SmithyHTTPAPI.HTTPClient {
+    private let client: AsyncHTTPClient.HTTPClient
+    private let config: HttpClientConfiguration
+
+    /// Creates a new `NIOHTTPClient`.
+    ///
+    /// The client is created with its own internal `AsyncHTTPClient`, which is configured with system defaults.
+    /// - Parameters:
+    ///   - httpClientConfiguration: The configuration to use for the client's `AsyncHTTPClient` setup.
+    public init(
+        httpClientConfiguration: HttpClientConfiguration
+    ) throws {
+        self.config = httpClientConfiguration
+        self.client = AsyncHTTPClient.HTTPClient(
+            configuration: .init() // TODO
+        )
+    }
+
+    public func send(request: SmithyHTTPAPI.HTTPRequest) async throws -> SmithyHTTPAPI.HTTPResponse {
+        // TODO
+        return HTTPResponse()
+    }
+}
