c663d7ec31d177047e5f54e6cfeac3d4cdfd60b1
David Yaffe <dayaffe@amazon.com>
2025-09-15 12:57:23 -0400

    Make extension private

--- Sources/ClientRuntime/Networking/Http/NIO/NIOHTTPClientConfiguration+HTTPClientConfiguration.swift
+++ Sources/ClientRuntime/Networking/Http/NIO/NIOHTTPClientConfiguration+HTTPClientConfiguration.swift
@@ -10,7 +10,7 @@ import NIOCore
 
 extension HTTPClient.Configuration {
 
-    public static func from(httpClientConfiguration: HttpClientConfiguration) -> HTTPClient.Configuration {
+    private static func from(httpClientConfiguration: HttpClientConfiguration) -> HTTPClient.Configuration {
         let connect: TimeAmount? = httpClientConfiguration.connectTimeout != nil
             ? .seconds(Int64(httpClientConfiguration.connectTimeout!))
             : nil
