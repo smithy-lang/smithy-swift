6a9c537af096532f9e70f1b3a5845cf6a5db26ee
Josh Elkins <jbelkins@amazon.com>
2025-11-09 11:29:43 -0600

    Make service trait optional

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/SmithyModelFileInfoGenerator.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/SmithyModelFileInfoGenerator.kt
@@ -2,21 +2,23 @@ package software.amazon.smithy.swift.codegen
 
 import software.amazon.smithy.aws.traits.ServiceTrait
 import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
-import software.amazon.smithy.swift.codegen.model.expectTrait
+import software.amazon.smithy.swift.codegen.model.getTrait
 
 class SmithyModelFileInfoGenerator(
     val ctx: ProtocolGenerator.GenerationContext,
 ) {
     fun writeSmithyModelFileInfo() {
-        val model =
-            ctx.service
-                .expectTrait<ServiceTrait>()
+        ctx.service.getTrait<ServiceTrait>()?.let { serviceTrait ->
+            val filename = "Sources/${ctx.settings.moduleName}/smithy-model-file-info.txt"
+            val modelFileName = serviceTrait
                 .sdkId
-                .replace(",", "")
                 .lowercase()
+                .replace(",", "")
                 .replace(" ", "-")
-        ctx.delegator.useFileWriter("Sources/${ctx.settings.moduleName}/smithy-model-file-info.txt") { writer ->
-            writer.write("codegen/sdk-codegen/aws-models/$model.json")
+            val contents = "codegen/sdk-codegen/aws-models/$modelFileName.json"
+            ctx.delegator.useFileWriter(filename) { writer ->
+                writer.write(contents)
+            }
         }
     }
 }
