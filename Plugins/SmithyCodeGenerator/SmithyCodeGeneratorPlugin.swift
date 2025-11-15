21ed6f74ca48307181121e04ee9996a9254458aa
Josh Elkins <jbelkins@amazon.com>
2025-11-15 11:58:30 -0600

    feat: Create code generator plugin

---
+++ Plugins/SmithyCodeGenerator/SmithyCodeGeneratorPlugin.swift
@@ -0,0 +1,68 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import struct Foundation.Data
+import class Foundation.FileManager
+import class Foundation.JSONDecoder
+import struct Foundation.URL
+import PackagePlugin
+
+@main
+struct SmithyCodeGeneratorPlugin: BuildToolPlugin {
+
+    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+        // This plugin only runs for package targets that can have source files.
+        guard let sourceFiles = target.sourceModule?.sourceFiles else { return [] }
+
+        // Retrieve the `SmithyCodegenCLI` tool from the plugin's tools.
+        let smithyCodegenCLITool = try context.tool(named: "SmithyCodegenCLI")
+
+        // Construct a build command for each source file with a particular suffix.
+        return try sourceFiles.map(\.path).compactMap {
+            try createBuildCommand(name: target.name, for: $0, in: context.pluginWorkDirectory, with: smithyCodegenCLITool.path)
+        }
+    }
+
+    private func createBuildCommand(
+        name: String,
+        for inputPath: Path,
+        in outputDirectoryPath: Path,
+        with generatorToolPath: Path
+    ) throws -> Command? {
+        // Skip any file that isn't the smithy-model-info.json for this service.
+        guard inputPath.lastComponent == "smithy-model-info.json" else { return nil }
+
+        let currentWorkingDirectoryFileURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+
+        // Get the smithy model path.
+        let modelInfoData = try Data(contentsOf: URL(fileURLWithPath: inputPath.string))
+        let smithyModelInfo = try JSONDecoder().decode(SmithyModelInfo.self, from: modelInfoData)
+        let modelPathURL = currentWorkingDirectoryFileURL.appendingPathComponent(smithyModelInfo.path)
+        let modelPath = Path(modelPathURL.path)
+
+        // Construct the schemas.swift path.
+        let schemasSwiftPath = outputDirectoryPath.appending("\(name)Schemas.swift")
+
+        // Construct the build command that invokes SmithyCodegenCLI.
+        return .buildCommand(
+            displayName: "Generating Swift source files from model file \(smithyModelInfo.path)",
+            executable: generatorToolPath,
+            arguments: [
+                "--schemas-path", schemasSwiftPath,
+                modelPath
+            ],
+            inputFiles: [inputPath, modelPath],
+            outputFiles: [schemasSwiftPath]
+        )
+    }
+}
+
+/// Codable structure for reading the contents of `smithy-model-info.json`
+private struct SmithyModelInfo: Decodable {
+    /// The path to the model, from the root of the target's project.  Required.
+    let path: String
+}
---
+++ Sources/SmithyCodegenCLI/SmithyCodegenCLI.swift
@@ -0,0 +1,62 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import ArgumentParser
+import Foundation
+import SmithyCodegenCore
+
+@main
+struct SmithyCodegenCLI: AsyncParsableCommand {
+
+    @Argument(help: "The full or relative path to the JSON model file.")
+    var modelPath: String
+
+    @Option(help: "The full or relative path to write the schemas output file.")
+    var schemasPath: String?
+
+    func run() async throws {
+        let currentWorkingDirectoryFileURL = currentWorkingDirectoryFileURL()
+        print("Current working directory: \(currentWorkingDirectoryFileURL.path)")
+
+        // Create the model file URL
+        let modelFileURL = URL(fileURLWithPath: modelPath, relativeTo: currentWorkingDirectoryFileURL)
+        guard FileManager.default.fileExists(atPath: modelFileURL.path) else {
+            throw SmithyCodegenCLIError(localizedDescription: "no file at model path \(modelFileURL.path)")
+        }
+        print("Model file path: \(modelFileURL.path)")
+
+        // If --schemas-path was supplied, create the schema file URL
+        let schemasFileURL = resolve(paramName: "--schemas-path", path: schemasPath)
+
+        // Use resolved file URLs to run code generator
+        try CodeGenerator(modelFileURL: modelFileURL, schemasFileURL: schemasFileURL).run()
+    }
+
+    private func currentWorkingDirectoryFileURL() -> URL {
+        // Get the current working directory as a file URL
+        var currentWorkingDirectoryPath = FileManager.default.currentDirectoryPath
+        if !currentWorkingDirectoryPath.hasSuffix("/") {
+            currentWorkingDirectoryPath.append("/")
+        }
+        return URL(fileURLWithPath: currentWorkingDirectoryPath)
+    }
+
+    private func resolve(paramName: String, path: String?) -> URL? {
+        if let path {
+            let fileURL = URL(fileURLWithPath: path, relativeTo: currentWorkingDirectoryFileURL())
+            print("Resolved \(paramName): \(fileURL.path)")
+            return fileURL
+        } else {
+            print("\(paramName) not provided, skipping generation")
+            return nil
+        }
+    }
+}
+
+struct SmithyCodegenCLIError: Error {
+    let localizedDescription: String
+}
