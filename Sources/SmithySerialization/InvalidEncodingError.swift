98e660dcc2fc5c5687aa84f9d3924eeb3001953c
Josh Elkins <jbelkins@amazon.com>
2025-11-24 12:16:31 -0500

    Refactor & document InvalidEncodingError

--- Sources/SmithySerialization/InvalidEncodingError.swift
+++ Sources/SmithySerialization/InvalidEncodingError.swift
@@ -5,11 +5,24 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-public struct InvalidEncodingError: Error {
-    public var localizedDescription: String { "The data in the response could not be parsed" }
-    public let wrapped: Error // this will be the underlying error thrown by the parser implementation
+/// The data being deserialized was invalid, and a response could not be parsed.
+///
+/// If an error of this type is received, it indicates a bug either on the server or client.  Please file a [bug ticket](https://github.com/smithy-lang/smithy-swift/issues).
+public struct InvalidEncodingError {
 
-    public init(wrapped: Error) {
+    /// The error thrown by the deserializing implementation.
+    ///
+    /// The exact error returned may vary depending on the encoding in use.
+    public let wrapped: any Swift.Error // this will be the underlying error thrown by the parser implementation
+
+    public init(wrapped: any Error) {
         self.wrapped = wrapped
+    }
+}
+
+extension InvalidEncodingError: Error {
+
+    public var localizedDescription: String {
+        "The data in the response could not be parsed.  More info: \(wrapped)"
     }
 }
