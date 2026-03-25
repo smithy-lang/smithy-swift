8c7d65be3e9ec32a59112882bf61129b8f18dd9e
Josh Elkins <jbelkins@amazon.com>
2026-03-25 17:15:08 -0500

    Fix lint

--- smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/serde/SerdeUtils.kt
+++ smithy-swift-codegen/src/main/kotlin/software/amazon/smithy/swift/codegen/integration/serde/SerdeUtils.kt
@@ -12,11 +12,12 @@ import software.amazon.smithy.swift.codegen.model.hasTrait
 
 class SerdeUtils {
     companion object {
-        fun useSchemaBased(ctx: ProtocolGenerator.GenerationContext) =
-            useSchemaBased(ctx.service)
+        fun useSchemaBased(ctx: ProtocolGenerator.GenerationContext) = useSchemaBased(ctx.service)
 
-        fun useSchemaBased(settings: SwiftSettings, model: Model) =
-            useSchemaBased(model.expectShape<ServiceShape>(settings.service))
+        fun useSchemaBased(
+            settings: SwiftSettings,
+            model: Model,
+        ) = useSchemaBased(model.expectShape<ServiceShape>(settings.service))
 
         private fun useSchemaBased(service: ServiceShape) =
             // This fun is temporary; it will be eliminated when all services/protocols are moved to schema-based
