6aee99753ef9a4f071eb76817baba88d49d4935f
Josh Elkins <jbelkins@amazon.com>
2026-04-27 17:40:40 -0500

    feat: Add jsonName trait support to schema-based JSON

---
+++ Sources/Smithy/TraitLibrary/JSONNameTrait.swift
@@ -0,0 +1,20 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+// https://smithy.io/2.0/spec/protocol-traits.html#jsonname-trait
+
+public struct JSONNameTrait: Trait {
+    public static var id: ShapeID { .init("smithy.api", "jsonName") }
+
+    public var node: Node { .string(name) }
+    public var name: String
+
+    public init(node: Node) throws {
+        guard let name = node.string else { throw TraitError("jsonName trait node must contain String") }
+        self.name = name
+    }
+}
