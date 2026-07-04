97b5743f4ded994e31f8aacb971959ca4fc7c2af
Josh Elkins <jbelkins@amazon.com>
2026-07-03 19:22:12 -0500

    Restore JSON deser code

--- Sources/SmithyJSON/Deserializer.swift
+++ Sources/SmithyJSON/Deserializer.swift
@@ -68,11 +68,11 @@ public final class Deserializer: ShapeDeserializer {
         }
 
         // Iterate over JSON elements of object & call consumer with the appropriate member.
-//        for (key, val) in object where val != .null { // skip null values in structures
-//            guard let memberSchema = try match(key: key, memberSchemas: memberSchemas) else { continue }
-//            let memberDeserializer = Deserializer(usesJSONNameTrait: usesJSONNameTrait, node: val)
-//            try value.deserializeMember(memberSchema, memberDeserializer)
-//        }
+        for (key, val) in object where val != .null { // skip null values in structures
+            guard let memberSchema = try match(key: key, memberSchemas: memberSchemas) else { continue }
+            let memberDeserializer = Deserializer(usesJSONNameTrait: usesJSONNameTrait, node: val)
+            try value.deserializeMember(memberSchema, memberDeserializer)
+        }
     }
 
     public func readList<E>(_ schema: Schema, _ consumer: (any ShapeDeserializer) throws -> E) throws -> [E] {
