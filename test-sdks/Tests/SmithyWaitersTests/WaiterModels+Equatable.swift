b21aae9541a3af4f51a0c1a04892a4648e602609
Josh Elkins <jbelkins@amazon.com>
2026-07-08 14:15:58 -0500

    Adopt Swift 5 style of retroactive conformance

--- test-sdks/Tests/SmithyWaitersTests/WaiterModels+Equatable.swift
+++ test-sdks/Tests/SmithyWaitersTests/WaiterModels+Equatable.swift
@@ -7,18 +7,18 @@
 
 @testable import WaitersTestSDK
 
-// `@retroactive` silences the retroactive-conformance warning on Swift 6+, but
-// the attribute is not recognized by the Swift 5.9 compiler, so gate on it.
-#if compiler(>=6.0)
+// Fully-qualifying the protocol as `Swift.Equatable` suppresses the Swift 6
+// "retroactive conformance" warning in a manner compatible with Swift 5.
+// See: https://github.com/swiftlang/swift-evolution/blob/main/proposals/0364-retroactive-conformance-warning.md#source-compatibility
 
-extension GetWidgetInput: @retroactive Equatable {
+extension GetWidgetInput: Swift.Equatable {
 
     public static func ==(lhs: GetWidgetInput, rhs: GetWidgetInput) -> Bool {
         return lhs.stringProperty == rhs.stringProperty
     }
 }
 
-extension GetWidgetOutput: @retroactive Equatable {
+extension GetWidgetOutput: Swift.Equatable {
 
     public static func ==(lhs: GetWidgetOutput, rhs: GetWidgetOutput) -> Bool {
         return lhs.booleanArrayProperty == rhs.booleanArrayProperty &&
@@ -30,53 +30,16 @@ extension GetWidgetOutput: @retroactive Equatable {
     }
 }
 
-extension WaitersClientTypes.Child: @retroactive Equatable {
+extension WaitersClientTypes.Child: Swift.Equatable {
 
     public static func ==(lhs: WaitersClientTypes.Child, rhs: WaitersClientTypes.Child) -> Bool {
         return lhs.grandchildren == rhs.grandchildren
     }
 }
 
-extension WaitersClientTypes.Grandchild: @retroactive Equatable {
+extension WaitersClientTypes.Grandchild: Swift.Equatable {
 
     public static func ==(lhs: WaitersClientTypes.Grandchild, rhs: WaitersClientTypes.Grandchild) -> Bool {
         return lhs.name == rhs.name && lhs.number == rhs.number
     }
 }
-
-#else
-
-extension WaitersTestSDK.GetWidgetInput: Equatable {
-
-    public static func ==(lhs: GetWidgetInput, rhs: GetWidgetInput) -> Bool {
-        return lhs.stringProperty == rhs.stringProperty
-    }
-}
-
-extension WaitersTestSDK.GetWidgetOutput: Equatable {
-
-    public static func ==(lhs: GetWidgetOutput, rhs: GetWidgetOutput) -> Bool {
-        return lhs.booleanArrayProperty == rhs.booleanArrayProperty &&
-        lhs.booleanProperty == rhs.booleanProperty &&
-        lhs.children == rhs.children &&
-        lhs.dataMap == rhs.dataMap &&
-        lhs.stringArrayProperty == rhs.stringArrayProperty &&
-        lhs.stringProperty == rhs.stringProperty
-    }
-}
-
-extension WaitersTestSDK.WaitersClientTypes.Child: Equatable {
-
-    public static func ==(lhs: WaitersClientTypes.Child, rhs: WaitersClientTypes.Child) -> Bool {
-        return lhs.grandchildren == rhs.grandchildren
-    }
-}
-
-extension WaitersTestSDK.WaitersClientTypes.Grandchild: Equatable {
-
-    public static func ==(lhs: WaitersClientTypes.Grandchild, rhs: WaitersClientTypes.Grandchild) -> Bool {
-        return lhs.name == rhs.name && lhs.number == rhs.number
-    }
-}
-
-#endif
