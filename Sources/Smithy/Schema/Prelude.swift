6ef21a88b771ef0a39f755b4562892242ebba0f9
Josh Elkins <jbelkins@amazon.com>
2026-07-07 21:40:53 -0500

    perf: Prelude converted to stored schemas

--- Sources/Smithy/Schema/Prelude.swift
+++ Sources/Smithy/Schema/Prelude.swift
@@ -11,59 +11,46 @@
 @_spi(SchemaBasedSerde)
 public enum Prelude {
 
-    public static var unitSchema: Schema {
+    public static let unitSchema: Schema =
         Schema(id: .init("smithy.api", "Unit"), type: .structure, traits: [UnitTypeTrait()])
-    }
 
-    public static var booleanSchema: Schema {
+    public static let booleanSchema: Schema =
         Schema(id: .init("smithy.api", "Boolean"), type: .boolean)
-    }
 
-    public static var stringSchema: Schema {
+    public static let stringSchema: Schema =
         Schema(id: .init("smithy.api", "String"), type: .string)
-    }
 
-    public static var integerSchema: Schema {
+    public static let integerSchema: Schema =
         Schema(id: .init("smithy.api", "Integer"), type: .integer)
-    }
 
-    public static var blobSchema: Schema {
+    public static let blobSchema: Schema =
         Schema(id: .init("smithy.api", "Blob"), type: .blob)
-    }
 
-    public static var timestampSchema: Schema {
+    public static let timestampSchema: Schema =
         Schema(id: .init("smithy.api", "Timestamp"), type: .timestamp)
-    }
 
-    public static var byteSchema: Schema {
+    public static let byteSchema: Schema =
         Schema(id: .init("smithy.api", "Byte"), type: .byte)
-    }
 
-    public static var shortSchema: Schema {
+    public static let shortSchema: Schema =
         Schema(id: .init("smithy.api", "Short"), type: .short)
-    }
 
-    public static var longSchema: Schema {
+    public static let longSchema: Schema =
         Schema(id: .init("smithy.api", "Long"), type: .long)
-    }
 
-    public static var floatSchema: Schema {
+    public static let floatSchema: Schema =
         Schema(id: .init("smithy.api", "Float"), type: .float)
-    }
 
-    public static var doubleSchema: Schema {
+    public static let doubleSchema: Schema =
         Schema(id: .init("smithy.api", "Double"), type: .double)
-    }
 
-    public static var bigIntegerSchema: Schema {
+    public static let bigIntegerSchema: Schema =
         Schema(id: .init("smithy.api", "BigInteger"), type: .bigInteger)
-    }
 
-    public static var bigDecimalSchema: Schema {
+    public static let bigDecimalSchema: Schema =
         Schema(id: .init("smithy.api", "BigDecimal"), type: .bigDecimal)
-    }
 
-    public static var documentSchema: Schema {
+    public static let documentSchema: Schema =
         Schema(id: .init("smithy.api", "Document"), type: .document, members: [
             // Since document can be a list or map of documents, among other types, it has the members for both
             // Map key/value comes first, then list member
@@ -72,33 +59,25 @@ public enum Prelude {
             .init(id: .init("smithy.api", "Document", "value"), type: .document, containerType: .document),
             .init(id: .init("smithy.api", "Document", "member"), type: .document, containerType: .document),
         ])
-    }
 
-    public static var primitiveBooleanSchema: Schema {
+    public static let primitiveBooleanSchema: Schema =
         Schema(id: .init("smithy.api", "PrimitiveBoolean"), type: .boolean, traits: [DefaultTrait(false)])
-    }
 
-    public static var primitiveIntegerSchema: Schema {
+    public static let primitiveIntegerSchema: Schema =
         Schema(id: .init("smithy.api", "PrimitiveInteger"), type: .integer, traits: [DefaultTrait(0)])
-    }
 
-    public static var primitiveByteSchema: Schema {
+    public static let primitiveByteSchema: Schema =
         Schema(id: .init("smithy.api", "PrimitiveByte"), type: .byte, traits: [DefaultTrait(0)])
-    }
 
-    public static var primitiveShortSchema: Schema {
+    public static let primitiveShortSchema: Schema =
         Schema(id: .init("smithy.api", "PrimitiveShort"), type: .short, traits: [DefaultTrait(0)])
-    }
 
-    public static var primitiveLongSchema: Schema {
+    public static let primitiveLongSchema: Schema =
         Schema(id: .init("smithy.api", "PrimitiveLong"), type: .long, traits: [DefaultTrait(0)])
-    }
 
-    public static var primitiveFloatSchema: Schema {
+    public static let primitiveFloatSchema: Schema =
         Schema(id: .init("smithy.api", "PrimitiveFloat"), type: .float, traits: [DefaultTrait(0.0)])
-    }
 
-    public static var primitiveDoubleSchema: Schema {
+    public static let primitiveDoubleSchema: Schema =
         Schema(id: .init("smithy.api", "PrimitiveDouble"), type: .double, traits: [DefaultTrait(0.0)])
-    }
 }
