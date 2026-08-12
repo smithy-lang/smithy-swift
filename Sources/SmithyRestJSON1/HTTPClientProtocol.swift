8a657aa168058e8e001270d6effa79e8ab6c6177
Josh Elkins <jbelkins@amazon.com>
2026-08-12 09:35:02 -0500

    Add bindings to RestJSON client protocol, clean up settings.json defaults

--- Sources/SmithyRestJSON1/HTTPClientProtocol.swift
+++ Sources/SmithyRestJSON1/HTTPClientProtocol.swift
@@ -9,9 +9,17 @@ import struct Foundation.Data
 import class Smithy.Context
 @_spi(SchemaBasedSerde)
 import struct Smithy.ShapeID
+@_spi(SchemaBasedSerde)
+import class Smithy.HTTPTrait
 import class SmithyHTTPAPI.HTTPRequest
 import class SmithyHTTPAPI.HTTPRequestBuilder
 import class SmithyHTTPAPI.HTTPResponse
+@_spi(SchemaBasedSerde)
+import class SmithyHTTPAPI.HTTPLabelSerializer
+@_spi(SchemaBasedSerde)
+import class SmithyHTTPAPI.HTTPQuerySerializer
+@_spi(SchemaBasedSerde)
+import class SmithyHTTPAPI.HTTPQueryParamsSerializer
 @_spi(SchemaBasedSerde)
 import protocol SmithySerialization.ClientProtocol
 @_spi(SchemaBasedSerde)
@@ -44,6 +52,36 @@ public struct HTTPClientProtocol: SmithySerialization.ClientProtocol {
         // This type is incomplete & not yet used in production.
         // This method body remains empty for now.
         // Will provide complete body for this later.
+
+        // Use the operation's HTTP trait plus the input members' HTTPLabel trait
+        // to populate a complete path for the HTTP request.
+        if let uri = operation.schema.getTrait(HTTPTrait.self)?.uri {
+            let labelSerializer = HTTPLabelSerializer(uri: uri)
+            try input.serializeMembers(operation.inputSchema, labelSerializer)
+            context.path = labelSerializer.uri
+            requestBuilder.withPath(labelSerializer.uri)
+        }
+
+        // Serialize any HTTP query-bound members to the HTTP request's query string
+        let querySerializer = HTTPQuerySerializer()
+        try input.serializeMembers(operation.inputSchema, querySerializer)
+        requestBuilder.withQueryItems(querySerializer.queryItems)
+
+        // Serialize any HTTP query-params-bound members to the HTTP request's query string
+        // Skip any query with a name that was already added
+        let queryParamsSerializer = HTTPQueryParamsSerializer()
+        try input.serializeMembers(operation.inputSchema, queryParamsSerializer)
+        queryParamsSerializer.queryItems.forEach { queryItem in
+            if !requestBuilder.queryItems.contains { $0.name == queryItem.name } {
+                requestBuilder.withQueryItem(queryItem)
+            }
+        }
+
+        // Serialize the input into the request body.
+        // HTTP bindings and HTTP payload trait are not yet accounted for.
+        let serializer = try self.codec.makeSerializer()
+        try input.serialize(serializer)
+        requestBuilder.withBody(.data(try serializer.data))
     }
 
     public func deserializeResponse<Input, Output>(
